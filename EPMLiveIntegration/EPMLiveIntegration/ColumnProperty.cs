ac3710d22e69b999aecde1ef1413d89d007dcaf6
Jason Hughes <jhughes@lmrsolutions.com>
2013-02-13 22:37:30 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C611

---
+++ EPMLiveIntegration/EPMLiveIntegration/ColumnProperty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPMLiveIntegration
+{
+    public class ColumnProperty
+    {
+        public string ColumnName;
+        public string DiplayName;
+        public Type type;
+        public string DefaultListColumn;
+    }
+}
---
+++ EPMLiveIntegration/EPMLiveIntegration/IntegrationLog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace EPMLiveIntegration
+{
+    public enum IntegrationLogType { Information = 1, Warning, Error };
+
+    public class IntegrationLog
+    {
+        private SqlConnection _cn;
+        private Guid _intlogid;
+        private Guid _listid;
+        private string _title;
+
+        public IntegrationLog(SqlConnection CN, Guid intlogid, Guid listid, string title)
+        {
+            _cn = CN;
+            _intlogid = intlogid;
+            _listid = listid;
+            _title = title;
+        }
+
+        public void LogMessage(string log, IntegrationLogType level)
+        {
+            if(_cn.State == System.Data.ConnectionState.Closed)
+                _cn.Open();
+
+            SqlCommand cmdError = new SqlCommand("INSERT INTO INT_LOG (INT_LIST_ID, LIST_ID, LOGTYPE, LOGTEXT) VALUES (@intlistid, @listid, @level, @text)", _cn);
+            cmdError.Parameters.AddWithValue("@intlistid", _intlogid);
+            cmdError.Parameters.AddWithValue("@listid", _listid);
+            cmdError.Parameters.AddWithValue("@level", (int)level);
+            cmdError.Parameters.AddWithValue("@text", _title + ": " + log);
+            cmdError.ExecuteNonQuery();
+
+            _cn.Close();
+        }
+    }
+}
