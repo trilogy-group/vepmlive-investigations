95fa5884c6ebe2d9f89c009e0d2477d178c5afc5
Jason Hughes <jhughes@lmrsolutions.com>
2013-05-13 23:13:47 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C1487

--- EPMLiveWorkPlanner/EPMLiveWorkPlanner/ISAPI/WorkPlanner.asmx.cs
+++ EPMLiveWorkPlanner/EPMLiveWorkPlanner/ISAPI/WorkPlanner.asmx.cs
@@ -4429,7 +4429,7 @@ namespace EPMLiveWorkPlanner
                             format = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.FullDateTimePattern;
                         }
                     }catch{}
-                    EditFormat = format;
+                    EditFormat = format + " HH:mm:ss";
                     break;
                 case SPFieldType.Number:
                     if(oDoc.FirstChild.Attributes["Percentage"] != null && oDoc.FirstChild.Attributes["Percentage"].Value.ToLower() == "true")
