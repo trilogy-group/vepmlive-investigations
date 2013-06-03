d934e7287c9b01d88f9132fbaf7ab6fb26b3c456
David Kuo <dkuo@lmrsolutions.com>
2013-06-03 18:03:00 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C1686

--- EPMLiveCore/EPMLiveCore/BtnCommonActions.cs
+++ EPMLiveCore/EPMLiveCore/BtnCommonActions.cs
@@ -52,38 +52,11 @@ namespace EPMLiveCore
                 writer.Write("<script type=\"text/javascript\"> var commonActionsAsyncUrl = \"" + _commonActionsDataHostPageUrl + "\"; </script>");
                 writer.Write("<script type=\"text/javascript\" src=\"" + site.MakeFullUrl(web.ServerRelativeUrl) + "/_layouts/epmlive/BtnCommonActions.js\"></script>");
 
-                writer.Write("<A id=\"lnkCommonActions\" title=\"Common Actions\" rel=\"tooltipbottom\" class=\"ms-socialNotif\" onclick=\"ToggleCommonActionsMenu();return false;\" href=\"javascript:;\"  style=\"width:55px\"><SPAN onlick=\"document.getElementById('lnkCommonActions').click()\"><SPAN onlick=\"document.getElementById('lnkCommonActions').click()\" style=\"POSITION: relative; WIDTH: 32px; DISPLAY: inline-block; HEIGHT: 32px; OVERFLOW: hidden\" ><IMG style=\"BORDER-BOTTOM: 0px; POSITION: absolute; BORDER-LEFT: 0px; BORDER-TOP: 0px; BORDER-RIGHT: 0px; LEFT: -409px !important; TOP:-11px;\" src=\"/_layouts/epmlive/images/epmlive-header.png\"></SPAN></SPAN></A>");
+                // container div
+                writer.Write("<div style=\"max-width:35px;height:70px;float:right;margin-right:20px;\" >");
 
                 // render the button
-                // =====================================================
-                //writer.Write("<a id=\"lnkCommonActions\" style=\"height: 15px;"+
-                //                                            "color: #1f497d;" +
-                //                                            "padding-top: 4px;" +
-                //                                            "padding-right: 10px;" +
-                //                                            "padding-bottom: 4px;" +
-                //                                            "padding-left: 10px;" +
-                //                                            "vertical-align: middle;" +
-                //                                            "border-top-color: transparent;" +
-                //                                            "border-right-color: transparent;" +
-                //                                            "border-bottom-color: transparent;" +
-                //                                            "border-left-color: transparent;" +
-                //                                            "border-top-width: 1px;" +
-                //                                            "border-right-width: 1px;" +
-                //                                            "border-bottom-width: 1px;" +
-                //                                            "border-left-width: 1px;" +
-                //                                            "border-top-style: solid;" +
-                //                                            "border-right-style: solid;" +
-                //                                            "border-bottom-style: solid;" +
-                //                                            "border-left-style: solid;" +
-                //                                            "display: inline-block;" +
-                //                                            "white-space: nowrap;\"" +
-                //                                            "onmouseover=\"this.style.color = '#4F81BD';\"" +
-                //                                            "onmouseout=\"this.style.color = '#1f497d';\"" +
-                //                                            "href=\"#\" onclick=\"ToggleCommonActionsMenu();return false;\">");
-                //writer.Write("<span style=\"background-image:url('/_layouts/images/menu-down.gif'); background-position: right center; background-repeat: no-repeat; padding-right:10px;display:block;\">");
-                //writer.Write("<span id=\"spnCommonActionsText\" class=\"menu-item-text\" style=\"display:block;\">Common Actions</span>");
-                //writer.Write("</span>");
-                //writer.Write("</a>");
+                writer.Write("<A id=\"lnkCommonActions\" title=\"Common Actions\" rel=\"tooltipbottom\" class=\"ms-socialNotif\" onclick=\"ToggleCommonActionsMenu();return false;\" href=\"javascript:;\"  style=\"width:55px\"><SPAN onlick=\"document.getElementById('lnkCommonActions').click()\"><SPAN onlick=\"document.getElementById('lnkCommonActions').click()\" style=\"POSITION: relative; WIDTH: 32px; DISPLAY: inline-block; HEIGHT: 32px; OVERFLOW: hidden\" ><IMG style=\"BORDER-BOTTOM: 0px; POSITION: absolute; BORDER-LEFT: 0px; BORDER-TOP: 0px; BORDER-RIGHT: 0px; LEFT: -409px !important; TOP:-11px;\" src=\"/_layouts/epmlive/images/epmlive-header.png\"></SPAN></SPAN></A>");
 
                 // html for actual menu
                 // ===================================================
@@ -97,6 +70,9 @@ namespace EPMLiveCore
                 writer.Write("</div>");
                 // =========================================
                 writer.Write("</div>");
+                writer.Write("</div>");
+
+                // container div closing tag
                 writer.Write("</div>");
             }
         }
--- EPMLiveCore/EPMLiveCore/BtnCreateNew.cs
+++ EPMLiveCore/EPMLiveCore/BtnCreateNew.cs
@@ -48,41 +48,11 @@ namespace EPMLiveCore
                 writer.Write("<script type=\"text/javascript\"> var createNewAsyncUrl = \"" + _createNewDataHostPageUrl + "\"; </script>");
                 writer.Write("<script type=\"text/javascript\" src=\"" + site.MakeFullUrl(web.ServerRelativeUrl) + "/_layouts/epmlive/BtnCreateNew.js\"></script>");
 
-
-
-                writer.Write("<A id=lnkCreateNew rel=\"tooltipbottom\" title=\"Create New\" class=\"ms-socialNotif\" onclick=\"ToggleCreateNewMenu();return false;\" href=\"javascript:;\"><SPAN onlick=\"document.getElementById('lnkCreateNew').click()\"><SPAN onlick=\"document.getElementById('lnkCreateNew').click()\" style=\"POSITION: relative; WIDTH: 32px; DISPLAY: inline-block; HEIGHT: 32px; OVERFLOW: hidden\" ><IMG style=\"BORDER-BOTTOM: 0px; POSITION: absolute; BORDER-LEFT: 0px; BORDER-TOP: 0px; BORDER-RIGHT: 0px; LEFT: -461px !important; TOP: -11px;\" src=\"/_layouts/epmlive/images/epmlive-header.png\"></SPAN></SPAN></A>");
+                // render container div
+                writer.Write("<div style=\"max-width:35px;height:70px;float:right;margin-right:20px;\" >");
 
                 // render button
-                // =====================================================
-                /*writer.Write("<a id=\"lnkCreateNew\" style=\"height: 15px;" +
-                                                            "color: #1f497d;" +
-                                                            "padding-top: 4px;" +
-                                                            "padding-right: 10px;" +
-                                                            "padding-bottom: 4px;" +
-                                                            "padding-left: 10px;" +
-                                                            "vertical-align: middle;" +
-                                                            "border-top-color: transparent;" +
-                                                            "border-right-color: transparent;" +
-                                                            "border-bottom-color: transparent;" +
-                                                            "border-left-color: transparent;" +
-                                                            "border-top-width: 1px;" +
-                                                            "border-right-width: 1px;" +
-                                                            "border-bottom-width: 1px;" +
-                                                            "border-left-width: 1px;" +
-                                                            "border-top-style: solid;" +
-                                                            "border-right-style: solid;" +
-                                                            "border-bottom-style: solid;" +
-                                                            "border-left-style: solid;" +
-                                                            "display: inline-block;" +
-                                                            "white-space: nowrap;\"" +
-                                                            "onmouseover=\"this.style.color = '#4F81BD';\"" +
-                                                            "onmouseout=\"this.style.color = '#1f497d';\"" +
-                                                            "href=\"#\"" +
-                                                            "onclick=\"ToggleCreateNewMenu();return false;\">");
-                writer.Write("<span onlick=\"document.getElementById('lnkCreateNew').click()\" style=\"background-image:url('/_layouts/images/menu-down.gif'); background-position: right center; background-repeat: no-repeat; padding-right:10px;\">");
-                writer.Write("<span onlick=\"document.getElementById('lnkCreateNew').click()\" id=\"spnCreateNewText\" class=\"menu-item-text\" >+ Create New Item</span>");
-                writer.Write("</span>");
-                writer.Write("</a>");*/
+                writer.Write("<A id=lnkCreateNew rel=\"tooltipbottom\" title=\"Create New\" class=\"ms-socialNotif\" onclick=\"ToggleCreateNewMenu();return false;\" href=\"javascript:;\"><SPAN onlick=\"document.getElementById('lnkCreateNew').click()\"><SPAN onlick=\"document.getElementById('lnkCreateNew').click()\" style=\"POSITION: relative; WIDTH: 32px; DISPLAY: inline-block; HEIGHT: 32px; OVERFLOW: hidden\" ><IMG style=\"BORDER-BOTTOM: 0px; POSITION: absolute; BORDER-LEFT: 0px; BORDER-TOP: 0px; BORDER-RIGHT: 0px; LEFT: -461px !important; TOP: -11px;\" src=\"/_layouts/epmlive/images/epmlive-header.png\"></SPAN></SPAN></A>");
 
                 // html for actual menu
                 // ===================================================
@@ -96,6 +66,9 @@ namespace EPMLiveCore
                 writer.Write("</div>");
                 // =========================================
                 writer.Write("</div>");
+                writer.Write("</div>");
+
+                // render container div closing tag
                 writer.Write("</div>");
             }
         }
