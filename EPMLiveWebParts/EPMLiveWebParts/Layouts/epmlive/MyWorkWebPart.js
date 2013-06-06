9b297f8fb6b0003ad9790df0c950ece761e8b04c
Munjal Patel <EPM\mpatel>
2013-06-06 19:27:46 +0000

    My Work - Ribbon tabs
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C1720

--- EPMLiveWebParts/EPMLiveWebParts/Layouts/epmlive/MyWorkWebPart.js
+++ EPMLiveWebParts/EPMLiveWebParts/Layouts/epmlive/MyWorkWebPart.js
@@ -59,6 +59,36 @@ Grids.OnReady = function (grid, start) {
                 window.setTimeout(function () {
                     try {
                         window.SelectRibbonTab('Ribbon.MyWorkTab', true);
+                        
+                        window.setTimeout(function () {
+                            try {
+                                var setTabStyle = function () {
+                                    var tabs = [$(document.getElementById('Ribbon.MyWorkTab-title')), $(document.getElementById('Ribbon.MyWorkViewsTab-title'))];
+
+                                    for (var tab in tabs) {
+                                        if (tabs.hasOwnProperty(tab)) {
+                                            var t = tabs[tab];
+
+                                            t.attr('style', 'border-top: 1px solid #E1E1E1 !important; height: 33px !important; margin-top: -4px !important');
+                                            t.find('a').attr('style', 'padding-top: 4px !important;');
+
+                                            if (t.attr('aria-selected') === 'false') {
+                                                t.attr('style', 'height: 33px !important; margin-top: -3px !important');
+                                            }
+
+                                            t.click(function () {
+                                                window.setTimeout(function () {
+                                                    setTabStyle();
+                                                }, 100);
+                                            });
+                                        }
+                                    }
+                                };
+
+                                setTabStyle();
+                            } catch (ex) {
+                            }
+                        }, 2000);
                     } catch (e) {
                     }
                 }, 500);
