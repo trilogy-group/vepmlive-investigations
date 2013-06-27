f72a6e4f505539d4a9ff49308de4964d83d37000
Munjal Patel <EPM\mpatel>
2013-06-27 21:39:15 +0000

    Office 365
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C1917

---
+++ EPMLiveCore/EPMLiveCore/Integrations/Office365/Infrastructure/AESCryptographyService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace EPMLiveCore.Integrations.Office365.Infrastructure
+{
+    internal class AESCryptographyService
+    {
+        #region Fields (5) 
+
+        private static readonly byte[] Key =
+            {
+                77, 72, 250, 87, 203, 51, 197, 46, 47, 138, 50, 53, 180, 18, 235, 173,
+                217, 83, 7, 52, 134, 127, 239, 11, 83, 200, 24, 196, 255, 135, 51, 81
+            };
+
+        private static readonly byte[] Vector =
+            {
+                51, 102, 16, 170, 191, 238, 20, 90, 243, 71, 35, 52, 60, 105, 18, 215
+            };
+
+        private readonly ICryptoTransform _decryptor;
+        private readonly UTF8Encoding _encoder;
+        private readonly ICryptoTransform _encryptor;
+
+        #endregion Fields 
+
+        #region Constructors (1) 
+
+        public AESCryptographyService()
+        {
+            var rm = new RijndaelManaged();
+            _encryptor = rm.CreateEncryptor(Key, Vector);
+            _decryptor = rm.CreateDecryptor(Key, Vector);
+            _encoder = new UTF8Encoding();
+        }
+
+        #endregion Constructors 
+
+        #region Methods (7) 
+
+        // Public Methods (6) 
+
+        public string Decrypt(string encrypted)
+        {
+            return _encoder.GetString(Decrypt(Convert.FromBase64String(encrypted)));
+        }
+
+        public byte[] Decrypt(byte[] buffer)
+        {
+            return Transform(buffer, _decryptor);
+        }
+
+        public string DecryptFromUrl(string encrypted)
+        {
+            return Decrypt(HttpUtility.UrlDecode(encrypted));
+        }
+
+        public string Encrypt(string unencrypted)
+        {
+            return Convert.ToBase64String(Encrypt(_encoder.GetBytes(unencrypted)));
+        }
+
+        public byte[] Encrypt(byte[] buffer)
+        {
+            return Transform(buffer, _encryptor);
+        }
+
+        public string EncryptToUrl(string unencrypted)
+        {
+            return HttpUtility.UrlEncode(Encrypt(unencrypted));
+        }
+
+        // Protected Methods (1) 
+
+        protected byte[] Transform(byte[] buffer, ICryptoTransform transform)
+        {
+            var stream = new MemoryStream();
+
+            using (var cs = new CryptoStream(stream, transform, CryptoStreamMode.Write))
+            {
+                cs.Write(buffer, 0, buffer.Length);
+            }
+
+            return stream.ToArray();
+        }
+
+        #endregion Methods 
+    }
+}
--- EPMLiveCore/EPMLiveCore/Integrations/Office365/Integrator.cs
+++ EPMLiveCore/EPMLiveCore/Integrations/Office365/Integrator.cs
@@ -1,55 +1,214 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Security;
 using EPMLiveCore.Integrations.Office365.Infrastructure;
 using EPMLiveIntegration;
+using Microsoft.SharePoint.Client;
 
 namespace EPMLiveCore.Integrations.Office365
 {
     public class Integrator : IIntegrator
     {
-        #region Methods (9) 
+        #region Methods (14) 
 
         // Public Methods (9) 
 
         public TransactionTable DeleteItems(WebProperties webProps, DataTable items, IntegrationLog log)
         {
-            throw new NotImplementedException();
+            var transactionTable = new TransactionTable();
+
+            try
+            {
+                if (!bool.Parse((string) webProps.Properties["AllowDeleteInt"]))
+                {
+                    throw new Exception("Office 365 delete is not allowed.");
+                }
+
+                O365Service o365Service = GetO365Service(webProps);
+
+                List<string> ids;
+                Dictionary<string, string> idMap = BuildIdMap(items, out ids);
+
+                int index = 0;
+
+                foreach (
+                    O365Result result in
+                        o365Service.DeleteListItemsById(ids.ToArray(), (string) webProps.Properties["List"]))
+                {
+                    string o365Id = result.ItemId.ToString();
+                    string spid;
+
+                    try
+                    {
+                        spid = idMap[o365Id];
+                    }
+                    catch
+                    {
+                        spid = items.Rows[index]["SPID"].ToString();
+                    }
+
+                    if (result.Success)
+                    {
+                        transactionTable.AddRow(spid, o365Id, TransactionType.DELETE);
+                    }
+                    else
+                    {
+                        transactionTable.AddRow(spid, o365Id, TransactionType.FAILED);
+                        log.LogMessage(string.Format(
+                            "Could not delete record with Office 365 ID: {0}, SharePoint ID: {1}. Message: {2}",
+                            o365Id, spid, result.Error), IntegrationLogType.Warning);
+                    }
+
+                    index++;
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogMessage(e.Message, IntegrationLogType.Error);
+            }
+
+            return transactionTable;
         }
 
         public List<ColumnProperty> GetColumns(WebProperties webProps, IntegrationLog log, string listName)
         {
-            throw new NotImplementedException();
+            var columnProperties = new List<ColumnProperty>();
+
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+
+                var ignoredFields = new[]
+                    {
+                        "AppAuthor", "AppEditor", "Attachments", "DocIcon",
+                        "ItemChildCount", "FolderChildCount", "_UIVersionString"
+                    };
+
+                columnProperties.AddRange(
+                    from field in o365Service.GetListFields(webProps.Properties["List"].ToString())
+                    let internalName = field.InternalName
+                    where !ignoredFields.Contains(internalName) &&
+                          !internalName.StartsWith("LinkTitle") && !internalName.EndsWith("NoMenu")
+                    orderby field.Title
+                    select new ColumnProperty
+                        {
+                            ColumnName = internalName,
+                            DiplayName = field.Title,
+                            type = TranslateFieldType(field.FieldTypeKind),
+                            DefaultListColumn = GetMatchingListColumn(internalName)
+                        });
+            }
+            catch (Exception e)
+            {
+                log.LogMessage(e.Message, IntegrationLogType.Error);
+            }
+
+            return columnProperties;
         }
 
         public Dictionary<string, string> GetDropDownValues(WebProperties webProps, IntegrationLog log, string property,
                                                             string parentPropertyValue)
         {
-            throw new NotImplementedException();
+            var dictionary = new Dictionary<string, string>();
+
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+
+                if (property.Equals("List")) dictionary = o365Service.GetIntegratableLists();
+                else if (property.Equals("UserMapType")) return new Dictionary<string, string> {{"Email", "Email"}};
+
+                throw new Exception("Invalid property: " + property);
+            }
+            catch (Exception e)
+            {
+                log.LogMessage(e.Message, IntegrationLogType.Error);
+            }
+
+            return dictionary;
         }
 
         public DataTable GetItem(WebProperties webProps, IntegrationLog log, string itemId, DataTable items)
         {
-            throw new NotImplementedException();
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+                o365Service.GetListItemsById((string) webProps.Properties["List"], itemId, items);
+            }
+            catch (Exception e)
+            {
+                log.LogMessage(e.Message, e.Message.StartsWith("No records found")
+                                              ? IntegrationLogType.Warning
+                                              : IntegrationLogType.Error);
+            }
+
+            return items;
         }
 
         public bool InstallIntegration(WebProperties webProps, IntegrationLog log, out string message,
                                        string integrationKey,
                                        string apiUrl)
         {
-            throw new NotImplementedException();
+            message = string.Empty;
+
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+
+                o365Service.InstallIntegration(webProps.IntegrationId, integrationKey, apiUrl, webProps.Title,
+                                               webProps.FullURL, (string) webProps.Properties["List"],
+                                               bool.Parse((string) webProps.Properties["AllowAddInt"]),
+                                               bool.Parse((string) webProps.Properties["AllowAddList"]),
+                                               bool.Parse((string) webProps.Properties["AllowDeleteInt"]));
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+
+            return false;
         }
 
         public DataTable PullData(WebProperties webProps, IntegrationLog log, DataTable items, DateTime lastSynchDate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+                o365Service.GetListItems((string) webProps.Properties["List"], items, lastSynchDate);
+            }
+            catch (Exception e)
+            {
+                log.LogMessage("Scheduled Pull. " + e.Message, e.Message.StartsWith("No records found")
+                                                                   ? IntegrationLogType.Warning
+                                                                   : IntegrationLogType.Error);
+            }
+
+            return items;
         }
 
         public bool RemoveIntegration(WebProperties webProps, IntegrationLog log, out string message,
                                       string integrationKey)
         {
-            throw new NotImplementedException();
+            message = string.Empty;
+
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+                o365Service.UninstallIntegration(integrationKey, webProps.IntegrationId);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("List 'EPMLiveIntegrations' does not exist")) return true;
+                message = e.Message;
+            }
+
+            return false;
         }
 
         public bool TestConnection(WebProperties webProps, IntegrationLog log, out string message)
@@ -59,37 +218,105 @@ namespace EPMLiveCore.Integrations.Office365
             try
             {
                 O365Service o365Service = GetO365Service(webProps);
-                return o365Service.EnsureEPMLiveAppInstalled();
+                if (!o365Service.EnsureEPMLiveAppInstalled())
+                {
+                    log.LogMessage(
+                        "Please make sure that you have installed the EPM Live app in your Office 365 environment.",
+                        IntegrationLogType.Error);
+
+                    return false;
+                }
+
+                return true;
             }
             catch (Exception e)
             {
                 message = e.Message;
                 log.LogMessage(message, IntegrationLogType.Error);
-            }
 
-            return false;
+                return false;
+            }
         }
 
         public TransactionTable UpdateItems(WebProperties webProps, DataTable items, IntegrationLog log)
         {
-            throw new NotImplementedException();
+            var transactionTable = new TransactionTable();
+
+            try
+            {
+                O365Service o365Service = GetO365Service(webProps);
+
+                List<string> ids;
+                Dictionary<string, string> idMap = BuildIdMap(items, out ids);
+
+                int index = 0;
+
+                foreach (
+                    O365Result result in
+                        o365Service.UpsertItems((string) webProps.Properties["List"], webProps.IntegrationId, items))
+                {
+                    string o365Id = result.ItemId.ToString();
+
+                    string spId;
+
+                    try
+                    {
+                        spId = idMap[o365Id];
+                    }
+                    catch
+                    {
+                        spId = items.Rows[index]["SPID"].ToString();
+                    }
+
+                    if (result.Success)
+                    {
+                        transactionTable.AddRow(spId, o365Id, result.TransactionType);
+                    }
+                    else
+                    {
+                        transactionTable.AddRow(spId, o365Id, TransactionType.FAILED);
+
+                        log.LogMessage(string.Format(
+                            "Could not insert / update record with Office 365 ID: {0}, SharePoint ID: {1}. Message: {2}",
+                            o365Id, spId, result.Error), IntegrationLogType.Warning);
+                    }
+
+                    index++;
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogMessage(e.Message, IntegrationLogType.Error);
+            }
+
+            return transactionTable;
         }
 
-        #endregion Methods 
+        // Private Methods (5) 
 
-        private O365Service GetO365Service(WebProperties webProps)
+        private static Dictionary<string, string> BuildIdMap(DataTable items, out List<string> ids)
         {
-            string username;
-            SecureString password;
-            string url;
+            var idMap = new Dictionary<string, string>();
+            ids = new List<string>();
 
-            GetAuthParameters(webProps, out username, out password, out url);
+            foreach (DataRow dataRow in items.Rows)
+            {
+                object oId = dataRow["ID"];
 
-            return new O365Service(username, password, url);
+                if (oId == null || oId == DBNull.Value) continue;
+
+                string id = oId.ToString();
+                if (ids.Contains(id)) continue;
+
+                ids.Add(id);
+                idMap.Add(id, dataRow["SPID"].ToString());
+            }
+
+            return idMap;
         }
 
         private static void GetAuthParameters(WebProperties webProps, out string username, out SecureString password,
-                                              out string url)
+                                              out string siteUrl)
         {
             username = webProps.Properties["Username"].ToString();
             if (string.IsNullOrEmpty(username)) throw new Exception("Please provide the username.");
@@ -99,14 +326,53 @@ namespace EPMLiveCore.Integrations.Office365
 
             password = pwd.ToSecureString();
 
-            url = webProps.Properties["URL"].ToString();
-            if (string.IsNullOrEmpty(url)) throw new Exception("Please provide the Office 365 site url.");
+            siteUrl = webProps.Properties["SiteUrl"].ToString();
+            if (string.IsNullOrEmpty(siteUrl)) throw new Exception("Please provide the Office 365 site url.");
 
             Uri uri;
-            if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out uri))
+            if (!Uri.TryCreate(siteUrl, UriKind.RelativeOrAbsolute, out uri))
             {
-                throw new Exception(url + " is not a valid url.");
+                throw new Exception(siteUrl + " is not a valid url.");
             }
         }
+
+        private string GetMatchingListColumn(string internalName)
+        {
+            return internalName.Equals("ID") ? "INTUID" : internalName;
+        }
+
+        private O365Service GetO365Service(WebProperties webProps)
+        {
+            string username;
+            SecureString password;
+            string siteUrl;
+
+            GetAuthParameters(webProps, out username, out password, out siteUrl);
+
+            return new O365Service(username, password, siteUrl);
+        }
+
+        private Type TranslateFieldType(FieldType fieldType)
+        {
+            switch (fieldType)
+            {
+                case FieldType.Boolean:
+                    return typeof (bool);
+                case FieldType.Counter:
+                case FieldType.Integer:
+                    return typeof (int);
+                case FieldType.Currency:
+                case FieldType.Number:
+                    return typeof (decimal);
+                case FieldType.DateTime:
+                    return typeof (DateTime);
+                case FieldType.Guid:
+                    return typeof (Guid);
+                default:
+                    return typeof (string);
+            }
+        }
+
+        #endregion Methods 
     }
 }
