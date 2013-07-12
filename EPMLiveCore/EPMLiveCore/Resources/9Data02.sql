22b1da63ceb9c6dd2f2a31eb014efdb99d7753c7
builduser <EPM\builduser>
2013-07-12 16:31:17 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2028

--- EPMLiveCore/EPMLiveCore/Resources/9Data02.sql
+++ EPMLiveCore/EPMLiveCore/Resources/9Data02.sql
@@ -24,19 +24,19 @@ begin
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 8)
 begin
-    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (8,'EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Jobs.AdSynchJob','Active Directory Synch',99)
+    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (8,'EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Jobs.AdSynchJob','Active Directory Synch',98)
 end
 else
 begin
-    UPDATE TIMERJOBTYPES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Jobs.AdSynchJob', [Title]='Active Directory Synch', Priority=99 where jobtype_id=8
+    UPDATE TIMERJOBTYPES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Jobs.AdSynchJob', [Title]='Active Directory Synch', Priority=98 where jobtype_id=8
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 3)
 begin
-    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (3,'EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Jobs.Notifications','Notifications',99)
+    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (3,'EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Jobs.Notifications','Notifications',98)
 end
 else
 begin
-    UPDATE TIMERJOBTYPES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Jobs.Notifications', [Title]='Notifications', Priority=99 where jobtype_id=3
+    UPDATE TIMERJOBTYPES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Jobs.Notifications', [Title]='Notifications', Priority=98 where jobtype_id=3
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 2)
 begin
@@ -64,11 +64,11 @@ begin
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 5)
 begin
-    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (5,'EPMLiveReportsAdmin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b90e532f481cf050','EPMLiveReportsAdmin.Jobs.CollectJob','Reporting Refresh',99)
+    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (5,'EPMLiveReportsAdmin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b90e532f481cf050','EPMLiveReportsAdmin.Jobs.CollectJob','Reporting Refresh',40)
 end
 else
 begin
-    UPDATE TIMERJOBTYPES SET NetAssembly='EPMLiveReportsAdmin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b90e532f481cf050', NetClass='EPMLiveReportsAdmin.Jobs.CollectJob', [Title]='Reporting Refresh', Priority=99 where jobtype_id=5
+    UPDATE TIMERJOBTYPES SET NetAssembly='EPMLiveReportsAdmin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b90e532f481cf050', NetClass='EPMLiveReportsAdmin.Jobs.CollectJob', [Title]='Reporting Refresh', Priority=40 where jobtype_id=5
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 7)
 begin
@@ -80,11 +80,11 @@ begin
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 10)
 begin
-    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (10,'WorkEnginePPM, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','WorkEnginePPM.Jobs.Cleanup','PPM Cleanup',99)
+    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (10,'WorkEnginePPM, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','WorkEnginePPM.Jobs.Cleanup','PPM Cleanup',98)
 end
 else
 begin
-    UPDATE TIMERJOBTYPES SET NetAssembly='WorkEnginePPM, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='WorkEnginePPM.Jobs.Cleanup', [Title]='PPM Cleanup', Priority=99 where jobtype_id=10
+    UPDATE TIMERJOBTYPES SET NetAssembly='WorkEnginePPM, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='WorkEnginePPM.Jobs.Cleanup', [Title]='PPM Cleanup', Priority=98 where jobtype_id=10
 end
 if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 20)
 begin
@@ -198,6 +198,14 @@ else
 begin
     UPDATE TIMERJOBTYPES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Jobs.EPMLiveUpgrade.Upgrader', [Title]='EPM Live Upgrader', Priority=11 where jobtype_id=201
 end
+if not exists (select jobtype_id from TIMERJOBTYPES where jobtype_id = 6)
+begin
+    INSERT INTO TIMERJOBTYPES (jobtype_id,NetAssembly,NetClass,[Title],priority) VALUES (6,'EPMLiveReportsAdmin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b90e532f481cf050','EPMLiveReportsAdmin.Jobs.ListDataCleanupJob','List Data Cleanup',99)
+end
+else
+begin
+    UPDATE TIMERJOBTYPES SET NetAssembly='EPMLiveReportsAdmin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=b90e532f481cf050', NetClass='EPMLiveReportsAdmin.Jobs.ListDataCleanupJob', [Title]='List Data Cleanup', Priority=99 where jobtype_id=6
+end
 if not exists (select MODULE_ID from INT_MODULES where MODULE_ID = 'a0950b9b-3525-40b8-a456-6403156dc499')
 begin
     INSERT INTO INT_MODULES (MODULE_ID,NetAssembly,NetClass,[Title],Description,Icon,CustomProps,AvailableOnline) VALUES ('a0950b9b-3525-40b8-a456-6403156dc499','EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.API.Integration.SQL','SQL','This provides access to data located in Microsoft SQL Server','sql.png','<Properties>
@@ -228,17 +236,51 @@ begin
 end
 if not exists (select MODULE_ID from INT_MODULES where MODULE_ID = 'a0950b9b-3525-40b8-a456-6403156dc49a')
 begin
-    INSERT INTO INT_MODULES (MODULE_ID,NetAssembly,NetClass,[Title],Description,Icon,CustomProps,AvailableOnline) VALUES ('a0950b9b-3525-40b8-a456-6403156dc49a','EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Integrations.Salesforce.Integrator','SalesForce','This connector allows data synchronization to and from salesforce','salesforce.png','<Properties><Connection><Input Type="Text" Property="Username" Title="Salesforce Username">Please make sure that this user has the ''API Enabled'' and ''Modify All Data'' permissions.</Input><Input Type="Password" Property="Password" Title="Password"/><Input Type="Password" Property="SecurityToken" Title="Security Token"/><Input Type="Checkbox" Property="Sandbox" Title="Sandbox">Check this box if you are connecting to your Sandbox organization.</input></Connection><General><Input Type="Select" Property="Object" Title="Select an object to map"/><Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/></General></Properties>','True')
+    INSERT INTO INT_MODULES (MODULE_ID,NetAssembly,NetClass,[Title],Description,Icon,CustomProps,AvailableOnline) VALUES ('a0950b9b-3525-40b8-a456-6403156dc49a','EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Integrations.Salesforce.Integrator','SalesForce','This connector allows data synchronization to and from salesforce','salesforce.png','<Properties><Connection><Input Type="Text" Property="Username" Title="Salesforce Username">Please make sure that this user has the ''API Enabled'' and ''Modify All Data'' permissions.</Input><Input Type="Password" Property="Password" Title="Password"/><Input Type="Password" Property="SecurityToken" Title="Security Token"/><Input Type="Checkbox" Property="Sandbox" Title="Sandbox">Check this box if you are connecting to your Sandbox organization.</Input></Connection><General><Input Type="Select" Property="Object" Title="Select an object to map"/><Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/></General></Properties>','True')
 end
 else
 begin
-    UPDATE INT_MODULES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Integrations.Salesforce.Integrator', [Title]='SalesForce', Description='This connector allows data synchronization to and from salesforce', Icon='salesforce.png', CustomProps='<Properties><Connection><Input Type="Text" Property="Username" Title="Salesforce Username">Please make sure that this user has the ''API Enabled'' and ''Modify All Data'' permissions.</Input><Input Type="Password" Property="Password" Title="Password"/><Input Type="Password" Property="SecurityToken" Title="Security Token"/><Input Type="Checkbox" Property="Sandbox" Title="Sandbox">Check this box if you are connecting to your Sandbox organization.</input></Connection><General><Input Type="Select" Property="Object" Title="Select an object to map"/><Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/></General></Properties>',AvailableOnline='True' where module_id='a0950b9b-3525-40b8-a456-6403156dc49a'
+    UPDATE INT_MODULES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Integrations.Salesforce.Integrator', [Title]='SalesForce', Description='This connector allows data synchronization to and from salesforce', Icon='salesforce.png', CustomProps='<Properties><Connection><Input Type="Text" Property="Username" Title="Salesforce Username">Please make sure that this user has the ''API Enabled'' and ''Modify All Data'' permissions.</Input><Input Type="Password" Property="Password" Title="Password"/><Input Type="Password" Property="SecurityToken" Title="Security Token"/><Input Type="Checkbox" Property="Sandbox" Title="Sandbox">Check this box if you are connecting to your Sandbox organization.</Input></Connection><General><Input Type="Select" Property="Object" Title="Select an object to map"/><Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/></General></Properties>',AvailableOnline='True' where module_id='a0950b9b-3525-40b8-a456-6403156dc49a'
 end
 if not exists (select MODULE_ID from INT_MODULES where MODULE_ID = 'a0950b9b-3525-40b8-a456-6403156dc49b')
 begin
-    INSERT INTO INT_MODULES (MODULE_ID,NetAssembly,NetClass,[Title],Description,Icon,CustomProps,AvailableOnline) VALUES ('a0950b9b-3525-40b8-a456-6403156dc49b','EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','API.Integration.Office365','Office 365','','office365.png','','True')
+    INSERT INTO INT_MODULES (MODULE_ID,NetAssembly,NetClass,[Title],Description,Icon,CustomProps,AvailableOnline) VALUES ('a0950b9b-3525-40b8-a456-6403156dc49b','EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.Integrations.Office365.Integrator','Office 365','This provides access to list data located on an Office 365 SharePoint site.','office365.png','<Properties><Connection><Input Type="Text" Property="SiteUrl" Title="Office 365 Site URL">Your Office 365 Site URL. For example, https://company.sharepoint.com/sites/it</Input><Input Type="Text" Property="Username" Title="Username">Your Office 365 username. For example, user@company.onmicrosoft.com</Input><Input Type="Password" Property="Password" Title="Password"/></Connection><General><Input Type="Select" Property="List" Title="Select a list to map"/><Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/></General></Properties>','True')
 end
 else
 begin
-    UPDATE INT_MODULES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='API.Integration.Office365', [Title]='Office 365', Description='', Icon='office365.png', CustomProps='',AvailableOnline='True' where module_id='a0950b9b-3525-40b8-a456-6403156dc49b'
+    UPDATE INT_MODULES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.Integrations.Office365.Integrator', [Title]='Office 365', Description='This provides access to list data located on an Office 365 SharePoint site.', Icon='office365.png', CustomProps='<Properties><Connection><Input Type="Text" Property="SiteUrl" Title="Office 365 Site URL">Your Office 365 Site URL. For example, https://company.sharepoint.com/sites/it</Input><Input Type="Text" Property="Username" Title="Username">Your Office 365 username. For example, user@company.onmicrosoft.com</Input><Input Type="Password" Property="Password" Title="Password"/></Connection><General><Input Type="Select" Property="List" Title="Select a list to map"/><Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/></General></Properties>',AvailableOnline='True' where module_id='a0950b9b-3525-40b8-a456-6403156dc49b'
+end
+if not exists (select MODULE_ID from INT_MODULES where MODULE_ID = 'a0950b9b-3525-40b8-a456-6403156dc49c')
+begin
+    INSERT INTO INT_MODULES (MODULE_ID,NetAssembly,NetClass,[Title],Description,Icon,CustomProps,AvailableOnline) VALUES ('a0950b9b-3525-40b8-a456-6403156dc49c','EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5','EPMLiveCore.API.Integration.Generic','Generic','This integration provides a web service that can be called from any 3rd party application','','<Properties>
+	<Connection>
+		<Input Type="Text" Property="Name" Title="Integration Name">Enter the name of the 3rd party application. This can be anything and does not affect the integration.</Input>
+		<Input Type="Text" Property="WSDL" Title="Web Service URL">Enter the URL for your outgoing webservice</Input>
+		
+	</Connection>
+	<General>
+		<Input Type="TextArea" Property="Fields" Title="Enter Field List"><![CDATA[Enter the list of fields that will come <b>in</b> from the 3rd party application seperated by a comma. These fields should be formatted in such a way that the 3rd party application understands what they are.]]></Input>
+		<Input Type="TextArea" Property="WSDLMap" Title="Web Service Field Map"><![CDATA[Enter the mapping of the Web Service Fields. This data will be sent <b>out</b>. Please see documentation for format of field.]]></Input>
+		<Input Type="Select" Property="WSDLFunction" Title="Select the function you want to call when posting to your web service."/>
+		<Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/>
+		<Input Type="Checkbox" Property="LogInfo" Title="Log informational messages."> This can be used to troubleshoot issues.</Input>
+	</General>
+</Properties>','True')
+end
+else
+begin
+    UPDATE INT_MODULES SET NetAssembly='EPM Live Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9f4da00116c38ec5', NetClass='EPMLiveCore.API.Integration.Generic', [Title]='Generic', Description='This integration provides a web service that can be called from any 3rd party application', Icon='', CustomProps='<Properties>
+	<Connection>
+		<Input Type="Text" Property="Name" Title="Integration Name">Enter the name of the 3rd party application. This can be anything and does not affect the integration.</Input>
+		<Input Type="Text" Property="WSDL" Title="Web Service URL">Enter the URL for your outgoing webservice</Input>
+		
+	</Connection>
+	<General>
+		<Input Type="TextArea" Property="Fields" Title="Enter Field List"><![CDATA[Enter the list of fields that will come <b>in</b> from the 3rd party application seperated by a comma. These fields should be formatted in such a way that the 3rd party application understands what they are.]]></Input>
+		<Input Type="TextArea" Property="WSDLMap" Title="Web Service Field Map"><![CDATA[Enter the mapping of the Web Service Fields. This data will be sent <b>out</b>. Please see documentation for format of field.]]></Input>
+		<Input Type="Select" Property="WSDLFunction" Title="Select the function you want to call when posting to your web service."/>
+		<Input Type="Select" Property="UserMapType" Title="Select the user mapping field"/>
+		<Input Type="Checkbox" Property="LogInfo" Title="Log informational messages."> This can be used to troubleshoot issues.</Input>
+	</General>
+</Properties>',AvailableOnline='True' where module_id='a0950b9b-3525-40b8-a456-6403156dc49c'
 end
