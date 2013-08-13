ecb91469c9212966aa39ebaa23174fe17aad7b3b
Munjal Patel <EPM\mpatel>
2013-08-13 19:03:55 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2160

--- EPMLiveCore/EPMLiveCore/Layouts/epmlive/javascripts/masterpages/UplandV5.js
+++ EPMLiveCore/EPMLiveCore/Layouts/epmlive/javascripts/masterpages/UplandV5.js
@@ -2,6 +2,24 @@
     'use strict';
 
     function epmLiveTweaks() {
+        var hideSearchIfRequired = function() {
+            var $sbtn = $('#search-container');
+            var $sbox = $('#search-box-container');
+
+            if ($sbtn.length === 0 || $sbox.length === 0) {
+                window.setTimeout(function() {
+                    hideSearchIfRequired();
+                }, 1);
+            } else {
+                if ($sbox.find('input').length !== 1) {
+                    $sbtn.hide();
+                    $sbox.hide();
+                }
+            }
+        };
+
+        hideSearchIfRequired();
+
         window.toggleSearch = function () {
             var $sbox = $('#search-box-container');
             var $sicon = $('#search-container');
