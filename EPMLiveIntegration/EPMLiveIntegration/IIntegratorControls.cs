45e53bce827ca59364c621b4b2ae6490bd4ff548
Jason Hughes <jhughes@lmrsolutions.com>
2013-08-28 21:17:46 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2287

---
+++ EPMLiveIntegration/EPMLiveIntegration/IIntegratorControls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPMLiveIntegration
+{
+    public class IntegrationControl
+    {
+        public IntegrationControl(string Control, string URL)
+        {
+            this.Control = Control;
+            this.URL = URL;
+        }
+
+        public string Control;
+        public string URL;
+    }
+
+    public interface IIntegratorControls
+    {
+        List<IntegrationControl> GetControls(WebProperties WebProps, IntegrationLog Log);
+        string GetURL(WebProperties WebProps, IntegrationLog Log, string control, string url);
+    }
+}
+
