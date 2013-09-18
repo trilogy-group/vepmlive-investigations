274a0fc651382a0ab29cc1899f3665ff7cbae9ad
Jason Hughes <jhughes@lmrsolutions.com>
2013-09-18 00:24:14 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2479

--- EPMLiveIntegration/EPMLiveIntegration/IIntegratorControls.cs
+++ EPMLiveIntegration/EPMLiveIntegration/IIntegratorControls.cs
@@ -10,13 +10,14 @@ namespace EPMLiveIntegration
         public string Control;
         public string Image;
         public string Title;
+        public bool Window;
     }
 
     public interface IIntegratorControls
     {
         List<string> GetLocalControls(WebProperties WebProps, IntegrationLog Log);
         List<IntegrationControl> GetRemoteControls(WebProperties WebProps, IntegrationLog Log);
-        string GetURL(WebProperties WebProps, IntegrationLog Log, string control, string url);
+        string GetURL(WebProperties WebProps, IntegrationLog Log, string control, string ItemID);
         string GetControlCode(WebProperties WebProps, IntegrationLog Log, string ItemID, string Control);
     }
 }
