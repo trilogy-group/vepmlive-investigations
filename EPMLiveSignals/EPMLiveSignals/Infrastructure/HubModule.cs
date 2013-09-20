657385e14fa0299fadf48f1a44e8ba95a38560ee
Munjal Patel <EPM\mpatel>
2013-09-20 23:01:25 +0000

    Initial check in
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2543

---
+++ EPMLiveSignals/EPMLiveSignals/Infrastructure/HubModule.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Web.Hosting;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+
+namespace EPMLiveSignals.Infrastructure
+{
+    public class HubModule : IHttpModule
+    {
+        #region Fields (2) 
+
+        private static bool _initialized;
+        private readonly object _locker = new object();
+
+        #endregion Fields 
+
+        #region IHttpModule Members
+
+        public void Dispose() { }
+
+        public void Init(HttpApplication context)
+        {
+            if (_initialized) return;
+
+            lock (_locker)
+            {
+                if (_initialized) return;
+
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableCrossDomain = true,
+                    EnableDetailedErrors = true,
+                    EnableJavaScriptProxies = true
+                };
+
+                RouteTable.Routes.MapHubs(hubConfiguration);
+                HostingEnvironment.RegisterVirtualPathProvider(new SignalRVirtualPathProvider());
+
+                _initialized = true;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ EPMLiveSignals/EPMLiveSignals/Infrastructure/SignalRVirtualPathProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Remoting;
+using System.Web.Caching;
+using System.Web.Hosting;
+
+namespace EPMLiveSignals.Infrastructure
+{
+    internal class SignalRVirtualPathProvider : VirtualPathProvider
+    {
+        #region Methods (9) 
+
+        // Public Methods (9) 
+
+        public override string CombineVirtualPaths(string basePath, string relativePath)
+        {
+            return Previous.CombineVirtualPaths(basePath, relativePath);
+        }
+
+        public override ObjRef CreateObjRef(Type requestedType)
+        {
+            return Previous.CreateObjRef(requestedType);
+        }
+
+        public override bool DirectoryExists(string virtualDir)
+        {
+            if (!string.IsNullOrEmpty(virtualDir) && virtualDir.Contains("signalr"))
+            {
+                return Previous.DirectoryExists(virtualDir.TrimStart('~'));
+            }
+
+            try
+            {
+                return Previous.DirectoryExists(virtualDir);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public override bool FileExists(string virtualPath)
+        {
+            return Previous.FileExists(virtualPath);
+        }
+
+        public override CacheDependency GetCacheDependency(string virtualPath, IEnumerable virtualPathDependencies,
+            DateTime utcStart)
+        {
+            return Previous.GetCacheDependency(virtualPath, virtualPathDependencies, utcStart);
+        }
+
+        public override string GetCacheKey(string virtualPath)
+        {
+            return Previous.GetCacheKey(virtualPath);
+        }
+
+        public override VirtualDirectory GetDirectory(string virtualDir)
+        {
+            return Previous.GetDirectory(virtualDir);
+        }
+
+        public override VirtualFile GetFile(string virtualPath)
+        {
+            return Previous.GetFile(virtualPath);
+        }
+
+        public override string GetFileHash(string virtualPath, IEnumerable virtualPathDependencies)
+        {
+            return Previous.GetFileHash(virtualPath, virtualPathDependencies);
+        }
+
+        #endregion Methods 
+    }
+}
