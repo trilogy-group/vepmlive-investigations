ea735b46fb80eb30b61f6ccbab4cd91c7e2d3e80
David Kuo <dkuo@lmrsolutions.com>
2013-09-25 06:25:25 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2602

--- EPMLiveCore/EPMLiveCore/DispForm.cs
+++ EPMLiveCore/EPMLiveCore/DispForm.cs
@@ -165,6 +165,12 @@ namespace EPMLiveCore
                 }
             }
 
+            //=====================Favorite
+
+            ribbonExtensions = new XmlDocument();
+            ribbonExtensions.LoadXml("<Button Id=\"Ribbon.ListItem.EPMLive.FavoriteStatus\" Sequence=\"100\" Command=\"Ribbon.ListForm.Display.Actions.Favorite\" LabelText=\"Favorite\" TemplateAlias=\"o1\" Image32by32=\"_layouts/epmlive/images/star32.png\" Image16by16=\"_layouts/epmlive/images/star16.png\"/>");
+            ribbon.RegisterDataExtension(ribbonExtensions.FirstChild, "Ribbon.ListForm.Display.Actions.Controls._children");
+
             EPMLiveCore.API.RibbonProperties rp = (EPMLiveCore.API.RibbonProperties)EPMLiveCore.Infrastructure.CacheStore.Current.Get("GR-" + Web.CurrentUser.ID, "GridSettings-" + List.ID, () =>
             {
                 return EPMLiveCore.API.ListCommands.GetRibbonProps(List);
