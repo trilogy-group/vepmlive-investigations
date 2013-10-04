5afac6d2cba096b6fe428babe2dd0bbf34f9478e
Munjal Patel <EPM\mpatel>
2013-10-04 02:25:08 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2860

--- EPMLiveCore/EPMLiveCore/Layouts/epmlive/javascripts/masterpages/UplandV5.js
+++ EPMLiveCore/EPMLiveCore/Layouts/epmlive/javascripts/masterpages/UplandV5.js
@@ -4,10 +4,17 @@
     'use strict';
 
     function loadUserVoice() {
+        //var uv = document.createElement('script');
+        //uv.type = 'text/javascript';
+        //uv.async = true;
+        //uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/yEDMyjE4NNMKmmRoDiWQ.js';
+        //var s = document.getElementsByTagName('script')[0];
+        //s.parentNode.insertBefore(uv, s);
+        
         var uv = document.createElement('script');
         uv.type = 'text/javascript';
         uv.async = true;
-        uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/yEDMyjE4NNMKmmRoDiWQ.js';
+        uv.src = '//widget.uservoice.com/uFW21LPmYTawwUX9btPog.js';
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(uv, s);
     }
