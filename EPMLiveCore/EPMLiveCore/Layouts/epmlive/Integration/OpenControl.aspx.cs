5ac34baf637c3f95725a3fa56a0bf6b40d41f0d6
Jason Hughes <jhughes@lmrsolutions.com>
2013-10-10 16:33:30 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C2996

--- EPMLiveCore/EPMLiveCore/Layouts/epmlive/Integration/OpenControl.aspx.cs
+++ EPMLiveCore/EPMLiveCore/Layouts/epmlive/Integration/OpenControl.aspx.cs
@@ -12,21 +12,61 @@ namespace EPMLiveCore.Layouts.epmlive.Integration
             string intuid = Request["ID"];
             string control = Request["Control"];
 
-            SPSecurity.RunWithElevatedPrivileges(delegate()
-            {
-                API.Integration.IntegrationCore core = new API.Integration.IntegrationCore(Web.Site.ID, Web.ID);
-                SPListItem li = core.GetListItemFromExternalID(intlistid, intuid);
-                if(li != null)
-                {
-                    RedirectTo(li, control);
-                }
-                else
-                {
-                    lblError.Text = "Could not find item";
-                }
-            });
 
+            if (intuid != "")
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate()
+                {
+                    API.Integration.IntegrationCore core = new API.Integration.IntegrationCore(Web.Site.ID, Web.ID);
+                    SPListItem li = core.GetListItemFromExternalID(intlistid, intuid);
+                    if (li != null)
+                    {
+                        RedirectTo(li, control);
+                    }
+                    else
+                    {
+                        lblError.Text = "Could not find item";
+                    }
+                });
+            }
+            else
+            {
+                RedirectTo(control);
+            }
            
+        }
+
+        private void RedirectTo(string control)
+        {
+            switch (control)
+            {
+                case "workplan":
+                    Microsoft.SharePoint.Utilities.SPUtility.Redirect("epmlive/workplanner.aspx?CloseMethod=3&isdlg=1", Microsoft.SharePoint.Utilities.SPRedirectFlags.RelativeToLayoutsPage, System.Web.HttpContext.Current);
+                    break;
+                case "costplan":
+                    {
+                        Microsoft.SharePoint.Utilities.SPUtility.Redirect("ppm/costs.aspx?CloseMethod=3&isdlg=1", Microsoft.SharePoint.Utilities.SPRedirectFlags.RelativeToLayoutsPage, System.Web.HttpContext.Current);
+                    }
+                    break;
+                case "resplan":
+                    {
+                        Microsoft.SharePoint.Utilities.SPUtility.Redirect("ppm/rpeditor.aspx?CloseMethod=3&isdlg=1", Microsoft.SharePoint.Utilities.SPRedirectFlags.RelativeToLayoutsPage, System.Web.HttpContext.Current);
+                    }
+                    break;
+                case "costanalyzer":
+                    {
+                        Microsoft.SharePoint.Utilities.SPUtility.Redirect("ppm/costanalyzer.aspx?CloseMethod=3&isdlg=1", Microsoft.SharePoint.Utilities.SPRedirectFlags.RelativeToLayoutsPage, System.Web.HttpContext.Current);
+                    }
+                    break;
+                case "resanalyzer":
+                    {
+                        Microsoft.SharePoint.Utilities.SPUtility.Redirect("ppm/rpanalyzer.aspx?CloseMethod=3&isdlg=1", Microsoft.SharePoint.Utilities.SPRedirectFlags.RelativeToLayoutsPage, System.Web.HttpContext.Current);
+                    }
+                    break;
+                default:
+                    lblError.Text = "Could not load control: " + control;
+                    break;
+            }
         }
 
         private void RedirectTo(SPListItem li, string control)
