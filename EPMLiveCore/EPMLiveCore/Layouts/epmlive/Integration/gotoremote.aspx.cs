44ec947849ead6b47d7686be3a992329e16d1117
Jason Hughes <jhughes@lmrsolutions.com>
2013-10-11 16:29:45 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C3047

--- EPMLiveCore/EPMLiveCore/Layouts/epmlive/Integration/gotoremote.aspx.cs
+++ EPMLiveCore/EPMLiveCore/Layouts/epmlive/Integration/gotoremote.aspx.cs
@@ -89,7 +89,7 @@ namespace EPMLiveCore.Layouts.epmlive.Integration
             {
                 if (bIframe)
                 {
-                    error = "<iframe src=\"" + url + "\" id=\"frmRemote\" style=\"width:100%;height:100%\">";
+                    error = "<iframe src=\"" + url + "\" id=\"frmRemote\" style=\"width:100%;height:600px\">";
                 }
                 else
                 {
