b35b1f0c9a36120311d50ebe69fb4df386c4fedd
David Kuo <dkuo@lmrsolutions.com>
2013-10-18 22:52:21 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C3197

--- EPMLiveCore/EPMLiveCore/WorkspaceEvents.cs
+++ EPMLiveCore/EPMLiveCore/WorkspaceEvents.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.SharePoint;
+﻿using EPMLiveCore.API;
+using EPMLiveCore.Controls.Navigation.Providers;
+using EPMLiveCore.Infrastructure;
+using EPMLiveCore.ReportingProxy;
+using Microsoft.SharePoint;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +13,39 @@ namespace EPMLiveCore
 {
     public class WorkspaceEvents : SPWebEventReceiver
     {
-        public override void WebDeleted(SPWebEventProperties properties)
+        public override void WebDeleting(SPWebEventProperties properties)
         {
             // will implement later
+            try
+            {
+                var sParentItem = properties.Web.AllProperties["ParentItem"];
+                var sColl = sParentItem.ToString().Split(new string[] {"^^"}, StringSplitOptions.RemoveEmptyEntries);
+                var webid = sColl[0];
+                var listid = sColl[1];
+                var itemid = sColl[2];
+                var isitem = (new Guid(listid) == Guid.Empty) ? "false" : "true";
+                var xml = 
+                    "<Data><Param key=\"SiteId\">" + properties.SiteId + "</Param>" + 
+                    "<Param key=\"WebId\">" + properties.Web.ID + "</Param>" +
+                    "<Param key=\"ItemId\">" + itemid + "</Param>" +
+                    "<Param key=\"FString\">" + properties.Web.ServerRelativeUrl + "</Param>" +
+                    "<Param key=\"Type\">4</Param>" +
+                    "<Param key=\"UserId\">" + properties.Web.CurrentUser.ID + "</Param>" +
+                    "<Param key=\"IsItem\">" + isitem + "</Param></Data>";
+
+                var data = new AnalyticsData(xml, AnalyticsType.FavoriteWorkspace, AnalyticsAction.Delete);
+                var qExec = new QueryExecutor(SPContext.Current.Web);
+                qExec.ExecuteEpmLiveQuery(
+                    FRFQueryFactory.GetQuery(data),
+                    FRFQueryParamFactory.GetParam(data));
+
+                CacheStore.Current.RemoveSafely(properties.Web.Url, CacheStoreCategory.Navigation);
+            }
+            catch
+            {
+            }
         }
+
+       
     }
 }
