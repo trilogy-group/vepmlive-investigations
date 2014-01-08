cb49ced98708f2d3687a9ece2b7493afae186a81
Dhaval Upadhayay <dhavalu@cybage.com>
2014-01-08 10:45:57 +0000

    MyWorkSummaryWebPart added
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013;C3889

---
+++ EPMLiveWebParts/EPMLiveWebParts/MyWorkSummary/MyWorkSummary.ascx
@@ -0,0 +1,58 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MyWorkSummary.ascx.cs" Inherits="EPMLiveWebParts.MyWorkSummary.MyWorkSummary" %>
+
+<style type="text/css">
+    .pipeSeperator {
+        float: left;
+        font-size: large;
+    }
+
+    .listMainDiv {
+        float: left;
+        padding: 5px;
+        margin-right: 5px;
+    }
+</style>
+
+
+<script type="text/javascript">
+
+    $(function () {
+        fillMyWorkSummaryData();
+    });
+
+    function fillMyWorkSummaryData() {
+        if (dataXml != '') {
+
+            $("#<%=myWorkSummaryItemsDiv.ClientID%>").hide();
+            $('#divMyWorkSummaryLoad').show();
+
+            EPMLiveCore.WorkEngineAPI.Execute("GetWorkItemsByWorkType", dataXml, function (response) {
+                var divHTML = response.toString().replace("<Result Status=\"0\">", "").replace("</Result>", "");
+                $("#<%=myWorkSummaryItemsDiv.ClientID%>").html("");
+                $("#<%=myWorkSummaryItemsDiv.ClientID%>").html(divHTML);
+
+                $('#divMyWorkSummaryLoad').hide();
+                $("#<%=myWorkSummaryItemsDiv.ClientID%>").show();
+
+            });
+        }
+    }
+
+    function displayMyWorkItemsByFilter() {
+        alert("This functionality is under development, sorry for inconvenience.");
+    }
+
+</script>
+
+<div id="divMyWorkSummaryLoad" style="align-content: center">
+    <img src="../_layouts/15/epmlive/images/mywork/loading16.gif" />
+</div>
+<div id="myWorkSummaryItemsDiv" runat="server" style="float: left;" />
+
---
+++ EPMLiveWebParts/EPMLiveWebParts/MyWorkSummary/MyWorkSummary.ascx.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml;
+
+namespace EPMLiveWebParts.MyWorkSummary
+{
+    [ToolboxItemAttribute(false)]
+    public partial class MyWorkSummary : WebPart
+    {
+        public MyWorkSummary()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected override void CreateChildControls()
+        {
+            SPWeb oWeb = SPContext.Current.Web;
+
+            string serviceUrl = ((SPContext.Current.Web.ServerRelativeUrl == "/")
+                                     ? ""
+                                     : SPContext.Current.Web.ServerRelativeUrl) + "/_vti_bin/Workengine.asmx";
+
+            ScriptManager scriptManager = ScriptManager.GetCurrent(Page);
+
+            if (scriptManager != null)
+            {
+                scriptManager.Services.Add(new ServiceReference(serviceUrl));
+            }
+            else
+            {
+                scriptManager = new ScriptManager();
+                scriptManager.Services.Add(new ServiceReference(serviceUrl));
+
+                Page.Form.Controls.Add(scriptManager);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PrepareMyWorkSummaryListsString();
+        }
+
+        protected void PrepareMyWorkSummaryListsString()
+        {
+            StringBuilder mainDivHtml = new StringBuilder();            
+            mainDivHtml.Append("<MyWorkItemsbyWorkType>");
+            mainDivHtml.Append("<SiteUrl>" + SPContext.Current.Site.Url + "</SiteUrl>");
+            mainDivHtml.Append("<SiteID>" + Convert.ToString(SPContext.Current.Site.ID) + "</SiteID>");
+            mainDivHtml.Append("<WebID>" + Convert.ToString(SPContext.Current.Web.ID) + "</WebID>");
+            mainDivHtml.Append("<CurrentUser>" + Convert.ToString(SPContext.Current.Web.CurrentUser.Name) + "</CurrentUser>");
+            mainDivHtml.Append("</MyWorkItemsbyWorkType>");            
+
+            this.Page.ClientScript.RegisterClientScriptBlock(this.GetType(), "dataXml", "<script type=\"text/javascript\">var dataXml='" + mainDivHtml.ToString() + "';</script>");
+
+        }
+    }
+}
