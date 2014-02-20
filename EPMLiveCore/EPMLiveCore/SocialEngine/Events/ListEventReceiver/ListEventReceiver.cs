41ce5ce28d704af523811586c6a778d3b18f0e9a
Munjal Patel <EPM\mpatel>
2014-02-20 23:31:20 +0000

    Adding Social Stream base events
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4067

---
+++ EPMLiveCore/EPMLiveCore/SocialEngine/Events/ListEventReceiver/ListEventReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace EPMLiveCore.SocialEngine.Events.ListEventReceiver
+{
+    /// <summary>
+    /// List Events
+    /// </summary>
+    public class ListEventReceiver : SPListEventReceiver
+    {
+        /// <summary>
+        /// A list was added.
+        /// </summary>
+        public override void ListAdded(SPListEventProperties properties)
+        {
+            base.ListAdded(properties);
+        }
+
+        /// <summary>
+        /// A list was deleted.
+        /// </summary>
+        public override void ListDeleted(SPListEventProperties properties)
+        {
+            base.ListDeleted(properties);
+        }
+
+
+    }
+}
---
+++ EPMLiveCore/EPMLiveCore/SocialEngine/Events/WebEventReceiver/WebEventReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace EPMLiveCore.SocialEngine.Events.WebEventReceiver
+{
+    /// <summary>
+    /// Web Events
+    /// </summary>
+    public class WebEventReceiver : SPWebEventReceiver
+    {
+        /// <summary>
+        /// A site was deleted.
+        /// </summary>
+        public override void WebDeleted(SPWebEventProperties properties)
+        {
+            base.WebDeleted(properties);
+        }
+
+        /// <summary>
+        /// A site was provisioned.
+        /// </summary>
+        public override void WebProvisioned(SPWebEventProperties properties)
+        {
+            base.WebProvisioned(properties);
+        }
+
+
+    }
+}
