44e45a9ebab159f65c8b6ed068c0692646c61258
Munjal Patel <EPM\mpatel>
2014-03-02 00:08:55 +0000

    Blank Social Stream Web Part
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4106

---
+++ EPMLiveWebParts/EPMLiveWebParts/CONTROLTEMPLATES/SocialStream.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.UI;
+using EPMLiveCore;
+using EPMLiveCore.Infrastructure;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace EPMLiveWebParts.CONTROLTEMPLATES
+{
+    public partial class SocialStream : UserControl
+    {
+        #region Methods (1) 
+
+        // Protected Methods (1) 
+
+        protected void Page_Load(object sender, EventArgs e) { }
+
+        #endregion Methods 
+
+        #region Overrides of Control
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            string layoutPath = SPContext.Current.Web.SafeServerRelativeUrl() + "/_layouts/15/epmlive/";
+
+            SPPageContentManager.RegisterStyleFile(layoutPath + "stylesheets/SocialStream.min.css");
+
+            EPMLiveScriptManager.RegisterScript(Page, new[]
+            {
+                "libraries/jquery.min", "libraries/handlebars-v1.3.0", "libraries/ember.min", "@EPMLive.SocialStream"
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ EPMLiveWebParts/EPMLiveWebParts/SocialStream/SocialStream.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using WebPart = Microsoft.SharePoint.WebPartPages.WebPart;
+
+namespace EPMLiveWebParts
+{
+    [ToolboxItem(false)]
+    public class SocialStream : WebPart
+    {
+        #region Methods (1) 
+
+        // Protected Methods (1) 
+
+        protected override void CreateChildControls()
+        {
+            var control = (CONTROLTEMPLATES.SocialStream) Page.LoadControl(@"~/_CONTROLTEMPLATES/SocialStream.ascx");
+            Controls.Add(control);
+        }
+
+        #endregion Methods 
+
+        #region Overrides of WebPart
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            ChromeType = PartChromeType.None;
+        }
+
+        #endregion
+    }
+}
