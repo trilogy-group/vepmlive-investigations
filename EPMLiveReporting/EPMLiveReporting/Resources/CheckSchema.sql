6d06f117ee62598e3871a53cab9e9c79eb277907
Munjal Patel <EPM\mpatel>
2014-03-12 19:07:25 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4202

--- EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
+++ EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
@@ -42,3 +42,155 @@ BEGIN
 	                                    
 END
 
+---------------TABLE: SS_Activities----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Activities')
+BEGIN
+	PRINT 'Creating Table SS_Activities'
+	CREATE TABLE [dbo].[SS_Activities](
+		[Id] [uniqueidentifier] NOT NULL,
+		[Data] [nvarchar](max) NULL,
+		[Kind] [int] NOT NULL,
+		[Date] [datetime2](7) NULL,
+		[UserId] [int] NOT NULL,
+		[ThreadId] [uniqueidentifier] NOT NULL,
+		[MassOperation] [bit] NULL,
+	 CONSTRAINT [PK_SS_Activities] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+	ALTER TABLE [dbo].[SS_Activities] ADD  CONSTRAINT [DF_SS_Activities_Id]  DEFAULT (newid()) FOR [Id]
+
+	ALTER TABLE [dbo].[SS_Activities] ADD  CONSTRAINT [DF_SS_Activities_Date]  DEFAULT (getdate()) FOR [Date]
+
+	ALTER TABLE [dbo].[SS_Activities] ADD  CONSTRAINT [DF_SS_Activities_MassOperation]  DEFAULT ((0)) FOR [MassOperation]
+END
+
+---------------TABLE: SS_Logs----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Logs')
+BEGIN
+	PRINT 'Creating Table SS_Logs'
+	CREATE TABLE [dbo].[SS_Logs](
+		[Id] [uniqueidentifier] NOT NULL,
+		[Message] [nvarchar](max) NULL,
+		[StackTrace] [nvarchar](max) NULL,
+		[Details] [nvarchar](max) NULL,
+		[Kind] [bit] NOT NULL,
+		[WebId] [uniqueidentifier] NULL,
+		[UserId] [int] NULL,
+		[DateTime] [datetime2](7) NULL
+	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+	ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_Id]  DEFAULT (newid()) FOR [Id]
+
+	ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_Kind]  DEFAULT ((0)) FOR [Kind]
+
+	ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_DateTime]  DEFAULT (getdate()) FOR [DateTime]
+END
+
+---------------TABLE: SS_Streams----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Streams')
+BEGIN
+	PRINT 'Creating Table SS_Streams'
+	CREATE TABLE [dbo].[SS_Streams](
+		[Id] [uniqueidentifier] NOT NULL,
+		[WebId] [uniqueidentifier] NOT NULL,
+		[Title] [nvarchar](max) NULL,
+		[ListId] [uniqueidentifier] NULL,
+		[ItemId] [int] NULL,
+	 CONSTRAINT [PK_SS_Streams] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+	ALTER TABLE [dbo].[SS_Streams] ADD  CONSTRAINT [DF_SS_Streams_Id]  DEFAULT (newid()) FOR [Id]
+END
+
+---------------TABLE: SS_Streams_Threads----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Streams_Threads')
+BEGIN
+	PRINT 'Creating Table SS_Streams_Threads'
+	CREATE TABLE [dbo].[SS_Streams_Threads](
+		[StreamId] [uniqueidentifier] NOT NULL,
+		[ThreadId] [uniqueidentifier] NOT NULL,
+	 CONSTRAINT [UK_SS_Streams_Threads] UNIQUE NONCLUSTERED 
+	(
+		[StreamId] ASC,
+		[ThreadId] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+END
+
+---------------TABLE: SS_StreamUsers----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_StreamUsers')
+BEGIN
+	PRINT 'Creating Table SS_StreamUsers'
+	CREATE TABLE [dbo].[SS_StreamUsers](
+		[StreamId] [uniqueidentifier] NOT NULL,
+		[UserId] [int] NOT NULL
+	) ON [PRIMARY]
+END
+
+---------------TABLE: SS_Threads----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Threads')
+BEGIN
+	PRINT 'Creating Table SS_Threads'
+	CREATE TABLE [dbo].[SS_Threads](
+		[Id] [uniqueidentifier] NOT NULL,
+		[Title] [nvarchar](max) NOT NULL,
+		[URL] [nvarchar](max) NULL,
+		[Kind] [int] NOT NULL,
+		[LastActivityDateTime] [datetime2](7) NULL,
+		[WebId] [uniqueidentifier] NULL,
+		[ListId] [uniqueidentifier] NULL,
+		[ItemId] [int] NULL,
+		[Deleted] [bit] NULL,
+	 CONSTRAINT [PK_SS_Threads] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+	ALTER TABLE [dbo].[SS_Threads] ADD  CONSTRAINT [DF_SS_Threads_Deleted]  DEFAULT ((0)) FOR [Deleted]
+END
+
+---------------TABLE: SS_ThreadUsers----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_ThreadUsers')
+BEGIN
+	PRINT 'Creating Table SS_ThreadUsers'
+	CREATE TABLE [dbo].[SS_ThreadUsers](
+		[ThreadId] [uniqueidentifier] NOT NULL,
+		[UserId] [int] NOT NULL,
+		[Role] [int] NOT NULL
+	) ON [PRIMARY]
+
+	ALTER TABLE [dbo].[SS_ThreadUsers] ADD  CONSTRAINT [DF_SS_ThreadUsers_Role]  DEFAULT ((0)) FOR [Role]
+END
+
+---------------TABLE: SS_AssociatedThreads----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_AssociatedThreads')
+BEGIN
+	PRINT 'Creating Table SS_AssociatedThreads'
+	CREATE TABLE [dbo].[SS_AssociatedThreads](
+		[Id] [uniqueidentifier] NOT NULL,
+		[ThreadId] [uniqueidentifier] NOT NULL,
+		[ListId] [uniqueidentifier] NOT NULL,
+		[ItemId] [int] NOT NULL,
+	 CONSTRAINT [PK_SS_AssociatedThreads] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+
+	ALTER TABLE [dbo].[SS_AssociatedThreads] ADD  CONSTRAINT [DF_SS_AssociatedThreads_Id]  DEFAULT (newid()) FOR [Id]
+END
