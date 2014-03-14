4e8bdd3d51e2e0d2716cf13c04ad8f704b149d83
Munjal Patel <EPM\mpatel>
2014-03-14 18:28:37 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4232

--- EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
+++ EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
@@ -47,133 +47,34 @@ END
 IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Activities')
 BEGIN
 	PRINT 'Creating Table SS_Activities'
-	CREATE TABLE [dbo].[SS_Activities](
-		[Id] [uniqueidentifier] NOT NULL,
-		[Data] [nvarchar](max) NULL,
-		[Kind] [int] NOT NULL,
-		[Date] [datetime2](7) NULL,
-		[UserId] [int] NOT NULL,
-		[ThreadId] [uniqueidentifier] NOT NULL,
-		[MassOperation] [bit] NULL,
-	 CONSTRAINT [PK_SS_Activities] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+	
+	CREATE TABLE [dbo].[SS_Activities] (
+		[Id]            UNIQUEIDENTIFIER CONSTRAINT [DF_SS_Activities_Id] DEFAULT (newid()) NOT NULL,
+		[ActivityKey]   NVARCHAR (256)   NULL,
+		[Data]          NVARCHAR (MAX)   NULL,
+		[Kind]          INT              NOT NULL,
+		[Date]          DATETIME2 (7)    CONSTRAINT [DF_SS_Activities_Date] DEFAULT (getdate()) NULL,
+		[UserId]        INT              NOT NULL,
+		[ThreadId]      UNIQUEIDENTIFIER NOT NULL,
+		[MassOperation] BIT              CONSTRAINT [DF_SS_Activities_MassOperation] DEFAULT ((0)) NULL,
+		CONSTRAINT [PK_SS_Activities] PRIMARY KEY CLUSTERED ([Id] ASC),
+		CONSTRAINT [UK_SS_Activities] UNIQUE NONCLUSTERED ([ThreadId] ASC, [Kind] ASC, [UserId] ASC, [Date] ASC)
+	);
 
-	ALTER TABLE [dbo].[SS_Activities] ADD  CONSTRAINT [DF_SS_Activities_Id]  DEFAULT (newid()) FOR [Id]
+	EXEC sp_executesql N'
+	CREATE TRIGGER [dbo].[UpdateThreadLastActivity] 
+	   ON  dbo.SS_Activities 
+	   AFTER INSERT,UPDATE
+	AS 
+	BEGIN
+		SET NOCOUNT ON;
+		IF (SELECT MassOperation FROM INSERTED) = 0
+		BEGIN
+			UPDATE SS_Threads SET LastActivityDateTime = (SELECT Date FROM INSERTED) WHERE Id = (SELECT ThreadId FROM INSERTED)
+		END
+	END'
 
-	ALTER TABLE [dbo].[SS_Activities] ADD  CONSTRAINT [DF_SS_Activities_Date]  DEFAULT (getdate()) FOR [Date]
-
-	ALTER TABLE [dbo].[SS_Activities] ADD  CONSTRAINT [DF_SS_Activities_MassOperation]  DEFAULT ((0)) FOR [MassOperation]
-END
-
----------------TABLE: SS_Logs----------------------
-
-IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Logs')
-BEGIN
-	PRINT 'Creating Table SS_Logs'
-	CREATE TABLE [dbo].[SS_Logs](
-		[Id] [uniqueidentifier] NOT NULL,
-		[Message] [nvarchar](max) NULL,
-		[StackTrace] [nvarchar](max) NULL,
-		[Details] [nvarchar](max) NULL,
-		[Kind] [bit] NOT NULL,
-		[WebId] [uniqueidentifier] NULL,
-		[UserId] [int] NULL,
-		[DateTime] [datetime2](7) NULL
-	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-	ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_Id]  DEFAULT (newid()) FOR [Id]
-
-	ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_Kind]  DEFAULT ((0)) FOR [Kind]
-
-	ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_DateTime]  DEFAULT (getdate()) FOR [DateTime]
-END
-
----------------TABLE: SS_Streams----------------------
-
-IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Streams')
-BEGIN
-	PRINT 'Creating Table SS_Streams'
-	CREATE TABLE [dbo].[SS_Streams](
-		[Id] [uniqueidentifier] NOT NULL,
-		[WebId] [uniqueidentifier] NOT NULL,
-		[Title] [nvarchar](max) NULL,
-		[ListId] [uniqueidentifier] NULL,
-		[ItemId] [int] NULL,
-	 CONSTRAINT [PK_SS_Streams] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-	ALTER TABLE [dbo].[SS_Streams] ADD  CONSTRAINT [DF_SS_Streams_Id]  DEFAULT (newid()) FOR [Id]
-END
-
----------------TABLE: SS_Streams_Threads----------------------
-
-IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Streams_Threads')
-BEGIN
-	PRINT 'Creating Table SS_Streams_Threads'
-	CREATE TABLE [dbo].[SS_Streams_Threads](
-		[StreamId] [uniqueidentifier] NOT NULL,
-		[ThreadId] [uniqueidentifier] NOT NULL,
-	 CONSTRAINT [UK_SS_Streams_Threads] UNIQUE NONCLUSTERED 
-	(
-		[StreamId] ASC,
-		[ThreadId] ASC
-	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-	) ON [PRIMARY]
-END
-
----------------TABLE: SS_StreamUsers----------------------
-
-IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_StreamUsers')
-BEGIN
-	PRINT 'Creating Table SS_StreamUsers'
-	CREATE TABLE [dbo].[SS_StreamUsers](
-		[StreamId] [uniqueidentifier] NOT NULL,
-		[UserId] [int] NOT NULL
-	) ON [PRIMARY]
-END
-
----------------TABLE: SS_Threads----------------------
-
-IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Threads')
-BEGIN
-	PRINT 'Creating Table SS_Threads'
-	CREATE TABLE [dbo].[SS_Threads](
-		[Id] [uniqueidentifier] NOT NULL,
-		[Title] [nvarchar](max) NOT NULL,
-		[URL] [nvarchar](max) NULL,
-		[Kind] [int] NOT NULL,
-		[LastActivityDateTime] [datetime2](7) NULL,
-		[WebId] [uniqueidentifier] NULL,
-		[ListId] [uniqueidentifier] NULL,
-		[ItemId] [int] NULL,
-		[Deleted] [bit] NULL,
-	 CONSTRAINT [PK_SS_Threads] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-	ALTER TABLE [dbo].[SS_Threads] ADD  CONSTRAINT [DF_SS_Threads_Deleted]  DEFAULT ((0)) FOR [Deleted]
-END
-
----------------TABLE: SS_ThreadUsers----------------------
-
-IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_ThreadUsers')
-BEGIN
-	PRINT 'Creating Table SS_ThreadUsers'
-	CREATE TABLE [dbo].[SS_ThreadUsers](
-		[ThreadId] [uniqueidentifier] NOT NULL,
-		[UserId] [int] NOT NULL,
-		[Role] [int] NOT NULL
-	) ON [PRIMARY]
-
-	ALTER TABLE [dbo].[SS_ThreadUsers] ADD  CONSTRAINT [DF_SS_ThreadUsers_Role]  DEFAULT ((0)) FOR [Role]
+	EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Created = 0, Updated = 1, Deleted = 2, Bulk Operation = 3, Comment = 4', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SS_Activities', @level2type = N'COLUMN', @level2name = N'Kind';
 END
 
 ---------------TABLE: SS_AssociatedThreads----------------------
@@ -181,16 +82,120 @@ END
 IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_AssociatedThreads')
 BEGIN
 	PRINT 'Creating Table SS_AssociatedThreads'
-	CREATE TABLE [dbo].[SS_AssociatedThreads](
-		[Id] [uniqueidentifier] NOT NULL,
-		[ThreadId] [uniqueidentifier] NOT NULL,
-		[ListId] [uniqueidentifier] NOT NULL,
-		[ItemId] [int] NOT NULL,
-	 CONSTRAINT [PK_SS_AssociatedThreads] PRIMARY KEY CLUSTERED 
-	(
-		[Id] ASC
-	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-	) ON [PRIMARY]
 
-	ALTER TABLE [dbo].[SS_AssociatedThreads] ADD  CONSTRAINT [DF_SS_AssociatedThreads_Id]  DEFAULT (newid()) FOR [Id]
+	CREATE TABLE [dbo].[SS_AssociatedThreads] (
+		[Id]       UNIQUEIDENTIFIER CONSTRAINT [DF_SS_AssociatedThreads_Id] DEFAULT (newid()) NOT NULL,
+		[ThreadId] UNIQUEIDENTIFIER NOT NULL,
+		[ListId]   UNIQUEIDENTIFIER NOT NULL,
+		[ItemId]   INT              NOT NULL,
+		CONSTRAINT [PK_SS_AssociatedThreads] PRIMARY KEY CLUSTERED ([Id] ASC)
+	);
+END
+
+---------------TABLE: SS_Logs----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Logs')
+BEGIN
+	PRINT 'Creating Table SS_Logs'
+	CREATE TABLE [dbo].[SS_Logs] (
+		[Id]         UNIQUEIDENTIFIER CONSTRAINT [DF_SS_Logs_Id] DEFAULT (newid()) NOT NULL,
+		[Message]    NVARCHAR (MAX)   NULL,
+		[StackTrace] NVARCHAR (MAX)   NULL,
+		[Details]    NVARCHAR (MAX)   NULL,
+		[Kind]       BIT              CONSTRAINT [DF_SS_Logs_Kind] DEFAULT ((0)) NOT NULL,
+		[WebId]      UNIQUEIDENTIFIER NULL,
+		[UserId]     INT              NULL,
+		[DateTime]   DATETIME2 (7)    CONSTRAINT [DF_SS_Logs_DateTime] DEFAULT (getdate()) NULL
+	);
+END
+
+---------------TABLE: SS_Streams----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Streams')
+BEGIN
+	PRINT 'Creating Table SS_Streams'
+	CREATE TABLE [dbo].[SS_Streams] (
+		[Id]     UNIQUEIDENTIFIER CONSTRAINT [DF_SS_Streams_Id] DEFAULT (newid()) NOT NULL,
+		[WebId]  UNIQUEIDENTIFIER NOT NULL,
+		[Title]  NVARCHAR (MAX)   NULL,
+		[ListId] UNIQUEIDENTIFIER NULL,
+		[ItemId] INT              NULL,
+		CONSTRAINT [PK_SS_Streams] PRIMARY KEY CLUSTERED ([Id] ASC)
+	);
+END
+
+---------------TABLE: SS_Streams_Threads----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Streams_Threads')
+BEGIN
+	PRINT 'Creating Table SS_Streams_Threads'
+	CREATE TABLE [dbo].[SS_Streams_Threads] (
+		[StreamId] UNIQUEIDENTIFIER NOT NULL,
+		[ThreadId] UNIQUEIDENTIFIER NOT NULL,
+		CONSTRAINT [UK_SS_Streams_Threads] UNIQUE NONCLUSTERED ([StreamId] ASC, [ThreadId] ASC)
+	);
+END
+
+---------------TABLE: SS_StreamUsers----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_StreamUsers')
+BEGIN
+	PRINT 'Creating Table SS_StreamUsers'
+	CREATE TABLE [dbo].[SS_StreamUsers] (
+		[StreamId] UNIQUEIDENTIFIER NOT NULL,
+		[UserId]   INT              NOT NULL
+	);
+END
+
+---------------TABLE: SS_Threads----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Threads')
+BEGIN
+	PRINT 'Creating Table SS_Threads'
+	CREATE TABLE [dbo].[SS_Threads] (
+		[Id]                   UNIQUEIDENTIFIER NOT NULL,
+		[Title]                NVARCHAR (256)   NOT NULL,
+		[URL]                  NVARCHAR (MAX)   NULL,
+		[Kind]                 INT              NOT NULL,
+		[LastActivityDateTime] DATETIME2 (7)    NULL,
+		[WebId]                UNIQUEIDENTIFIER NULL,
+		[ListId]               UNIQUEIDENTIFIER NULL,
+		[ItemId]               INT              NULL,
+		[Deleted]              BIT              CONSTRAINT [DF_SS_Threads_Deleted] DEFAULT ((0)) NULL,
+		CONSTRAINT [PK_SS_Threads] PRIMARY KEY CLUSTERED ([Id] ASC),
+		CONSTRAINT [UK_SS_Threads] UNIQUE NONCLUSTERED ([WebId] ASC, [ListId] ASC, [ItemId] ASC, [Kind] ASC)
+	);
+
+	EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Workspace = 0, List = 1, List Item = 2', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SS_Threads', @level2type = N'COLUMN', @level2name = N'Kind';
+END
+
+---------------TABLE: SS_ThreadUsers----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_ThreadUsers')
+BEGIN
+	PRINT 'Creating Table SS_ThreadUsers'
+	CREATE TABLE [dbo].[SS_ThreadUsers] (
+		[ThreadId] UNIQUEIDENTIFIER NOT NULL,
+		[UserId]   INT              NOT NULL,
+		[Role]     INT              CONSTRAINT [DF_SS_ThreadUsers_Role] DEFAULT ((0)) NOT NULL,
+		CONSTRAINT [UK_SS_ThreadUsers] UNIQUE NONCLUSTERED ([ThreadId] ASC, [UserId] ASC)
+	);
+
+	EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Sum of all roles a user belongs to. None = 0, Author = 1, Commenter = 2, Assignee = 4, Liker = 8', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SS_ThreadUsers', @level2type = N'COLUMN', @level2name = N'Role';
+END
+
+---------------TABLE: SS_AssociatedThreads----------------------
+
+IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME = 'SS_Transactions')
+BEGIN
+	PRINT 'Creating Table SS_Transactions'
+	CREATE TABLE [dbo].[SS_Transactions] (
+		[Id]        UNIQUEIDENTIFIER CONSTRAINT [DF_SS_Transections_Id] DEFAULT (newid()) NOT NULL,
+		[WebId]     UNIQUEIDENTIFIER NOT NULL,
+		[ListId]    UNIQUEIDENTIFIER NOT NULL,
+		[ItemId]    INT              NOT NULL,
+		[Component] NVARCHAR (256)   NOT NULL,
+		[Time]      DATETIME2 (7)    CONSTRAINT [DF_SS_Transactions_Time] DEFAULT (getdate()) NOT NULL,
+		CONSTRAINT [PK_SS_Transactions] PRIMARY KEY CLUSTERED ([Id] ASC)
+	);
 END
