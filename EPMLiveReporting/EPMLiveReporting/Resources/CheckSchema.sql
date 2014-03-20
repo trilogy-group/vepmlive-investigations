d205ae0bb02bd4a42b5807030b05214aba9c6067
Munjal Patel <EPM\mpatel>
2014-03-20 19:32:05 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4327

--- EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
+++ EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
@@ -160,17 +160,17 @@ IF NOT EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.tables WHERE TABLE_NAME
 BEGIN
 	PRINT 'Creating Table SS_Threads'
 	CREATE TABLE [dbo].[SS_Threads] (
-		[Id]                   UNIQUEIDENTIFIER NOT NULL,
-		[Title]                NVARCHAR (256)   NOT NULL,
-		[URL]                  NVARCHAR (MAX)   NULL,
-		[Kind]                 INT              NOT NULL,
-		[LastActivityDateTime] DATETIME2 (7)    NULL,
-		[WebId]                UNIQUEIDENTIFIER NULL,
-		[ListId]               UNIQUEIDENTIFIER NULL,
-		[ItemId]               INT              NULL,
-		[Deleted]              BIT              CONSTRAINT [DF_SS_Threads_Deleted] DEFAULT ((0)) NULL,
-		CONSTRAINT [PK_SS_Threads] PRIMARY KEY CLUSTERED ([Id] ASC),
-		CONSTRAINT [UK_SS_Threads] UNIQUE NONCLUSTERED ([WebId] ASC, [ListId] ASC, [ItemId] ASC, [Kind] ASC)
+		[Id]                    UNIQUEIDENTIFIER NOT NULL,
+		[Title]                 NVARCHAR (256)   NOT NULL,
+		[URL]                   NVARCHAR (MAX)   NULL,
+		[Kind]                  INT              NOT NULL,
+		[LastActivityDateTime]  DATETIME2 (7)    NULL,
+		[FirstActivityDateTime] DATETIME2 (7)    NOT NULL,
+		[WebId]                 UNIQUEIDENTIFIER NULL,
+		[ListId]                UNIQUEIDENTIFIER NULL,
+		[ItemId]                INT              NULL,
+		[Deleted]               BIT              CONSTRAINT [DF_SS_Threads_Deleted] DEFAULT ((0)) NULL,
+		CONSTRAINT [PK_SS_Threads] PRIMARY KEY CLUSTERED ([Id] ASC)
 	);
 
 	EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Workspace = 0, List = 1, List Item = 2', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SS_Threads', @level2type = N'COLUMN', @level2name = N'Kind';
@@ -223,3 +223,95 @@ ALTER TABLE RPTList ALTER COLUMN ListName NVARCHAR(500) NOT NULL
 ALTER TABLE RPTList ALTER COLUMN TableName NVARCHAR(500) NOT NULL
 ALTER TABLE RPTList ALTER COLUMN TableNameSnapshot NVARCHAR(500) NOT NULL
 
+---------------FUNCTION: fnCheckUserAccess----------------------
+
+PRINT 'Creating function fnCheckUserAccess'
+
+IF NOT  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fnCheckUserAccess]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+BEGIN
+	EXEC sp_executesql N'
+	CREATE FUNCTION [dbo].[fnCheckUserAccess]
+	(
+		@UserId INT,
+		@WebId  UNIQUEIDENTIFIER,
+		@ListId UNIQUEIDENTIFIER = NULL,
+		@ItemId INT = NULL
+	)
+	RETURNS BIT
+	AS
+	BEGIN
+		IF @UserId = 1073741823 RETURN 1
+	
+		IF @ItemId IS NULL
+		BEGIN
+			IF (@WebId IN (SELECT WEBID FROM dbo.RPTWEBGROUPS WHERE (SECTYPE = 1) AND (GROUPID IN 
+					(SELECT GroupId FROM (SELECT GROUPID FROM dbo.RPTGROUPUSER WHERE USERID = @UserId) AS Groups)) 
+					OR (SECTYPE = 0) AND (GROUPID = @UserId))) RETURN 1
+			RETURN 0
+		END
+	
+		IF (@ItemId IN (SELECT ITEMID FROM dbo.RPTITEMGROUPS WHERE (SECTYPE = 1) AND (GROUPID IN 
+				(SELECT GroupId FROM (SELECT GROUPID FROM dbo.RPTGROUPUSER WHERE USERID = @UserId) AS Groups)) 
+				OR (SECTYPE = 0) AND (GROUPID = @UserId) AND (LISTID = @ListId))) RETURN 1
+		RETURN 0
+	END'
+END
+
+---------------SP: fnCheckUserAccess----------------------
+
+PRINT 'Creating SP SS_GetActivities'
+
+IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SS_GetActivities]') AND type in (N'P', N'PC'))
+BEGIN
+	EXEC sp_executesql N'
+	CREATE PROCEDURE [dbo].[SS_GetActivities] 
+		@UserId		INT,
+		@WebUrl		NVARCHAR(MAX),
+		@MinDate	DATETIME2 = NULL,
+		@MaxDate	DATETIME2 = NULL,
+		@Page		INT = 1,
+		@Limit		INT = 1000000
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+
+		DECLARE @Start INT, @End INT, @WebUrlSuffix CHAR
+	
+		SET @Start = (@Page - 1) * @Limit
+		SET @End =   (@Page * @Limit + 1)
+	
+		SET @WebUrlSuffix = ''/''
+	
+		IF @WebUrl = ''/'' SET @WebUrlSuffix = ''''
+	
+		IF @MinDate IS NULL SET @MinDate = CAST(''1753-1-1'' AS DATETIME2)
+		IF @MaxDate IS NULL SET @MaxDate = GETDATE();
+	
+		WITH Result AS (
+			SELECT ROW_NUMBER() OVER (ORDER BY dbo.SS_Threads.LastActivityDateTime DESC, dbo.SS_Activities.Date DESC) AS RowNum,
+					dbo.SS_Activities.Id AS ActivityId, dbo.SS_Activities.ActivityKey, dbo.SS_Activities.Data AS ActivityData, dbo.SS_Activities.Kind AS ActivityKind, 
+						  dbo.SS_Activities.Date AS ActivityDate, dbo.SS_Activities.MassOperation AS IsMassOperation, dbo.SS_Threads.Id AS ThreadId, dbo.SS_Threads.Title AS ThreadTitle, 
+						  dbo.SS_Threads.URL AS ThreadURL, dbo.SS_Threads.Kind AS ThreadKind, dbo.SS_Threads.Deleted AS ThreadDeleted, 
+						  dbo.SS_Threads.LastActivityDateTime AS ThreadLastActivityOn, dbo.SS_Threads.FirstActivityDateTime AS ThreadFirstActivityOn, 
+						  dbo.SS_Threads.WebId, dbo.RPTWeb.WebTitle, dbo.RPTWeb.WebUrl, dbo.SS_Threads.ListId, 
+						  dbo.RPTList.ListName, dbo.ReportListIds.Icon AS ListIcon, dbo.SS_Threads.ItemId, dbo.SS_Activities.UserId, 
+						  dbo.LSTUserInformationList.Title AS UserDisplayName, dbo.LSTUserInformationList.Name AS UserAccount, dbo.LSTUserInformationList.Picture AS UserPicture, 
+						  dbo.fnCheckUserAccess(@UserId, dbo.SS_Threads.WebId, dbo.SS_Threads.ListId, dbo.SS_Threads.ItemId) AS HasAccess
+			FROM	dbo.SS_Activities INNER JOIN
+					dbo.SS_Threads ON dbo.SS_Activities.ThreadId = dbo.SS_Threads.Id INNER JOIN
+						  dbo.RPTWeb ON dbo.SS_Threads.WebId = dbo.RPTWeb.WebId INNER JOIN
+						  dbo.LSTUserInformationList ON dbo.SS_Activities.UserId = dbo.LSTUserInformationList.ID LEFT OUTER JOIN
+						  dbo.ReportListIds ON dbo.SS_Threads.ListId = dbo.ReportListIds.Id LEFT OUTER JOIN
+						  dbo.RPTList ON dbo.SS_Threads.ListId = dbo.RPTList.RPTListId
+			WHERE   (NOT (dbo.SS_Threads.ListId IS NOT NULL)) OR (NOT (dbo.RPTList.ListName IS NULL))
+		) SELECT TOP (@End - 1) 
+			ActivityId, ActivityKey, ActivityData, ActivityKind, ActivityDate, IsMassOperation, 
+			ThreadId, ThreadTitle, ThreadUrl, ThreadKind, ThreadDeleted, ThreadLastActivityOn, ThreadFirstActivityOn,
+			WebId, WebTitle, WebUrl, ListId, ListName, ListIcon, ItemId, UserId, UserDisplayName, UserAccount, UserPicture
+		FROM Result WHERE HasAccess = 1 AND ThreadDeleted = 0 AND ActivityDate > @MinDate AND ActivityDate < @MaxDate 
+			AND (WebUrl = @WebUrl OR WebUrl LIKE @WebUrl + @WebUrlSuffix + ''%'')
+			AND RowNum > @Start AND RowNum < @End
+
+		SET NOCOUNT OFF;
+	END'
+END
