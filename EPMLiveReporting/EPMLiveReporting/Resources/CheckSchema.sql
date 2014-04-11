4b6198d8495c909a455e6fdbd4e2c1d57941f7d3
Munjal Patel <EPM\mpatel>
2014-04-11 23:12:01 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4748

--- EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
+++ EPMLiveReporting/EPMLiveReporting/Resources/CheckSchema.sql
@@ -133,11 +133,13 @@ BEGIN
 		[Kind]       INT              CONSTRAINT [DF_SS_Logs_Kind] DEFAULT ((0)) NOT NULL,
 		[WebId]      UNIQUEIDENTIFIER NULL,
 		[UserId]     INT              NULL,
-		[DateTime]   DATETIME2 (7)    CONSTRAINT [DF_SS_Logs_DateTime] DEFAULT (getdate()) NULL
+		[DateTime]   DATETIME2 (7)    CONSTRAINT [DF_SS_Logs_DateTime] DEFAULT (getutcdate()) NULL
 	);
 END
 
-ALTER TABLE dbo.SS_Logs ALTER COLUMN Kind INT
+ALTER TABLE [dbo].[SS_Logs] ALTER COLUMN [Kind] INT
+ALTER TABLE [dbo].[SS_Logs] DROP CONSTRAINT [DF_SS_Logs_DateTime]
+ALTER TABLE [dbo].[SS_Logs] ADD  CONSTRAINT [DF_SS_Logs_DateTime]  DEFAULT (getutcdate()) FOR [DateTime]
 
 ---------------TABLE: SS_Streams----------------------
 
