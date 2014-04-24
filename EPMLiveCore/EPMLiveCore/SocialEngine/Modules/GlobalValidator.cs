a5b4d3330e4ceb4ea178770b0b5358685b7d5629
Munjal Patel <EPM\mpatel>
2014-04-24 18:34:58 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C4947

---
+++ EPMLiveCore/EPMLiveCore/SocialEngine/Modules/GlobalValidator.cs
@@ -0,0 +1,31 @@
+﻿using EPMLiveCore.SocialEngine.Contracts;
+using EPMLiveCore.SocialEngine.Events;
+
+namespace EPMLiveCore.SocialEngine.Modules
+{
+    internal class GlobalValidator : ISocialEngineModule
+    {
+        #region Methods (1) 
+
+        // Private Methods (1) 
+
+        private void OnValidateActivity(ProcessActivityEventArgs args)
+        {
+            if (args.ContextWeb.CurrentUser.ID != args.ContextWeb.Site.SystemAccount.ID) return;
+
+            args.Cancel = true;
+            args.CancellationMessage = "Ignoring activities performed by the System Account";
+        }
+
+        #endregion Methods 
+
+        #region Implementation of ISocialEngineModule
+
+        public void Initialize(SocialEngineEvents events)
+        {
+            events.OnValidateActivity += OnValidateActivity;
+        }
+
+        #endregion
+    }
+}
