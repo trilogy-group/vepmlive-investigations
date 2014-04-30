f9fd8b1f0d522094bb6ad35a7531e513c3aa6802
Jason Hughes <jhughes@lmrsolutions.com>
2014-04-30 18:21:23 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5022

---
+++ EPMLiveCore/EPMLiveCore/PlatformIntegrationEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace EPMLiveCore
+{
+    public class PlatformIntegrationEvent : SPItemEventReceiver
+    {
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdded(properties);
+        }
+
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+        }
+
+        public override void ItemDeleting(SPItemEventProperties properties)
+        {
+            base.ItemDeleting(properties);
+        }
+
+
+    }
+}
