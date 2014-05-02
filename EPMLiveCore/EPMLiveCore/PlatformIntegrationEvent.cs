e5a9ca1d57f6751e5f41d2322d83c4ddadc5cc08
Jason Hughes <jhughes@lmrsolutions.com>
2014-05-02 21:30:07 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5075

--- EPMLiveCore/EPMLiveCore/PlatformIntegrationEvent.cs
+++ EPMLiveCore/EPMLiveCore/PlatformIntegrationEvent.cs
@@ -4,26 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SharePoint;
+using System.Data.SqlClient;
 
 namespace EPMLiveCore
 {
     public class PlatformIntegrationEvent : SPItemEventReceiver
     {
+        private string url = "";
+        private string key = "";
+
+
         public override void ItemAdded(SPItemEventProperties properties)
         {
-            base.ItemAdded(properties);
+            SendItem(properties);
         }
 
         public override void ItemUpdated(SPItemEventProperties properties)
         {
-            base.ItemUpdated(properties);
+            SendItem(properties);
         }
 
         public override void ItemDeleting(SPItemEventProperties properties)
         {
-            base.ItemDeleting(properties);
+            GetKeyAndUrl(properties);
+
+            if (url != "")
+            {
+
+
+
+            }
         }
 
+        private void SendItem(SPItemEventProperties properties)
+        {
+            GetKeyAndUrl(properties);
 
+            if (url != "")
+            {
+                UplandPlatformAPI.IntegrationAPI api = new UplandPlatformAPI.IntegrationAPI();
+                api.Url = url;
+                api.PostItemSimple(key, properties.ListItemId.ToString());
+
+
+            }
+        }
+
+        private void GetKeyAndUrl(SPItemEventProperties properties)
+        {
+            SPSecurity.RunWithElevatedPrivileges(delegate()
+            {
+                try
+                {
+                    SqlConnection cn = new SqlConnection(EPMLiveCore.CoreFunctions.getConnectionString(properties.Site.WebApplication.Id));
+                    cn.Open();
+
+                    SqlCommand cmd = new SqlCommand("select IntegrationUrl, IntegrationKey from PLATFORMINTEGRATIONS where ListId=@ListId", cn);
+                    cmd.Parameters.AddWithValue("@ListId", properties.ListId);
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        url = dr.GetString(0);
+                        key = dr.GetString(1);
+                    }
+                    dr.Close();
+
+                    cn.Close();
+                }
+                catch { }
+            });
+        }
     }
 }
