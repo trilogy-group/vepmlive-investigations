5c28e57528541b59b29bcd5f23f4e68323a2f159
Munjal Patel <EPM\mpatel>
2014-08-09 01:23:03 +0000

    Fixes Toolbar width on ResourceGrid issue
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5565

--- EPMLiveWebParts/EPMLiveWebParts/javascripts/EPMLive.ResourceGrid.js
+++ EPMLiveWebParts/EPMLiveWebParts/javascripts/EPMLive.ResourceGrid.js
@@ -1195,12 +1195,12 @@ function registerEpmLiveResourceGridScript() {
                                 if (window.epmLiveMasterPageVersion >= 5.5) {
                                     if (!epmLiveResourceGrid.loaderStopped) {
 
-                                        //if ($$.ribbonBehavior == 0) {
-                                        //    $(document.getElementById("s4-ribbonrow")).height(126);
-                                        //}
-                                        //else {
-                                        //    $(document.getElementById("s4-ribbonrow")).height(35);
-                                        //}
+                                        if ($$.ribbonBehavior == 0) {
+                                            $(document.getElementById("s4-ribbonrow")).height(126);
+                                        }
+                                        else {
+                                            $(document.getElementById("s4-ribbonrow")).height(35);
+                                        }
 
                                         window.EPM.UI.Loader.current().stopLoading('WebPart' + $$.webpartQualifier);
                                         epmLiveResourceGrid.loaderStopped = true;
