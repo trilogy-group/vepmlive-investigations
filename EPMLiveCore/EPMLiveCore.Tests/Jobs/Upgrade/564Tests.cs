9137454824fdd41ef65ba20509c9f4681b03459f
Munjal Patel <EPM\mpatel>
2014-09-23 23:10:00 +0000

    Our very first unit test!!!
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5737

---
+++ EPMLiveCore/EPMLiveCore.Tests/Jobs/Upgrade/564Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveCore.Tests.Jobs.Upgrade
+{
+    [TestClass]
+    public class Upgrade564Tests
+    {
+        #region Methods (1) 
+
+        // Public Methods (1) 
+
+        [TestMethod]
+        public void CS_OFF_ColumnShouldBeAddedTo_EPGP_CAPACITY_VALUES() { }
+
+        #endregion Methods 
+    }
+}
--- EPMLiveCore/EPMLiveCore.Tests/Properties/AssemblyInfo.cs
+++ EPMLiveCore/EPMLiveCore.Tests/Properties/AssemblyInfo.cs
@@ -1,28 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
-using Microsoft.SharePoint.WebControls;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EPMLiveCore")]
+[assembly: AssemblyTitle("EPMLiveCore.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("EPMLiveCore")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EPMLiveCore.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)] 
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c773ae44-f066-446a-9cbd-d24afa794e34")]
+[assembly: Guid("0b322267-fb4c-4d57-8d36-e32e0d993c83")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,10 +33,4 @@ using Microsoft.SharePoint.WebControls;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: MdsCompliant(true)]
-[assembly: InternalsVisibleTo("EPMLiveCore.Tests, PublicKey="+
-    "0024000004800000940000000602000000240000525341310004000001000100074e1c00010a06"+
-    "ed87d0c775dbfcd080884cf261356325468b60ee656004531fd6da6ff205625856da23afef57de"+
-    "1ab0c7912f349cf68f7ee72744c6b5d1fb75cdb96db332385249e8894b40998b62ec13b92bd0f2"+
-    "7c71f6e3320b5e9fc392a3e55131d7ce86657a312752fc34073051fe3fe3484a41f2e207d50bfd"+
-    "ae6ea5de")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
