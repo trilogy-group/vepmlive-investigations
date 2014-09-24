cca8062defaf0f4e2e6594f4eea4c50e0e9255eb
Bhavdip Shah <bhavdips@cybage.com>
2014-09-24 07:03:58 +0000

    EPML-3942 : Make Grid Gantt Filter insensitive
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5738

--- EPMLiveWebParts/EPMLiveWebParts/WebPageCode/getganttitems.cs
+++ EPMLiveWebParts/EPMLiveWebParts/WebPageCode/getganttitems.cs
@@ -134,6 +134,10 @@ namespace EPMLiveWebParts
                     attr.Value = oField.InternalName;
                     ndNew.Attributes.Append(attr);
 
+                    attr = doc.CreateAttribute("CaseSensitive");
+                    attr.Value = "0";
+                    ndNew.Attributes.Append(attr);
+
                     XmlDocument oDoc = new XmlDocument();
                     oDoc.LoadXml(oField.SchemaXml);
 
