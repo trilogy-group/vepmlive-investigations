fe68f9dbe59f072e8e163fa8333ede01c32e2d8c
builduser <EPM\builduser>
2014-09-24 18:48:53 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5743

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyFileVersion("5.6.3.901")]
+[assembly: AssemblyFileVersion("5.6.4.513")]
