bede87653da0dc658410be729c7f73f020e62f3d
Munjal Patel <EPM\mpatel>
2014-09-25 20:55:35 +0000

    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Release;C5754

--- EPMLiveCore/EPMLiveCore.Tests/Jobs/Upgrade/564Tests.cs
+++ EPMLiveCore/EPMLiveCore.Tests/Jobs/Upgrade/564Tests.cs
@@ -1,4 +1,9 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Collections.Generic;
+using EPMLiveCore.Jobs.EPMLiveUpgrade;
+using EPMLiveCore.Jobs.EPMLiveUpgrade.Infrastructure;
+using EPMLiveCore.Jobs.EPMLiveUpgrade.Steps;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SPEmulators;
 
@@ -14,12 +19,18 @@ namespace EPMLiveCore.Tests.Jobs.Upgrade
         [TestMethod]
         public void CS_OFF_ColumnShouldBeAddedTo_EPGP_CAPACITY_VALUES()
         {
-            using (var context = new SPEmulationContext(IsolationLevel.Fake))
-            {
-                context.Web.Title.Should().Be("Munjal");
-            }
+            // Ensure PFE DB update step is one of the update step
+
+            List<Type> upgradeSteps = UpgradeUtilities.GetUpgradeSteps("5.6.4");
+            upgradeSteps.Should().Contain(t => t == typeof (UpgradePfeDb));
         }
 
         #endregion Methods 
+
+        [TestMethod]
+        public void Transfer_AssignedToID_From_LSTMyWork_To_TSITEM()
+        {
+            
+        }
     }
 }
--- EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Upgrader.cs
+++ EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Upgrader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using EPMLiveCore.API;
 using EPMLiveCore.Jobs.EPMLiveUpgrade.Infrastructure;
 using Microsoft.SharePoint;
@@ -8,24 +9,31 @@ namespace EPMLiveCore.Jobs.EPMLiveUpgrade
 {
     public class Upgrader : BaseJob
     {
-        #region Methods (1) 
+        #region Methods (2) 
 
-        // Public Methods (1) 
+        // Public Methods (2) 
 
         public void execute(SPSite site, SPWeb web, string data)
         {
             bool isPfeSite = site.Features[new Guid("158c5682-d839-4248-b780-82b4710ee152")] != null;
 
             List<Type> upgradeSteps = UpgradeUtilities.GetUpgradeSteps(data);
-            totalCount = upgradeSteps.Count;
+
+            IEnumerable<IUpgradeStep> steps =
+                upgradeSteps.Select(s => Activator.CreateInstance(s, new object[] {web, isPfeSite}))
+                    .OfType<IUpgradeStep>();
+
+            execute(site, web, data, steps);
+        }
+
+        public void execute(SPSite site, SPWeb web, string data, IEnumerable<IUpgradeStep> upgradeSteps)
+        {
+            IUpgradeStep[] steps = upgradeSteps as IUpgradeStep[] ?? upgradeSteps.ToArray();
+            totalCount = steps.Count();
 
             int progressCounter = 1;
-            foreach (Type upgradeStep in upgradeSteps)
+            foreach (IUpgradeStep step in steps)
             {
-                var step = Activator.CreateInstance(upgradeStep, new object[] {web, isPfeSite}) as IUpgradeStep;
-
-                if (step == null) continue;
-
                 bool success = step.Perform();
                 sErrors += step.Log;
 
