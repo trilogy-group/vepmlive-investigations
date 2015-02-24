69f919181e684937f36ce210f9b91cd123935f97
Munjal Patel <EPM\mpatel>
2015-02-24 19:41:14 +0000

    EPML-4762 -- Added upgrade step to add resorce updated event
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Main;C6288

---
+++ EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Steps/567Steps.cs
@@ -0,0 +1,44 @@
+﻿using EPMLiveCore.Jobs.EPMLiveUpgrade.Infrastructure;
+using Microsoft.SharePoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMLiveCore.Jobs.EPMLiveUpgrade.Steps
+{
+    [UpgradeStep(Version = EPMLiveVersion.V567, Order = 1.0, Description = "Adding Resource Pool events")]
+    internal class AddResourcePoolEvents : UpgradeStep
+    {
+        public AddResourcePoolEvents(SPWeb spWeb, bool isPfeSite) : base(spWeb, isPfeSite) { }
+
+        public override bool Perform()
+        {
+            SPSecurity.RunWithElevatedPrivileges(() =>
+            {
+                try
+                {
+                    using (var workEngineAPI = new WorkEngineAPI())
+                    {
+                        var log = System.Web.HttpUtility.HtmlEncode(workEngineAPI.Execute("AddRemoveFeatureEvents",
+                            @"<AddRemoveFeatureEvents><Data><Feature Name=""PFEResourceManagement"" Operation=""ADD""/></Data></AddRemoveFeatureEvents>"));
+
+                        LogMessage(log, 2);
+                        LogMessage("Resource Pool Events processed", MessageKind.SUCCESS, 4);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    string message = exception.InnerException != null
+                        ? exception.InnerException.Message
+                        : exception.Message;
+
+                    LogMessage(message, MessageKind.FAILURE, 4);
+                }
+            });
+
+            return true;
+        }
+    }
+}
