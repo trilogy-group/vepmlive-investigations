bcac75e57532fd3a05a634c43d39e3259dac72eb
Dhaval Upadhayay <dhavalu@cybage.com>
2015-02-25 11:17:26 +0000

    PML-4805 added code to upgrader
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Main;C6292

--- EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Steps/567Steps.cs
+++ EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Steps/567Steps.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace EPMLiveCore.Jobs.EPMLiveUpgrade.Steps
 {
-    [UpgradeStep(Version = EPMLiveVersion.V567, Order = 1.0, Description = "Adding Resource Pool events")]
+    [UpgradeStep(Version = EPMLiveVersion.V567, Order = 1.0, Description = "Adding Resource Pool events, Updating Description(Body) field with base content types")]
     internal class AddResourcePoolEvents : UpgradeStep
     {
         public AddResourcePoolEvents(SPWeb spWeb, bool isPfeSite) : base(spWeb, isPfeSite) { }
@@ -36,6 +36,55 @@ namespace EPMLiveCore.Jobs.EPMLiveUpgrade.Steps
 
                     LogMessage(message, MessageKind.FAILURE, 4);
                 }
+
+
+                try
+                {
+                    Web.AllowUnsafeUpdates = true;
+                    foreach (SPList list in Web.Lists)
+                    {
+                        if (!list.Hidden)
+                        {
+                            foreach (SPContentType contentType in list.ContentTypes)
+                            {
+                                try
+                                {
+                                    if (contentType != null && (contentType.Name == "Item" || contentType.Name == "Task"))
+                                    {
+                                        SPField fldDescription = list.Fields.GetFieldByInternalName("Body");
+                                        if (fldDescription != null)
+                                        {
+                                            SPFieldLink fld = new SPFieldLink(fldDescription);
+                                            if (fld != null)
+                                            {
+                                                if (contentType.FieldLinks[fld.Id] == null)
+                                                {
+                                                    contentType.FieldLinks.Delete(fld.Name);
+                                                    contentType.Update();
+                                                    contentType.FieldLinks.Add(fld);
+                                                    contentType.Update();
+                                                    list.Update();
+                                                    LogMessage(string.Format("Update processed with List: {0} , ContentType: {1} , Field: {2}", list.Title, contentType.Name, fld.DisplayName), MessageKind.SUCCESS, 4);
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    string message = exception.InnerException != null
+                        ? exception.InnerException.Message
+                        : exception.Message;
+
+                    LogMessage(message, MessageKind.FAILURE, 4);
+                }
+
             });
 
             return true;
