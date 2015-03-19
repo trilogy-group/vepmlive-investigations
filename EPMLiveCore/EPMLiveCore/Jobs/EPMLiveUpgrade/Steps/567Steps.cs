f4288bdfe2daf49c737c950b01d211a3bfdbea83
Dhaval Upadhayay <dhavalu@cybage.com>
2015-03-19 15:14:41 +0000

    EPML-4934: fixed to iterate only lists with basetemplate 10702
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Releases/R5.6.7;C6403

--- EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Steps/567Steps.cs
+++ EPMLiveCore/EPMLiveCore/Jobs/EPMLiveUpgrade/Steps/567Steps.cs
@@ -43,64 +43,64 @@ namespace EPMLiveCore.Jobs.EPMLiveUpgrade.Steps
         }
     }
 
-    //[UpgradeStep(Version = EPMLiveVersion.V567, Order = 2.0, Description = "Updating Description(Body) field with base content types")]
-    //internal class UpdateBodyField : UpgradeStep
-    //{
-    //    public UpdateBodyField(SPWeb spWeb, bool isPfeSite) : base(spWeb, isPfeSite) { }
+    [UpgradeStep(Version = EPMLiveVersion.V567, Order = 2.0, Description = "Updating Description(Body) field with base content types")]
+    internal class UpdateBodyField : UpgradeStep
+    {
+        public UpdateBodyField(SPWeb spWeb, bool isPfeSite) : base(spWeb, isPfeSite) { }
 
-    //    public override bool Perform()
-    //    {
-    //        SPSecurity.RunWithElevatedPrivileges(() =>
-    //        {
-    //            try
-    //            {
-    //                Web.AllowUnsafeUpdates = true;
-    //                foreach (SPList list in Web.Lists)
-    //                {
-    //                    if (!list.Hidden)
-    //                    {
-    //                        foreach (SPContentType contentType in list.ContentTypes)
-    //                        {
-    //                            try
-    //                            {
-    //                                if (contentType != null && (contentType.Name == "Item" || contentType.Name == "Task"))
-    //                                {
-    //                                    SPField fldDescription = list.Fields.GetFieldByInternalName("Body");
-    //                                    if (fldDescription != null)
-    //                                    {
-    //                                        SPFieldLink fld = new SPFieldLink(fldDescription);
-    //                                        if (fld != null)
-    //                                        {
-    //                                            if (contentType.FieldLinks[fld.Id] == null)
-    //                                            {
-    //                                                contentType.FieldLinks.Delete(fld.Name);
-    //                                                contentType.Update();
-    //                                                contentType.FieldLinks.Add(fld);
-    //                                                contentType.Update();
-    //                                                list.Update();
-    //                                                LogMessage(string.Format("Update processed with List: {0} , ContentType: {1} , Field: {2}", list.Title, contentType.Name, fld.DisplayName), MessageKind.SUCCESS, 4);
-    //                                            }
-    //                                        }
-    //                                    }
-    //                                }
-    //                            }
-    //                            catch { }
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //            catch (Exception exception)
-    //            {
-    //                string message = exception.InnerException != null
-    //                    ? exception.InnerException.Message
-    //                    : exception.Message;
+        public override bool Perform()
+        {
+            SPSecurity.RunWithElevatedPrivileges(() =>
+            {
+                try
+                {
+                    Web.AllowUnsafeUpdates = true;
+                    foreach (SPList list in Web.Lists)
+                    {
+                        if (!list.Hidden && (uint)list.BaseTemplate == 10702)
+                        {
+                            foreach (SPContentType contentType in list.ContentTypes)
+                            {
+                                try
+                                {
+                                    if (contentType != null && contentType.Name == "Task")
+                                    {
+                                        SPField fldDescription = list.Fields.GetFieldByInternalName("Body");
+                                        if (fldDescription != null)
+                                        {
+                                            SPFieldLink fld = new SPFieldLink(fldDescription);
+                                            if (fld != null)
+                                            {
+                                                if (contentType.FieldLinks[fld.Id] == null)
+                                                {
+                                                    contentType.FieldLinks.Delete(fld.Name);
+                                                    contentType.Update();
+                                                    contentType.FieldLinks.Add(fld);
+                                                    contentType.Update();
+                                                    list.Update();
+                                                    LogMessage(string.Format("Update processed with List: {0} , ContentType: {1} , Field: {2}", list.Title, contentType.Name, fld.DisplayName), MessageKind.SUCCESS, 4);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    string message = exception.InnerException != null
+                        ? exception.InnerException.Message
+                        : exception.Message;
 
-    //                LogMessage(message, MessageKind.FAILURE, 4);
-    //            }
+                    LogMessage(message, MessageKind.FAILURE, 4);
+                }
 
-    //        });
+            });
 
-    //        return true;
-    //    }
-    //}
+            return true;
+        }
+    }
 }
