719d972f921b1df638a821d7bfb5f02bea7e3569
Bhavdip Shah <bhavdips@cybage.com>
2015-03-26 11:39:01 +0000

    EPML-4538 : List Names in Snapshot Management Page showing as Unique ID's
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Main;C6430

--- EPMLiveReporting/EPMLiveReporting/Layouts/EPMLive/AllSnapshots.aspx.cs
+++ EPMLiveReporting/EPMLiveReporting/Layouts/EPMLive/AllSnapshots.aspx.cs
@@ -5,6 +5,7 @@ using System.Web.UI;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Utilities;
 using Microsoft.SharePoint.WebControls;
+using System.Text;
 
 namespace EPMLiveReportsAdmin.Layouts.EPMLive
 {
@@ -39,8 +40,10 @@ namespace EPMLiveReportsAdmin.Layouts.EPMLive
 
         protected void LoadSnapshots()
         {
-            _DAO.Command =
-                "SELECT Enabled as [Active], Title as [Report Title], PeriodDate as [Reporting Period], DateArchived as [Snapshot Date], ListNames as [Lists],periodid,siteid FROM RPTPeriods";
+            DataTable manipulateDt = new DataTable();
+
+            _DAO.Command = "SELECT Enabled as [Active], Title as [Report Title], PeriodDate as [Reporting Period], DateArchived as [Snapshot Date], ListNames as [Lists],periodid,siteid FROM RPTPeriods";
+
             DataTable dt = _DAO.GetTable(_DAO.GetClientReportingConnection);
             SPBoundField gridColumn;
 
@@ -58,6 +61,33 @@ namespace EPMLiveReportsAdmin.Layouts.EPMLive
                     }
                     grdVwSnapshots.Columns.Add(gridColumn);
                 }
+
+                //EPML-4538 : List Names in Snapshot Management Page showing as Unique ID's
+                //Manipulate data table to process List Guids and display list name on Snapshot Management page.
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        StringBuilder listNames = new StringBuilder();
+                        string listIds = Convert.ToString(dt.Rows[i]["Lists"]);
+                        if (!string.IsNullOrEmpty(listIds) && listIds.Contains(","))
+                            listIds = listIds.Replace(", ", "','").Trim();
+
+                        _DAO.Command = "SELECT DISTINCT ListName FROM RPTList WHERE RPTListId in('" + listIds + "')";
+                        manipulateDt = _DAO.GetTable(_DAO.GetClientReportingConnection);
+
+                        if (manipulateDt != null && manipulateDt.Rows.Count > 0)
+                        {
+                            for (int id = 0; id < manipulateDt.Rows.Count; id++)
+                            {
+                                listNames.Append(Convert.ToString(manipulateDt.Rows[id][0]) + ", ");
+                            }
+                            if (listNames.Length > 0)
+                                dt.Rows[i]["Lists"] = listNames.ToString().Substring(0, listNames.Length - 2).Trim();
+                        }
+                    }
+                }
+
                 grdVwSnapshots.DataSource = dt;
                 grdVwSnapshots.DataBind();
             }
