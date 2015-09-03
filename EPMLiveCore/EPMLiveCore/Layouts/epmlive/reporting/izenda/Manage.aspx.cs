19734e20014e97c361291071bbea0b0b1722c1c8
Bhavdip Shah <bhavdips@cybage.com>
2015-09-03 05:04:10 +0000

    EPML-EPML-5574: Advanced Reporting Page Throws Error
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Main;C6886

--- EPMLiveCore/EPMLiveCore/Layouts/epmlive/reporting/izenda/Manage.aspx.cs
+++ EPMLiveCore/EPMLiveCore/Layouts/epmlive/reporting/izenda/Manage.aspx.cs
@@ -30,7 +30,8 @@ namespace EPMLiveCore.Layouts.epmlive.reporting.izenda
                 SqlConnection cn = new SqlConnection(CoreFunctions.getConnectionString(Web.Site.WebApplication.Id));
                 cn.Open();
 
-                SqlCommand cmd = new SqlCommand("SELECT * FROM IzendaAdHocReports where TenantID='" + Web.ID.ToString() + "' order by Name", cn);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM IzendaAdHocReports where TenantID=@siteid order by Name", cn);
+                cmd.Parameters.AddWithValue("@siteid", Convert.ToString(Web.ID));
 
                 DataSet ds = new DataSet();
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
