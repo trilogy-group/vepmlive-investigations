23ac1f038d0c4e8204250a0686d2b4ba346f931d
Pratibha Sainger <psainger@uplandsoftware.com>
2016-01-16 00:03:42 +0000

    Issue with Remove Solutions link in Central Admin -> Application Settings page
    git-tfs-id: [https://tfs.epmlive.com/tfs/EPMLive/]$/EPMLive2013Release/Main;C7237

--- EPMLiveCore/EPMLiveCore/WebPageCode/removesolutions.aspx.cs
+++ EPMLiveCore/EPMLiveCore/WebPageCode/removesolutions.aspx.cs
@@ -34,11 +34,12 @@ namespace EPMLiveCore
 
                 SPFarm farm = SPFarm.Local;
 
-                Remove(new Guid("55aca119-d7c7-494a-b5a7-c3ade07d06eb"), farm, apps);
-                Remove(new Guid("98e5c373-e1a0-45ce-8124-30c203cd8003"), farm, apps);
-                Remove(new Guid("1858d521-0375-4a61-9281-f5210854bc12"), farm, apps);
-                Remove(new Guid("160f5e32-b93f-4682-95bc-6db38233535a"), farm, apps);
-                Remove(new Guid("5a3fe24c-2dc5-4a1c-aec1-6ce942825ceb"), farm, apps);
+                Remove(new Guid("55aca119-d7c7-494a-b5a7-c3ade07d06eb"), farm, apps); // Core
+                Remove(new Guid("98e5c373-e1a0-45ce-8124-30c203cd8003"), farm, apps); // WebParts
+                Remove(new Guid("1858d521-0375-4a61-9281-f5210854bc12"), farm, apps); // Timesheets
+                //Remove(new Guid("160f5e32-b93f-4682-95bc-6db38233535a"), farm, apps); //Old Dashboards solution
+                Remove(new Guid("8f916fa9-1c2d-4416-8036-4a272256e23d"), farm, apps); //Dashboards 
+                Remove(new Guid("5a3fe24c-2dc5-4a1c-aec1-6ce942825ceb"), farm, apps); // PFE
             }
             catch(Exception ex) { output = "Failed: " + ex.Message; return; }
             output = "Success";
