b79d01f5fc3869d795049f6f6ae1b3336ba62011
Bhavdip Shah <bhavdip.shah@versata.com>
2016-07-05 14:19:21 +0000

    EPMLCID-6418: Ribbon Bar options are disabled on all the lists

--- EPMLiveWebParts/EPMLiveWebParts/ResourcePlanning/ResourcePlanning.cs
+++ EPMLiveWebParts/EPMLiveWebParts/ResourcePlanning/ResourcePlanning.cs
@@ -100,7 +100,7 @@ namespace EPMLiveWebParts
 
                             function _registerCustomPageComponent()
                             {
-                                SP.SOD.registerSod(""GridViewContextualTabPageComponent.js"", ""\/_layouts\/epmlive\/gridlistribbon.aspx"");
+                                SP.SOD.registerSod(""GridViewContextualTabPageComponent.js"", "" " + SPContext.Current.Web.Url + @"\/_layouts\/epmlive\/gridlistribbon.aspx"");
                                 SP.SOD.executeFunc(""GridViewContextualTabPageComponent.js"", ""ContextualWebPart.CustomPageComponent"", _addCustomPageComponent);
                             }
 
