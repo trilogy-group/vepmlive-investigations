bafc68710525dd7de337cb05ecc83cc010022828
ipodsekin <igor.podsekin@aurea.com>
2016-08-17 23:09:23 +0300

    Add epmliveintegration to IS

--- BuildTeamCity/tl_build_v3.ps1
+++ BuildTeamCity/tl_build_v3.ps1
@@ -382,7 +382,7 @@ Copy-Item $SourcesDirectory\packages\DocumentFormat.OpenXml.2.5\lib\DocumentForm
 Copy-Item $BinariesDirectory\WE_QueueMgr.exe $BuildDependenciesFolder\ServerFiles -Force  
 Copy-Item $BinariesDirectory\WE_QueueMgr.exe $BuildDependenciesFolder -Force  
 Copy-Item $BinariesDirectory\UserNameChecker.dll $BuildDependenciesFolder -Force  
-
+Copy-Item $BinariesDirectory\EPMLiveIntegration.dll $BuildDependenciesFolder -Force
 
 #Run Installshield project to generate product .exe
 & "C:\Program Files (x86)\InstallShield\2015\System\IsCmdBld.exe" -p "$SourcesDirectory\InstallShield\WorkEngine5\WorkEngine5.ism" -y $NewReleaseNumber -a "Product Configuration 1" -r "PrimaryRelease" -l PATH_TO_BUILDDDEPENDENC_FILES="$BuildDependenciesFolder" -l PATH_TO_PRODUCTOUTPUT_FILES="$ProductOutput"
