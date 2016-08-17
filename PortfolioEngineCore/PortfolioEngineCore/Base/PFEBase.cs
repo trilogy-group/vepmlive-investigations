c70ed9bf653f53072025d72805be6aced5545508
Chander <chander.k@versata.com>
2016-08-17 00:42:11 -0700

    W.R.T Code review changes Also some changes in existing code reported by reviewer

--- PortfolioEngineCore/PortfolioEngineCore/Base/PFEBase.cs
+++ PortfolioEngineCore/PortfolioEngineCore/Base/PFEBase.cs
@@ -4,190 +4,196 @@ using System.Data.SqlClient;
 
 namespace PortfolioEngineCore
 {
-    public class PFEBase
-    {
-        #region Fields (10)
+	public class PFEBase : IDisposable
+	{
+		#region Fields (10)
 
-        protected SqlConnection _PFECN;
-        protected string _basepath;
-        protected string _company;
-        protected string _dbcnstring;
-        protected bool _debug;
-        protected string _pid;
-        protected string _username;
-        protected Debugger debug;
-        protected int _userWResID;
-        protected int _port;
-        protected string _session;
-        protected DBAccess _dba;
+		protected SqlConnection _PFECN;
+		protected string _basepath;
+		protected string _company;
+		protected string _dbcnstring;
+		protected bool _debug;
+		protected string _pid;
+		protected string _username;
+		protected Debugger debug;
+		protected int _userWResID;
+		protected int _port;
+		protected string _session;
+		protected DBAccess _dba;
 
-        #endregion Fields
+		#endregion Fields
 
-        #region Constructors (2)
+		#region Constructors (2)
 
-        public PFEBase(string basepath, string username, string pid, string company, string dbcnstring, SecurityLevels secLevel, bool bDebug = false)
-        {
-            PFEBaseCommon(basepath, username, pid, company, dbcnstring, secLevel, bDebug);
-        }
+		public PFEBase(string basepath, string username, string pid, string company, string dbcnstring, SecurityLevels secLevel, bool bDebug = false)
+		{
+			PFEBaseCommon(basepath, username, pid, company, dbcnstring, secLevel, bDebug);
+		}
 
-        public PFEBase(string sBaseInfo, SecurityLevels secLevel = SecurityLevels.Base, bool bDebug = false)
-        {
-            CStruct xBaseInfo = new CStruct();
-            xBaseInfo.LoadXML(sBaseInfo);
+		public PFEBase(string sBaseInfo, SecurityLevels secLevel = SecurityLevels.Base, bool bDebug = false)
+		{
+			CStruct xBaseInfo = new CStruct();
+			xBaseInfo.LoadXML(sBaseInfo);
 
-            string basepath = xBaseInfo.GetString("basepath");
-            string username = xBaseInfo.GetString("username");
-            string pid = xBaseInfo.GetString("pid");
-            string company = xBaseInfo.GetString("company");
-            string dbcnstring = xBaseInfo.GetString("dbcnstring");
-            _port = xBaseInfo.GetInt("port");
-            _session = xBaseInfo.GetString("session");
+			string basepath = xBaseInfo.GetString("basepath");
+			string username = xBaseInfo.GetString("username");
+			string pid = xBaseInfo.GetString("pid");
+			string company = xBaseInfo.GetString("company");
+			string dbcnstring = xBaseInfo.GetString("dbcnstring");
+			_port = xBaseInfo.GetInt("port");
+			_session = xBaseInfo.GetString("session");
 
-            PFEBaseCommon(basepath, username, pid, company, dbcnstring, secLevel, bDebug);
-        }
+			PFEBaseCommon(basepath, username, pid, company, dbcnstring, secLevel, bDebug);
+		}
 
-        private void PFEBaseCommon(string basepath, string username, string pid, string company, string dbcnstring, SecurityLevels secLevel, bool bDebug)
-        {
-            debug = new Debugger(bDebug);
-            debug.AddMessage("Begin Debug PFE BASE");
+		private void PFEBaseCommon(string basepath, string username, string pid, string company, string dbcnstring, SecurityLevels secLevel, bool bDebug)
+		{
+			debug = new Debugger(bDebug);
+			debug.AddMessage("Begin Debug PFE BASE");
 
-            _basepath = basepath;
-            _username = username;
-            _pid = pid;
-            _company = company;
-            _dbcnstring = dbcnstring;
-            _debug = bDebug;
+			_basepath = basepath;
+			_username = username;
+			_pid = pid;
+			_company = company;
+			_dbcnstring = dbcnstring;
+			_debug = bDebug;
 
-            var act = new Activation(debug);
-            act.checkActivation(basepath, PFEEncrypt.Decrypt(pid, "MnjkafjhkAS&*^#@"), company);
+			var act = new Activation(debug);
+			act.checkActivation(basepath, PFEEncrypt.Decrypt(pid, "MnjkafjhkAS&*^#@"), company);
 
-            var db = new Database(debug);
-            _PFECN = db.OpenDatabase(basepath, PFEEncrypt.Decrypt(dbcnstring, "MnjkafjhkAS&*^#@"));
-
-
-            try
-            {
-                _PFECN.Open();
-
-                string cmdText = "SELECT WRES_ID FROM EPG_RESOURCES WHERE WRES_CAN_LOGIN = 1 AND WRES_USE_NT_LOGON = 1 AND WRES_NT_ACCOUNT=@WRES_NT_ACCOUNT";
-                using (var sqlCommand = new SqlCommand(cmdText, _PFECN) { CommandType = CommandType.Text })
-                {
-                    var usernameParameter = new SqlParameter("@WRES_NT_ACCOUNT", SqlDbType.NVarChar, 255) { Value = username };
-
-                    sqlCommand.Parameters.Add(usernameParameter);
-
-                    SqlDataReader reader = sqlCommand.ExecuteReader();
-                    if (reader.Read())
-                        _userWResID = DBAccess.ReadIntValue(reader["WRES_ID"]);
-                    reader.Close();
-                }
-                _PFECN.Close();
-
-            }
-            catch (Exception exception)
-            {
-                throw new PFEException((int)PFEError.CheckSecurity, exception.GetBaseMessage());
-            }
-
-            var sec = new BaseSecurity(debug, _PFECN);
-
-            if (!sec.ChecksScurity(username, secLevel))
-            {
-                throw new PFEException((int)PFEError.BaseAccessDenied, "Access Denied");
-            }
-
-            _dbcnstring = db.sCN;
-            _dba = new DBAccess(db.sCN, _PFECN);
-            _dba.UserWResID = _userWResID;
-            _dba.UserName = _username;
-
-            debug.AddMessage("PFE Base Loaded");
-        }
+			var db = new Database(debug);
+			_PFECN = db.OpenDatabase(basepath, PFEEncrypt.Decrypt(dbcnstring, "MnjkafjhkAS&*^#@"));
 
 
-        #endregion Constructors
+			try
+			{
+				_PFECN.Open();
 
-        #region Methods (2)
+				string cmdText = "SELECT WRES_ID FROM EPG_RESOURCES WHERE WRES_CAN_LOGIN = 1 AND WRES_USE_NT_LOGON = 1 AND WRES_NT_ACCOUNT=@WRES_NT_ACCOUNT";
+				using (var sqlCommand = new SqlCommand(cmdText, _PFECN) { CommandType = CommandType.Text })
+				{
+					var usernameParameter = new SqlParameter("@WRES_NT_ACCOUNT", SqlDbType.NVarChar, 255) { Value = username };
 
-        // Public Methods (2) 
+					sqlCommand.Parameters.Add(usernameParameter);
 
-        public string getDebugInfo()
-        {
-            return debug.GetMessage();
-        }
+					SqlDataReader reader = sqlCommand.ExecuteReader();
+					if (reader.Read())
+						_userWResID = DBAccess.ReadIntValue(reader["WRES_ID"]);
+					reader.Close();
+				}
+				_PFECN.Close();
 
-        public string FormatErrorText()
-        {
-            if (_dba != null)
-            {
-                return _dba.FormatErrorText();
-            }
-            return "";
-        }
+			}
+			catch (Exception exception)
+			{
+				throw new PFEException((int)PFEError.CheckSecurity, exception.GetBaseMessage());
+			}
 
-        #endregion Methods
+			var sec = new BaseSecurity(debug, _PFECN);
 
-        #region Properties (3)
+			if (!sec.ChecksScurity(username, secLevel))
+			{
+				throw new PFEException((int)PFEError.BaseAccessDenied, "Access Denied");
+			}
 
-        public int Status
-        {
-            get
-            {
-                if (_dba != null)
-                {
-                    return (int)_dba.Status;
-                }
-                return 0;
-            }
-        }
+			_dbcnstring = db.sCN;
+			_dba = new DBAccess(db.sCN, _PFECN);
+			_dba.UserWResID = _userWResID;
+			_dba.UserName = _username;
 
-        public string StatusText
-        {
-            get
-            {
-                if (_dba != null)
-                {
-                    return _dba.StatusText;
-                }
-                return "";
-            }
-        }
+			debug.AddMessage("PFE Base Loaded");
+		}
 
-        public string StackTrace
-        {
-            get
-            {
-                if (_dba != null)
-                {
-                    return _dba.StackTrace;
-                }
-                return "";
-            }
-        }
 
-        public string BasePath
-        {
-            get
-            {
-                return _basepath;
-            }
-        }
+		#endregion Constructors
 
-        public string UserName
-        {
-            get
-            {
-                return _username;
-            }
-        }
+		#region Methods (2)
 
-        public int UserId
-        {
-            get
-            {
-                return _userWResID;
-            }
-        }
-        #endregion Properties
-    }
+		// Public Methods (2) 
+
+		public string getDebugInfo()
+		{
+			return debug.GetMessage();
+		}
+
+		public string FormatErrorText()
+		{
+			if (_dba != null)
+			{
+				return _dba.FormatErrorText();
+			}
+			return "";
+		}
+
+		public void Dispose()
+		{
+			if (_dba != null)
+				_dba.Close();
+		}
+
+		#endregion Methods
+
+		#region Properties (3)
+
+		public int Status
+		{
+			get
+			{
+				if (_dba != null)
+				{
+					return (int)_dba.Status;
+				}
+				return 0;
+			}
+		}
+
+		public string StatusText
+		{
+			get
+			{
+				if (_dba != null)
+				{
+					return _dba.StatusText;
+				}
+				return "";
+			}
+		}
+
+		public string StackTrace
+		{
+			get
+			{
+				if (_dba != null)
+				{
+					return _dba.StackTrace;
+				}
+				return "";
+			}
+		}
+
+		public string BasePath
+		{
+			get
+			{
+				return _basepath;
+			}
+		}
+
+		public string UserName
+		{
+			get
+			{
+				return _username;
+			}
+		}
+
+		public int UserId
+		{
+			get
+			{
+				return _userWResID;
+			}
+		}
+		#endregion Properties
+	}
 }
