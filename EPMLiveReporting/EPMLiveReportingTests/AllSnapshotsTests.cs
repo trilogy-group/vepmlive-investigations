50dfa0aaaaa8cad2f2e4fcf3776245005eb21cbe
Administrator <Administrator@WIN-6J09GF4NBP8>
2016-10-13 23:29:39 -0700

    EPMLCID-6629: Automated Unit Test Case Script Check-in

---
+++ EPMLiveReporting/EPMLiveReportingTests/AllSnapshotsTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.SharePoint.Utilities.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Fakes;
+
+namespace EPMLiveReportsAdmin.Layouts.EPMLive.Tests
+{
+    [TestClass()]
+    public class AllSnapshotsTests
+    {
+        [TestMethod()]
+        public void RaisePostBackEventTest()
+        {
+            using (new SPEmulators.SPEmulationContext(SPEmulators.IsolationLevel.Fake))
+            {
+                string snapshotId = "AE3914B2-C74F-4400-B01F-FE8A9EDF21BA";
+
+                using (ShimsContext.Create())
+                {
+                    ShimHttpContext.CurrentGet = () =>
+                    {
+                        return new ShimHttpContext();
+                    };
+                }
+
+                ShimSPUtility.RedirectStringSPRedirectFlagsHttpContext = (url, flags, httpContext) =>
+                {
+                    return false;
+                };
+
+                try
+                {
+                    AllSnapshots allSnapshotPage = new AllSnapshots();
+                    allSnapshotPage.RaisePostBackEvent(snapshotId);
+                    Assert.IsTrue(true);
+                }
+                catch
+                {
+                    Assert.IsTrue(false);
+                }
+            }
+        }
+    }
+}
