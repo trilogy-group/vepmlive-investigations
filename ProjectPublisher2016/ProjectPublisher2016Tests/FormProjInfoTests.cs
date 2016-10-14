ee3a6c0b36fa5f508851595ef592b4905f4d6660
Igor Podsekin <igor.podsekin@aurea.com>
2016-10-14 05:01:55 -0700

    frame

---
+++ ProjectPublisher2016/ProjectPublisher2016Tests/FormProjInfoTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectPublisher2016;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Fakes;
+
+namespace ProjectPublisher2016.Tests
+{
+    [TestClass()]
+    public class FormProjInfoTests
+    {
+        [TestMethod()]
+        public void FormProjInfoTest()
+        {
+            FormProjInfo instanse = new FormProjInfo();
+            var textBox = new TextBox();
+
+            //initialize textBox here
+            instanse.listView1.Controls.AddRange(new Control[] { textBox });
+
+            //initialize instanse.listView1 here
+
+            string shownMessage = string.Empty;
+            ShimMessageBox.ShowString = (message) =>
+            {
+                shownMessage = message;
+                return DialogResult.OK;
+            };
+
+            instanse.button1Click(null, null);
+
+            Assert.AreEqual(shownMessage, "You must enter a value for bla bla");
+        }
+    }
+}
