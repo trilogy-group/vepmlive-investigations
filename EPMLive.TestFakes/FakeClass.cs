ca1979d992a5f02b0abc08321467998af3875e8f
Igor Podsekin <igor.podsekin@aurea.com>
2016-10-14 07:02:01 -0700

    fakes lib2

---
+++ EPMLive.TestFakes/FakeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLive.TestFakes
+{
+    [TestClass]
+    public class FakeClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
