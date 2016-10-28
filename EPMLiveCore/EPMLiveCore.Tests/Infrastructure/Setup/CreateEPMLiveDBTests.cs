1afc25e9321d680b00da8390f8b08d2e2607575c
Ankit Gandhi <ankit.gandhi@versata.com>
2016-10-28 03:40:27 -0700

    EPMLCID-6670: Committed the code & Unit test cases

---
+++ EPMLiveCore/EPMLiveCore.Tests/Infrastructure/Setup/CreateEPMLiveDBTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EPMLiveCore.Infrastructure.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Administration.Fakes;
+using System.Data.SqlClient.Fakes;
+using EPMLiveCore.Fakes;
+using EPMLiveCore.Infrastructure.Setup.Fakes;
+
+namespace EPMLiveCore.Infrastructure.Setup.Tests
+{
+    [TestClass()]
+    public class CreateEPMLiveDBTests
+    {
+        protected string output = string.Empty;
+        [TestMethod()]
+        public void CreateEPMLiveDBTest_Sucess()
+        {
+            using (SPEmulators.SPEmulationContext ctx = new SPEmulators.SPEmulationContext(SPEmulators.IsolationLevel.Fake))
+            {
+                Guid webApplicationId = Guid.NewGuid();
+                CreateEPMLiveDB createEPMLiveDB = new CreateEPMLiveDB();
+
+                ShimSPWebApplication.AllInstances.ApplicationPoolGet = (instance) =>
+                {
+                    return new ShimSPApplicationPool();
+                };
+
+                ShimSPPersistedObject.AllInstances.IdGet = (instance) =>
+                {
+                    return webApplicationId;
+                };
+
+                ShimSPProcessIdentity.AllInstances.UsernameGet = (instance) =>
+                {
+                    return "epmldev\farmadmin";
+                };
+
+                ShimCreateEPMLiveDB.AllInstances.GetWebApplicationGuid = (instance, a) =>
+                {
+                    return new ShimSPWebApplication();
+                };
+
+                ShimSqlCommand.AllInstances.ExecuteNonQuery = (instance) =>
+                {
+                    return 1;
+                };
+
+                ShimCoreFunctions.setConnectionStringGuidStringStringOut = (Guid gWebApp, string cn, out string sError) =>
+                {
+                    sError = "";
+                    return true;
+                };
+
+                Assert.AreEqual(createEPMLiveDB.CreateEPMLiveDatabase(webApplicationId, "win-6j09gf4nbp8", "EPMLive1", "", ""), "Sucess");
+            }
+        }
+
+        [TestMethod()]
+        public void CreateEPMLiveDBTest_Fail()
+        {
+            using (SPEmulators.SPEmulationContext ctx = new SPEmulators.SPEmulationContext(SPEmulators.IsolationLevel.Fake))
+            {
+                Guid webApplicationId = Guid.NewGuid();
+                CreateEPMLiveDB createEPMLiveDB = new CreateEPMLiveDB();
+
+                ShimSPWebApplication.AllInstances.ApplicationPoolGet = (instance) =>
+                {
+                    return new ShimSPApplicationPool();
+                };
+
+                ShimSPPersistedObject.AllInstances.IdGet = (instance) =>
+                {
+                    return webApplicationId;
+                };
+
+                ShimSPProcessIdentity.AllInstances.UsernameGet = (instance) =>
+                {
+                    return "epmldev\farmadmin";
+                };
+
+                ShimCreateEPMLiveDB.AllInstances.GetWebApplicationGuid = (instance, a) =>
+                {
+                    return new ShimSPWebApplication();
+                };
+
+                ShimSqlCommand.AllInstances.ExecuteNonQuery = (instance) =>
+                {
+                    return 1;
+                };
+
+                ShimCoreFunctions.setConnectionStringGuidStringStringOut = (Guid gWebApp, string cn, out string sError) =>
+                {
+                    sError = "Error";
+                    return false;
+                };
+
+                Assert.AreEqual(Convert.ToString(createEPMLiveDB.CreateEPMLiveDatabase(webApplicationId, "win-6j09gf4nbp8", "EPMLive1", "", "")), "Error Setting String: Error");             
+            }
+        }
+    }
+}
---
+++ EPMLiveCore/EPMLiveCore/Infrastructure/Setup/CreateEPMLiveDB.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.SharePoint.Administration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMLiveCore.Infrastructure.Setup
+{
+    public class CreateEPMLiveDB
+    {
+        protected string output = string.Empty;
+        public string CreateEPMLiveDatabase(Guid webAppID, string sSqlServer, string sDatabase, string sUsername, string sPassword)
+        {            
+            try
+            {
+                SPWebApplication webapp = GetWebApplication(webAppID);
+
+                string sSql = "";
+                if (sUsername == "")
+                    sSql = "Trusted_Connection=True";
+                else
+                    sSql = "User Id=" + sUsername + ";Password=" + sPassword;
+                
+                SqlConnection cn = new SqlConnection("Server=" + sSqlServer + ";Database=master;" + sSql);
+
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand("CREATE DATABASE " + sDatabase, cn);
+                cmd.ExecuteNonQuery();
+
+                cn.Close();
+
+                cn = new SqlConnection("Server=" + sSqlServer + ";Database=" + sDatabase + ";" + sSql);
+
+                cn.Open();
+
+                try
+                {
+                    cmd = new SqlCommand("create user [" + webapp.ApplicationPool.Username + "] from login [" + webapp.ApplicationPool.Username + "]", cn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch { }
+
+                try
+                {
+                    cmd = new SqlCommand("sp_addrolemember", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@rolename", "db_owner");
+                    cmd.Parameters.AddWithValue("@membername", webapp.ApplicationPool.Username);
+                    cmd.ExecuteNonQuery();
+                }
+                catch { }
+
+                cmd = new SqlCommand(Properties.Resources._0Tables01, cn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(Properties.Resources._0Tables02, cn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(Properties.Resources._1Views01, cn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(Properties.Resources._2SPs01, cn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(Properties.Resources._9Data01, cn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(Properties.Resources._9Data02, cn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand("INSERT INTO VERSIONS (VERSION, DTINSTALLED) VALUES (@version, GETDATE())", cn);
+                cmd.Parameters.AddWithValue("@version", CoreFunctions.GetFullAssemblyVersion());
+                cmd.ExecuteNonQuery();
+
+                cn.Close();
+
+                string sError = "";
+                string sqlString = "Server=" + sSqlServer + ";Database=" + sDatabase + ";" + sSql;                              
+                EPMLiveCore.CoreFunctions.setConnectionString(webapp.Id, sqlString, out sError);
+
+                if (sError != "")
+                {
+                    output = "Error Setting String: " + sError;                    
+                    return output;
+                }                
+            }
+            catch (Exception ex)
+            {                
+                output = "Failed: " + ex.Message;
+                return output;
+            }            
+            return output = "Sucess";
+        }
+        protected SPWebApplication GetWebApplication(Guid webAppId)
+        {
+            SPWebApplication web = SPWebService.ContentService.WebApplications[webAppId];
+            return web;
+        }
+    }
+}
