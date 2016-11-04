44d44de952592384965c94d1b61a06e47110ed6b
Igor Podsekin <igor.podsekin@aurea.com>
2016-11-04 11:34:48 -0700

    fix build

--- EPMLive.TestFakes/FakeClass.cs
+++ EPMLive.TestFakes/FakeClass.cs
@@ -3,10 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EPMLive.TestFakes
 {
-    [TestClass]
     public class FakeClass
     {
-        [TestMethod]
         public void TestMethod1()
         {
         }
