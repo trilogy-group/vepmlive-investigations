3d9e88ea640c53972fc4d908ed809de45ef76ddc
Igor Podsekin <igor.podsekin@aurea.com>
2017-01-02 17:24:25 -0800

    calc coverage

--- BuildTeamCity/calcCoverage.ps1
+++ BuildTeamCity/calcCoverage.ps1
@@ -1,33 +1,51 @@
+$ScriptDir = split-path -parent $MyInvocation.MyCommand.Definition
+$ScriptDir = (get-item $ScriptDir).parent.FullName
+
+$openCoverPath = (Resolve-Path "$ScriptDir\packages\OpenCover.*\tools\OpenCover.Console.exe").ToString()
+$vsConsolePath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+$a = New-Object -ComObject Scripting.FileSystemObject
+$f = $a.GetFile($vsConsolePath)
+$vsConsolePath = $f.ShortPath
+
+$targetFiles= (@(Get-ChildItem "$ScriptDir" -Include *.Tests.dll -Recurse -File |
+    Where-Object {(
+        $_.DirectoryName -inotmatch '\\obj\\' -and
+        $_.DirectoryName -ilike "*\Debug"
+    )}
+) | Select -ExpandProperty FullName) -join " "
+
+Write-Host $targetFiles
+
+&$openCoverPath  -register:user -target:$vsConsolePath "-targetargs:$targetFiles" "-filter:+[*]* -[*.tests]* -[*.Tests]* -[*.TestFakes]*" -excludebyfile:"*\*Designer.cs" -output:$ScriptDir\opencovertests.xml -mergebyhash
+
 function PublishArtifact($artifactPath) {
     Write-Host ("`n`nPublishing Artifact") -ForegroundColor Yellow
     Write-Host "##teamcity[publishArtifacts '$artifactPath']"
 }
 
-$ScriptDir = split-path -parent $MyInvocation.MyCommand.Definition
-$ScriptDir = (get-item $ScriptDir).parent.FullName 
-$coverageFile = $(get-ChildItem -Path "$ScriptDir\TestResults" -Recurse -Include *coverage)[0]
-$xmlCoverageFile = "$ScriptDir\TestResults\vstest.coveragexml"
+#$ScriptDir = split-path -parent $MyInvocation.MyCommand.Definition
+#$ScriptDir = (get-item $ScriptDir).parent.FullName 
+#$coverageFile = $(get-ChildItem -Path "$ScriptDir\TestResults" -Recurse -Include *coverage)[0]
+$xmlCoverageFile = "$ScriptDir\opencovertests.xml"
 $reportPath = "$ScriptDir\CoverageReports"
 $artifactsDir = "$ScriptDir\Artifacts"
 
 New-Item -ItemType Directory -Force -Path $reportPath
 New-Item -ItemType Directory -Force -Path $artifactsDir
 
-Add-Type -path "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.Coverage.Analysis.dll"
+#Add-Type -path "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.Coverage.Analysis.dll"
 
-[string[]] $executablePaths = @($coverageFile)
-[string[]] $symbolPaths = @()
+#[string[]] $executablePaths = @($coverageFile)
+#[string[]] $symbolPaths = @()
 
-$info = [Microsoft.VisualStudio.Coverage.Analysis.CoverageInfo]::CreateFromFile($coverageFile, $executablePaths, $symbolPaths);
-$data = $info.BuildDataSet()
+#$info = [Microsoft.VisualStudio.Coverage.Analysis.CoverageInfo]::CreateFromFile($coverageFile, $executablePaths, $symbolPaths);
+#$data = $info.BuildDataSet()
 
-$data.WriteXml($xmlCoverageFile)
+#$data.WriteXml($xmlCoverageFile)
 
-$RepGenPath = "$ScriptDir\packages\ReportGenerator.2.5.2\tools\ReportGenerator.exe"
+$RepGenPath = (Resolve-Path "$ScriptDir\packages\ReportGenerator.*\tools\ReportGenerator.exe").ToString()
 
 &$RepGenPath -reports:$xmlCoverageFile -targetdir:$reportPath -reportTypes:"HtmlSummary;XMLSummary"
-
-
 
 function CreateCoverageArtifactsPackage() {
     Write-Host ("`n`Packing Coverage Artifacts") -ForegroundColor Yellow
