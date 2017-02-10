0f60ef5944e73f12ad497cb85489eda9c84ce1c2
farmadmin <farmadmin@WIN-6J09GF4NBP8>
2017-02-10 12:43:53 -0800

    fix the active licenses list

--- Saas/OnlineLicensing.Api/Data/LicensingModel.Designer.cs
+++ Saas/OnlineLicensing.Api/Data/LicensingModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Jeff\EPMLive\epmlive2013\Saas\OnlineLicensing.Api\Data\LicensingModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\farmadmin\Desktop\Repos\epmlive2013release\Saas\OnlineLicensing.Api\Data\LicensingModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
