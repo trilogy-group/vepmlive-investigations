a9bcffc16ec9543113223f06027e4f4c9eb56510
Chander <chander.k@versata.com>
2017-05-11 13:35:08 +0100

    We have resolved this issue in 6.0.0 but it is not merged with Dev branch Please check the old Pull request https://github.com/trilogy-group/vepmlive-epmlive2013release/pull/106

---
+++ EPMLiveTimesheets/EPMLiveTimesheets/Logger/Logger.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.SharePoint.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSheets.Log
+{
+    public class Logger : SPDiagnosticsServiceBase
+    {
+        public static string DiagnosticAreaName = "TimeSheet";
+        private static Logger _Current;
+        public static Logger Current
+        {
+            get
+            {
+                if (_Current == null)
+                {
+                    _Current = new Logger();
+                }
+
+                return _Current;
+            }
+        }
+
+        public Logger() : base("TimeSheet Logging Service", SPFarm.Local)
+        {
+
+        }
+
+        public enum Category
+        {
+            Unexpected,
+            High,
+            Medium,
+            Information
+        }
+
+        protected override IEnumerable<SPDiagnosticsArea> ProvideAreas()
+        {
+            List<SPDiagnosticsArea> areas = new List<SPDiagnosticsArea>
+        {
+            new SPDiagnosticsArea(DiagnosticAreaName, new List<SPDiagnosticsCategory>
+            {
+                new SPDiagnosticsCategory("Unexpected", TraceSeverity.Unexpected, EventSeverity.Error),
+                new SPDiagnosticsCategory("High", TraceSeverity.High, EventSeverity.Warning),
+                new SPDiagnosticsCategory("Medium", TraceSeverity.Medium, EventSeverity.Information),
+                new SPDiagnosticsCategory("Information", TraceSeverity.Verbose, EventSeverity.Information)
+            })
+        };
+
+            return areas;
+        }
+
+        public static void WriteLog(Category categoryName, string source, string errorMessage)
+        {
+            SPDiagnosticsCategory category = Logger.Current.Areas[DiagnosticAreaName].Categories[categoryName.ToString()];
+            Logger.Current.WriteTrace(0, category, category.TraceSeverity, string.Concat(source, ": ", errorMessage));
+        }
+    }
+}
