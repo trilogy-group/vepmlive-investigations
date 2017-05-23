d0f9fc497ff18ae77121064eeb61ef9a849f2697
Chander <chander.k@versata.com>
2017-05-23 16:56:27 +0100

    Link : https://jira.devfactory.com/browse/EPMLCID-6879 Cause : Grouping was not working because code was not implemented for it Solution : Added code which is performing group by as per view setting

--- PortfolioEngineCore/WorkEnginePPM/Layouts/ppm/ResPlanAnalyzer.ascx.js
+++ PortfolioEngineCore/WorkEnginePPM/Layouts/ppm/ResPlanAnalyzer.ascx.js
@@ -5807,8 +5807,16 @@
                 }
             } catch (e) {
             }
+            try {
 
-
+                var g_1 = Grids["g_1"];
+                var grouping = this.selectedView["g_1"]['Grouping'].split('|');
+                g_1.DoGrouping(grouping[1]);
+                g_1.Render();
+            } catch (e) {
+                console.log(e);
+            }
+           
             if (this.AnalyzerShowBarschecked == true) {
                 this.viewTab.setButtonStateOn("idAnalyzerShowBars");
             } else {
@@ -5853,6 +5861,7 @@
 
             WorkEnginePPM.ResPlanAnalyzer.ExecuteJSON("ApplyResourceAnalyzerViewServerSideSettings", this.selectedView.ViewGUID, SetChangeViewCompleteDelegate);
         }
+
     }
 
 
@@ -6433,6 +6442,7 @@
                 case "AnalyzerTab_SelView_Changed":
                     maxPeriodLimitExceedsConfirm = undefined;
                     this.SetViewChanged(null);
+                    
                     break;
 
 
