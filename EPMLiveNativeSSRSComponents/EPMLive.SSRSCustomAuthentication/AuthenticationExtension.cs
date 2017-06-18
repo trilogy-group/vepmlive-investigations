7502948d7ec7a88f04a6745275d19ae248ec1921
Anuj Kumar <anuj.kumar@aurea.com>
2017-06-18 07:42:25 +0000

    Added custom authentication and authorization for SSRS.

---
+++ EPMLiveNativeSSRSComponents/EPMLive.SSRSCustomAuthentication/AuthenticationExtension.cs
@@ -0,0 +1,67 @@
+﻿using EPMLive.SSRSCustomAuthentication.AuthenticationProvider;
+using Microsoft.ReportingServices.Interfaces;
+using System;
+using System.Security.Principal;
+using System.Web;
+
+namespace EPMLive.SSRSCustomAuthentication
+{
+    public class AuthenticationExtension : IAuthenticationExtension2
+    {
+        private readonly IAuthenticationProvider authProvider;
+
+        public AuthenticationExtension()
+        {
+            
+        }
+
+        public string LocalizedName
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public void GetUserInfo(out IIdentity userIdentity, out IntPtr userId)
+        {
+            if (HttpContext.Current != null && HttpContext.Current.User != null)
+            {
+                userIdentity = HttpContext.Current.User.Identity;
+            }
+            else
+            {
+                throw new NullReferenceException("Anonymous logon is not allowed!");
+            }
+
+            userId = IntPtr.Zero;
+        }
+
+        public void GetUserInfo(IRSRequestContext requestContext, out IIdentity userIdentity, out IntPtr userId)
+        {
+            userIdentity = null;
+
+            if (requestContext.User != null)
+            {
+                userIdentity = requestContext.User;
+            }
+
+            userId = IntPtr.Zero;
+        }
+
+        public bool IsValidPrincipalName(string principalName)
+        {
+            return authProvider.VerifyUser(principalName);
+        }
+
+        public bool LogonUser(string userName, string password, string authority)
+        {
+            return authProvider.VerifyPassword(userName, password);
+        }
+
+        public void SetConfiguration(string configuration)
+        {
+            
+        }
+    }
+}
---
+++ EPMLiveNativeSSRSComponents/EPMLive.SSRSCustomAuthentication/AuthenticationProvider/ADAuthenticationProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EPMLive.SSRSCustomAuthentication.AuthenticationProvider
+{
+    public class ADAuthenticationProvider : IAuthenticationProvider
+    {
+        public bool VerifyPassword(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyUser(string principalName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
