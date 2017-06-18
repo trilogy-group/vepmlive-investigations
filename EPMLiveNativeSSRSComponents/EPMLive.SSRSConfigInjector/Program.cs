e784affc561b1ea7b9b3b5df09783dbb84ad4e7b
Anuj Kumar <anuj.kumar@aurea.com>
2017-06-18 09:56:42 +0000

    Injected configurations.

--- EPMLiveNativeSSRSComponents/EPMLive.SSRSConfigInjector/Program.cs
+++ EPMLiveNativeSSRSComponents/EPMLive.SSRSConfigInjector/Program.cs
@@ -40,11 +40,6 @@ namespace EPMLive.SSRSConfigInjector
 
         private static void ModifyReportServerPolicyConfig()
         {
-            throw new NotImplementedException();
-        }
-
-        private static void ModifyReportServerConfig()
-        {
             var fileName = ".\\..\\..\\Samples\\rsreportserver.config";
             var xmlDocument = new XmlDocument();
             xmlDocument.Load(fileName);
@@ -55,6 +50,68 @@ namespace EPMLive.SSRSConfigInjector
             var settings = new XmlWriterSettings { Indent = true };
             var writer = XmlWriter.Create(fileName, settings);
             xmlDocument.Save(writer);
+        }
+
+        private static void ModifyReportServerConfig()
+        {
+            var fileName = ".\\..\\..\\Samples\\rsreportserver.config";
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(fileName);
+
+            AddAuthNode(xmlDocument);
+            AddExistingUiNode(xmlDocument);
+            AddExistingSecurityExtNode(xmlDocument);
+            AddExistingAuthExtNode(xmlDocument);
+
+            var settings = new XmlWriterSettings { Indent = true };
+            var writer = XmlWriter.Create(fileName, settings);
+            xmlDocument.Save(writer);
+        }
+
+        private static void AddExistingAuthExtNode(XmlDocument xmlDocument)
+        {
+            var existingAuthExtNode = xmlDocument.SelectSingleNode("/Configuration/Extensions/Authentication/Extension[@Type='EPMLive.SSRSCustomAuthentication.AuthenticationExtension, EPMLive.SSRSCustomAuthentication']");
+            if (existingAuthExtNode == null)
+            {
+                var authExtNode = xmlDocument.SelectSingleNode("/Configuration/Extensions/Authentication");
+                authExtNode.RemoveAll();
+                var fragment = xmlDocument.CreateDocumentFragment();
+                fragment.InnerXml = "<Extension Name=\"Forms\" Type=\"EPMLive.SSRSCustomAuthentication.AuthenticationExtension, EPMLive.SSRSCustomAuthentication\"/>";
+                authExtNode.AppendChild(fragment);
+            }
+        }
+
+        private static void AddExistingSecurityExtNode(XmlDocument xmlDocument)
+        {
+            var existingsecurityExtNode = xmlDocument.SelectSingleNode("/Configuration/Extensions/Security/Extension[@Type='EPMLive.SSRSCustomAuthentication.AuthenticationExtension, EPMLive.SSRSCustomAuthentication']");
+            if (existingsecurityExtNode == null)
+            {
+                var securityExtNode = xmlDocument.SelectSingleNode("/Configuration/Extensions/Security");
+                securityExtNode.RemoveAll();
+                var fragment = xmlDocument.CreateDocumentFragment();
+                fragment.InnerXml = "<Extension Name=\"Forms\" Type=\"EPMLive.SSRSCustomAuthentication.AuthorizationExtension, EPMLive.SSRSCustomAuthentication\"><Configuration><AdminConfiguration><UserName>admin</UserName></AdminConfiguration></Configuration></Extension>";
+                securityExtNode.AppendChild(fragment);
+            }
+        }
+
+        private static void AddExistingUiNode(XmlDocument xmlDocument)
+        {
+            var existingCustomAuthUI = xmlDocument.SelectSingleNode("/Configuration/UI/CustomAuthenticationUI");
+            if (existingCustomAuthUI == null)
+            {
+                var fragment = xmlDocument.CreateDocumentFragment();
+                fragment.InnerXml = @"<CustomAuthenticationUI><UseSSL>False</UseSSL><PassThroughCookies><PassThroughCookie>sqlAuthCookie</PassThroughCookie></PassThroughCookies></CustomAuthenticationUI>";
+                var uiNode = xmlDocument.SelectSingleNode("/Configuration/UI");
+                uiNode.AppendChild(fragment);
+            }
+        }
+
+        private static void AddAuthNode(XmlDocument xmlDocument)
+        {
+            var customNode = xmlDocument.CreateElement("Custom");
+            var authenticationNode = xmlDocument.SelectSingleNode("/Configuration/Authentication/AuthenticationTypes");
+            authenticationNode.RemoveAll();
+            authenticationNode.AppendChild(customNode);
         }
     }
 }
