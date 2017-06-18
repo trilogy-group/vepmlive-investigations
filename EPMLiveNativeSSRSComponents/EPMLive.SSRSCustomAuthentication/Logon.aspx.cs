7873e6717b621a59c34deb86954794804da2fc38
Anuj Kumar <anuj.kumar@aurea.com>
2017-06-18 07:54:14 +0000

    Added page for logon.

---
+++ EPMLiveNativeSSRSComponents/EPMLive.SSRSCustomAuthentication/Logon.aspx.cs
@@ -0,0 +1,41 @@
+﻿using EPMLive.SSRSCustomAuthentication.AuthenticationProvider;
+using System;
+using System.Web.Security;
+using System.Web.UI;
+
+namespace EPMLive.SSRSCustomAuthentication
+{
+    public partial class Logon : Page
+    {
+        IAuthenticationProvider authenticationProvider;
+
+        public Logon()
+        {
+            authenticationProvider = new ADAuthenticationProvider();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            var userName = txtUserName.Text.Trim();
+            var password = txtPassword.Text.Trim();
+
+            var passwordVerified = authenticationProvider.VerifyPassword(userName, password);
+
+            if (passwordVerified)
+            {
+                FormsAuthentication.RedirectFromLoginPage(userName, false);
+            }            
+            else
+            {
+                pnlError.Visible = true;
+                txtPassword.Text = "";
+                txtUserName.Text = "";
+            }
+        }
+    }
+}
