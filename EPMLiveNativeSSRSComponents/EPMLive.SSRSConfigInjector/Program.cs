2c3b9855cf3c3cc3e5b75efa4a4e416e0f842c7e
Anuj Kumar <anuj.kumar@aurea.com>
2017-06-18 08:55:52 +0000

    Added samples of config files to be modified.

---
+++ EPMLiveNativeSSRSComponents/EPMLive.SSRSConfigInjector/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace EPMLive.SSRSConfigInjector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reportServerBasePath = "C:\\Program Files\\Microsoft SQL Server\\MSRS13.MSSQLSERVER";
+            var libraryPath = "..\\..\\EPMLive.SSRSCustomAuthentication\\bin\\Debug\\EPMLive.SSRSCustomAuthentication.";
+            
+            if (File.Exists(libraryPath + "dll"))
+            {
+                var ssrsBinPath = Path.Combine(reportServerBasePath, "Reporting Services\\ReportServer\bin");
+                File.Copy(libraryPath + "dll", Path.Combine(ssrsBinPath, libraryPath + "dll"));
+                File.Copy(libraryPath + "dll", Path.Combine(ssrsBinPath, libraryPath + "pdb"));
+            }
+
+        }
+    }
+}
