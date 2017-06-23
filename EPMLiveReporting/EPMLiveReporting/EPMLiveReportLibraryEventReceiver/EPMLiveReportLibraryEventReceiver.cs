448b4548e19dc96fa6e6d3efdd7c2d44bce925e9
Anuj Kumar <anuj.kumar@aurea.com>
2017-06-23 10:57:27 -0700

    Added event receiver.

---
+++ EPMLiveReporting/EPMLiveReporting/EPMLiveReportLibraryEventReceiver/EPMLiveReportLibraryEventReceiver.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SharePoint;
+using System.Diagnostics;
+
+namespace EPMLiveReportsAdmin
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class EPMLiveReportLibraryEventReceiver : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item was updated.
+        /// </summary>
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            properties.ListItem["Synchronized"] = false;
+            properties.ListItem.SystemUpdate();
+            base.ItemUpdated(properties);            
+        }
+    }
+}
