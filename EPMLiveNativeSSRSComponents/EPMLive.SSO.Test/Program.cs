ec13ddbde8d4f400165d8be92b94b111308be2a5
Anuj Kumar <anuj.kumar@aurea.com>
2017-06-28 22:02:40 -0700

    Added more projects.

---
+++ EPMLiveNativeSSRSComponents/EPMLive.SSO.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using EMPLive.SSO;
+using System.Net;
+
+namespace EPMLive.SSO.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new ReportingService2010Extended();
+            proxy.LogonUser("farmadmin", "Pass@word1", null);
+        }
+    }
+}
