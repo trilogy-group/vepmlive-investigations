4971d2372d31e8787c487eff2458f20951abf767
Chander <chander.k@versata.com>
2017-06-30 08:15:06 +0100

    https://jira.devfactory.com/browse/EPMLCID-11747
    Description : All changes required for EPMLIve Database:
    EPMLIve database Upgrader steps:
    Need to run 6.2.1 upgrade steps to add Email template for Rejection TimeSheet mail.

--- EPMLiveCore/EPMLiveCore/API/Notification/APIEmail.cs
+++ EPMLiveCore/EPMLiveCore/API/Notification/APIEmail.cs
@@ -742,94 +742,69 @@ namespace EPMLiveCore.API
 
         public static void sendEmail(int templateID, Hashtable additionalParams, List<String> emailTo, string emailFrom, SPWeb oWeb, bool hideFrom)
         {
-            string body = "";
-            string subject = "";
-
-            Guid siteid = SPContext.Current.Site.ID;
-            Guid webid = SPContext.Current.Web.ID;
-            SPSecurity.RunWithElevatedPrivileges(delegate ()
-            {
-                using (SqlConnection cn = new SqlConnection(CoreFunctions.getConnectionString(oWeb.Site.WebApplication.Id)))
-                {
-                    cn.Open();
-
-                    GetCoreInformation(cn, templateID, out body, out subject, oWeb, oWeb.CurrentUser);
-
-                    foreach (string s in additionalParams.Keys)
-                    {
-                        body = body.Replace("{" + s + "}", additionalParams[s].ToString());
-                        subject = subject.Replace("{" + s + "}", additionalParams[s].ToString());
-                    }
-
-                    SPAdministrationWebApplication spWebAdmin = Microsoft.SharePoint.Administration.SPAdministrationWebApplication.Local;
-                    string sMailSvr = spWebAdmin.OutboundMailServiceInstance?.Server.Address;
-
-                    if (string.IsNullOrEmpty(sMailSvr)) return;
-
-                    using (MailMessage mailMsg = new MailMessage())
-                    {
-
-
-                        if (hideFrom)
-                        {
-                            mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress);
-                        }
-                        else
-                        {
-                            if (string.IsNullOrEmpty(emailFrom))
-                                mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress, oWeb.CurrentUser.Name);
-                            else
-                                mailMsg.From = new MailAddress(oWeb.CurrentUser.Email, oWeb.CurrentUser.Name);
-                        }
-
-                        emailTo.ForEach(i => mailMsg.To.Add(i));
-                        mailMsg.Subject = subject;
-                        mailMsg.Body = body;
-                        mailMsg.IsBodyHtml = true;
-
-                        using (SmtpClient smtpClient = new SmtpClient())
-                        {
-                            smtpClient.Host = sMailSvr;
-                            smtpClient.Send(mailMsg);
-                        }
-                    }
-                }
-            });
-        }
-
-        public static void sendEmail(string body, string subject, string emailTo, SPWeb oWeb)
-        {
             try
             {
+                string body = "";
+                string subject = "";
+
                 Guid siteid = SPContext.Current.Site.ID;
                 Guid webid = SPContext.Current.Web.ID;
                 SPSecurity.RunWithElevatedPrivileges(delegate ()
                 {
-                    SPAdministrationWebApplication spWebAdmin = Microsoft.SharePoint.Administration.SPAdministrationWebApplication.Local;
-                    string sMailSvr = spWebAdmin.OutboundMailServiceInstance?.Server.Address;
-
-                    if (string.IsNullOrEmpty(sMailSvr)) return;
-
-                    using (MailMessage mailMsg = new MailMessage())
+                    using (SqlConnection cn = new SqlConnection(CoreFunctions.getConnectionString(oWeb.Site.WebApplication.Id)))
                     {
-                        mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress, oWeb.CurrentUser.Name);
-                        mailMsg.To.Add(emailTo);
-                        mailMsg.Subject = subject;
-                        mailMsg.Body = body;
-                        mailMsg.IsBodyHtml = true;
-                        using (SmtpClient smtpClient = new SmtpClient())
+                        cn.Open();
+
+                        GetCoreInformation(cn, templateID, out body, out subject, oWeb, oWeb.CurrentUser);
+
+                        foreach (string s in additionalParams.Keys)
                         {
-                            smtpClient.Host = sMailSvr;
-                            smtpClient.Send(mailMsg);
+                            body = body.Replace("{" + s + "}", additionalParams[s].ToString());
+                            subject = subject.Replace("{" + s + "}", additionalParams[s].ToString());
+                        }
+
+                        SPAdministrationWebApplication spWebAdmin = Microsoft.SharePoint.Administration.SPAdministrationWebApplication.Local;
+                        string sMailSvr = spWebAdmin.OutboundMailServiceInstance?.Server.Address;
+
+                        if (string.IsNullOrEmpty(sMailSvr)) return;
+
+                        using (MailMessage mailMsg = new MailMessage())
+                        {
+
+
+                            if (hideFrom)
+                            {
+                                mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress);
+                            }
+                            else
+                            {
+                                if (string.IsNullOrEmpty(emailFrom))
+                                    mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress, oWeb.CurrentUser.Name);
+                                else
+                                    mailMsg.From = new MailAddress(oWeb.CurrentUser.Email, oWeb.CurrentUser.Name);
+                            }
+
+                            emailTo.ForEach(i => mailMsg.To.Add(i));
+                            mailMsg.Subject = subject;
+                            mailMsg.Body = body;
+                            mailMsg.IsBodyHtml = true;
+
+                            using (SmtpClient smtpClient = new SmtpClient())
+                            {
+                                smtpClient.Host = sMailSvr;
+                                smtpClient.Send(mailMsg);
+                            }
                         }
                     }
-
                 });
             }
             catch (Exception ex)
             {
-                LoggingService.WriteTrace("Mail", "TimeSheet Mail", TraceSeverity.Medium, ex.Message);
+                LoggingService.WriteTrace("APIEMail", "Mail", TraceSeverity.Medium, ex.Message);
             }
+            
         }
+
+       
     }
 }
