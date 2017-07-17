fa9b6f10fbb61ff7eb0af320c37da52fc24dce87
Anuj Kumar <anuj.kumar@aurea.com>
2017-07-17 06:57:27 -0700

    Added test case for pfe job.

---
+++ PortfolioEngineCore/PortfolioEngineCore.Tests/PfeJobTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioEngineCore.Tests
+{
+    [TestClass]
+    public class PfeJobTest
+    {
+        [TestMethod]
+        public void PfeJob_Should_Be_Saved_In_Queue()
+        {
+            var sut = new Fakes.StubPfeJob();
+            var messageQueued = false;
+            using (ShimsContext.Create())
+            {
+                var dbrepository = new Fakes.ShimDbRepository()
+                {
+                    QueuePfeJobPfeJob = (job) => { return 1; }
+                };
+                var messageQueue = new Fakes.ShimMsmq()
+                {
+                    QueueString = (input) => messageQueued = true
+                };
+                var output = sut.Queue(dbrepository.Instance, messageQueue.Instance, "TestPath");
+                Assert.AreEqual(output, 1);
+                Assert.IsTrue(messageQueued);
+            }
+        }
+    }
+}
