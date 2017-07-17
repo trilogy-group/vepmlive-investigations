ce7818e07146af033a6294cdf21f047e21939038
Anuj Kumar <anuj.kumar@aurea.com>
2017-07-17 04:45:02 -0700

    Made changes in queue service to dequeue message and process them.

---
+++ EPK_QueueMgr/EPK_QueueMgr/QMSite.cs
@@ -0,0 +1,15 @@
+﻿namespace WE_QueueMgr
+{
+    internal class QMSite
+    {
+        public string basePath;
+        public string connection;
+        public string pid;
+        public string cn;
+        public string WRES_ID;
+        public string userName;
+        public string NTAccount;
+        public string SessionInfo;
+        public string ActiveTraceChannels;
+    }
+}
--- PortfolioEngineCore/PortfolioEngineCore/Base/MessageQueue/Msmq.cs
+++ PortfolioEngineCore/PortfolioEngineCore/Base/MessageQueue/Msmq.cs
@@ -6,16 +6,35 @@ namespace PortfolioEngineCore
     public class Msmq : IMessageQueue
     {
         private readonly string address;
+        private readonly MessageQueue queue;
 
         public Msmq(string queueAddress)
         {
             address = queueAddress;
+            queue = new MessageQueue(queueAddress);
         }
 
         public void Queue(string basePath)
         {
-            var pfeQueue = new MessageQueue(address);
-            pfeQueue.Send(basePath);
+            queue.Send(basePath);
+        }
+
+        public string Receive()
+        {
+            try
+            {
+                var message = queue.Receive(new TimeSpan(0, 0, 1));
+                message.Formatter = new XmlMessageFormatter(new string[] { "System.String, mscorlib" });
+                return message.Body.ToString();
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message == "Timeout for the requested operation has expired.")
+                {
+                    return string.Empty;
+                }
+                throw;                
+            }
         }
     }
 }
