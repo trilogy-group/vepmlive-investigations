e457413f2be58f87e1f8be18a88d84270aefdd72
Anuj Kumar <anuj.kumar@aurea.com>
2017-07-22 07:20:00 -0700

    Added section to validate user roles.

--- Utils/SyncReportGenerator/Program.cs
+++ Utils/SyncReportGenerator/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using System.Configuration;
 using System.IO;
+using Microsoft.SharePoint.Administration.Claims;
 
 namespace SyncReportGenerator
 {
@@ -32,6 +33,7 @@ namespace SyncReportGenerator
                     {
                         try
                         {
+                            LogSuccess($"--------- Report for {site.Url} --------");
                             var reportLibrary = web.Lists["Report Library"] as SPDocumentLibrary;
                             if (!topLevelFolders.Any(x => x.Path == $"/{site.ID}"))
                             {
@@ -40,19 +42,10 @@ namespace SyncReportGenerator
                             else
                             {
                                 LogSuccess($"Tenant folder exists for site with URL '{site.Url}'");
-                                var items = reportLibrary.Items;
-                                var itemsSynced = client.ListChildren($"/{site.ID}", true).ToList();
-                                foreach (SPListItem item in items)
-                                {
-                                    if (!itemsSynced.Any(x => x.Path.Contains($"/{site.ID}/{item.File.Url}")))
-                                    {
-                                        LogError($"Item not synced '{site.Url}{item.File.ServerRelativeUrl}'");
-                                    }
-                                    else
-                                    {
-                                        LogSuccess($"Item synced '{site.Url}{item.File.ServerRelativeUrl}'");
-                                    }
-                                }
+                                ValidateCatalogItems(client, site, reportLibrary);
+                                ValidateRole(client, site, web, "Administrators", "Content Manager");
+                                ValidateRole(client, site, web, "Administrators", "Report Builder");
+                                ValidateRole(client, site, web, "Report Viewers", "Browser");
                             }
                         }
                         catch (Exception exception)
@@ -64,6 +57,72 @@ namespace SyncReportGenerator
             }
 
             Console.Read();
+        }
+
+        private static void ValidateRole(ReportingService2010Extended client, SPSite site, SPWeb web, string role, string ssrsRole)
+        {
+            try
+            {
+                bool inheritParent;
+                var policies = client.GetPolicies($"/{site.ID.ToString()}", out inheritParent).ToList();
+                var groups = web.SiteGroups;
+                var reportViewers = groups.GetByName(role);
+                foreach (SPUser user in reportViewers.Users.OfType<SPUser>().Where(i => i.Name != "System Account"))
+                {
+                    try
+                    {
+                        var loginName = SPClaimProviderManager.Local.DecodeClaim(user.LoginName).Value;
+                        loginName = loginName.Split('\\').Last();
+                        if (!policies.Any(x => x.GroupUserName.ToLower() == loginName.ToLower() && x.Roles.Where(y => y.Name == ssrsRole).Count() == 1))
+                        {
+                            LogError($"User {loginName} not assigned correct permissions of {ssrsRole}");
+                        }
+                        else
+                        {
+                            LogSuccess($"User {loginName} assigned correct permissions of {ssrsRole}");
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        LogError(exception.Message);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message == "Group cannot be found.")
+                {
+                    LogError($"Group {role} cannot be found.");
+                }
+                else
+                {
+                    LogError(exception.Message);
+                }
+            }
+        }
+
+        private static void ValidateCatalogItems(ReportingService2010Extended client, SPSite site, SPDocumentLibrary reportLibrary)
+        {
+            var items = reportLibrary.Items;
+            var itemsSynced = client.ListChildren($"/{site.ID}", true).ToList();
+            foreach (SPListItem item in items)
+            {
+                try
+                {
+                    if (!itemsSynced.Any(x => x.Path.Contains($"/{site.ID}/{item.File.Url}")))
+                    {
+                        LogError($"Item not synced '{site.Url}{item.File.ServerRelativeUrl}'");
+                    }
+                    else
+                    {
+                        LogSuccess($"Item synced '{site.Url}{item.File.ServerRelativeUrl}'");
+                    }
+                }
+                catch (Exception exception)
+                {
+                    LogError(exception.Message);
+                }
+            }
         }
 
         private static void Log(string message, string level)
