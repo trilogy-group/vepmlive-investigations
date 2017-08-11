08e74e541bdad657f5666e34186b930108af689f
Essam Elsayed <essam.elsayed@versata.com>
2017-08-10 22:05:08 -0700

    Add

---
+++ ProjectPublisher2016/ProjectPublisher2016/DummyReference.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Tools;
+namespace ProjectPublisher2016
+{
+    class DummyReference
+    {
+        public DummyReference()
+        {
+            var dummyType = typeof(ActionsPane);
+            Console.WriteLine(dummyType.FullName);
+        }
+    }
+}
