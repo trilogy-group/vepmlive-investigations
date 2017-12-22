0153251e2c16b6199b341b06bc9814d7858f7384
Essam Elsayed <essam.elsayed@versata.com>
2017-12-22 13:41:45 -0800

    feature/EPMLCID-15527 fix UI tests

--- EMPLiveAutomationTesting/lyris-automation-customer/src/test/java/com/ipm/tests/SmokeVerificationStepDefinition.java
+++ EMPLiveAutomationTesting/lyris-automation-customer/src/test/java/com/ipm/tests/SmokeVerificationStepDefinition.java
@@ -323,7 +323,7 @@ public class SmokeVerificationStepDefinition {
         WebElement element = driver.findElement(By.xpath(".//*[@id='epm-social-stream']/div/ul[2]/li[6]/a"));
         JavascriptExecutor executor = (JavascriptExecutor) driver;
         executor.executeScript("arguments[0].click();", element);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("dialogTitleSpan")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.ClassName("ms-dlgFrame")));
     }
 
     @Then("^A new item page form should be displayed$")
