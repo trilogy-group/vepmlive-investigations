c577f0e83e0580d98088265cad40a4f060b1a7da
Essam Elsayed <essam.elsayed@versata.com>
2017-12-22 13:24:58 -0800

    feature/EPMLCID-15527 fix UI tests

--- EMPLiveAutomationTesting/lyris-automation-customer/src/test/java/com/ipm/tests/ProjectPlannerDefinition.java
+++ EMPLiveAutomationTesting/lyris-automation-customer/src/test/java/com/ipm/tests/ProjectPlannerDefinition.java
@@ -690,7 +690,7 @@ public class ProjectPlannerDefinition {
     @And("^I click on Blank Plan$")
     public void iClickOnBlankPlan() throws Throwable {
         checkPageIsReady();
-		driver.switchTo().frame(1);
+		driver.SwitchTo().Frame(driver.FindElement(By.ClassName("ms-dlgFrame")));
         WebDriverWait wait = new WebDriverWait(driver, 60);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='ctl00_PlaceHolderMain_pnlPlanner']/div[2]/div[1]/a[2]")));
         driver.findElement(By.xpath(".//*[@id='ctl00_PlaceHolderMain_pnlPlanner']/div[2]/div[1]/a[2]")).click();
