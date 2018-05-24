49c56eb9628847caea880932303bc5a744e86a12
Ajay suwalka <ajay.suwalka@aurea.com>
2018-05-24 07:51:07 +0530

    Skeleton EPMLCID-18175

--- EMPLiveAutomationTesting/protractor/e2e/page-objects/pages/settings/settings-page.constants.ts
+++ EMPLiveAutomationTesting/protractor/e2e/page-objects/pages/settings/settings-page.constants.ts
@@ -1,19 +1,35 @@
 export class SettingsPageConstants {
+    static readonly epmLive = `(EPM Live)`;
     static get generalSettings() {
-        const epmLive = `(EPM Live)`;
         return {
             listName: `List name, description and navigation`,
             versionSettings: `Versioning settings`,
             advancedSettings: `Advanced settings`,
             validationSettings: `Validation settings`,
             audienceTargetingSettings: `Audience targeting settings`,
-            manageEditableFields: `Manage Editable Fields ${epmLive}`,
-            portfolioEngineSettings: `PortfolioEngine Settings ${epmLive}`,
-            totalFieldSettings: `Total Field Settings ${epmLive}`,
-            generalSettings: `General Settings ${epmLive}`,
-            lookupSettings: `Lookup Settings ${epmLive}`,
-            integration: `Integration ${epmLive}`,
-            createDashboard: `Create Dashboard ${epmLive}`,
+            manageEditableFields: `Manage Editable Fields ${this.epmLive}`,
+            portfolioEngineSettings: `PortfolioEngine Settings ${this.epmLive}`,
+            totalFieldSettings: `Total Field Settings ${this.epmLive}`,
+            generalSettings: `General Settings ${this.epmLive}`,
+            lookupSettings: `Lookup Settings ${this.epmLive}`,
+            integration: `Integration ${this.epmLive}`,
+            createDashboard: `Create Dashboard ${this.epmLive}`
+        };
+    }
+
+    static get permissionsAndManagement() {
+        return {
+            deleteThisList: 'Delete this list',
+            saveListAsTemplate: 'Save list as template',
+            permissionsForThisList: 'Permissions for this list',
+            workflowSettings: 'Workflow Settings',
+            viewPermissionSettings: `View Permission Settings ${this.epmLive}`
+        };
+    }
+
+    static get communications() {
+        return {
+            rssSettings: 'RSS settings'
         };
     }
 }
