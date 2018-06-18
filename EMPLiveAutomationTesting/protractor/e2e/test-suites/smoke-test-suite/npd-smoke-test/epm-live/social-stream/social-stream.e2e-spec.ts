feedcd9489a29e9ed361143c40911e2afe7ed7b8
Ajay suwalka <ajay.suwalka@aurea.com>
2018-06-18 20:34:48 +0530

    HEadless browser

--- EMPLiveAutomationTesting/protractor/e2e/test-suites/smoke-test-suite/npd-smoke-test/epm-live/social-stream/social-stream.e2e-spec.ts
+++ EMPLiveAutomationTesting/protractor/e2e/test-suites/smoke-test-suite/npd-smoke-test/epm-live/social-stream/social-stream.e2e-spec.ts
@@ -12,8 +12,6 @@ import {ElementHelper} from '../../../../../components/html/element-helper';
 import {TextboxHelper} from '../../../../../components/html/textbox-helper';
 import {ProjectItemPageHelper} from '../../../../../page-objects/pages/items-page/project-item/project-item-page.helper';
 import {ProjectItemPageConstants} from '../../../../../page-objects/pages/items-page/project-item/project-item-page.constants';
-import {MyTimeOffPageConstants} from '../../../../../page-objects/pages/my-workplace/my-time-off/my-time-off-page.constants';
-import {MyTimeOffPageHelper} from '../../../../../page-objects/pages/my-workplace/my-time-off/my-time-off-page.helper';
 import {LinkPageConstants} from '../../../../../page-objects/pages/my-workplace/link/link-page.constants';
 import {MyWorkplacePage} from '../../../../../page-objects/pages/my-workplace/my-workplace.po';
 import {LinkPageHelper} from '../../../../../page-objects/pages/my-workplace/link/link-page.helper';
@@ -138,42 +136,6 @@ describe(SuiteNames.smokeTestSuite, () => {
         await expect(await PageHelper.isElementPresent(ElementHelper.getElementByText(projectNameValue)))
             .toBe(true, ValidationsHelper.getLabelDisplayedValidation(projectNameValue));
 
-    });
-
-    it('Add Time Off - [891123]', async () => {
-        const stepLogger = new StepLogger(891123);
-
-        stepLogger.step('Click on "More" Link on the top menu bar');
-        await ElementHelper.click(HomePage.toolBarMenuItems.more);
-
-        stepLogger.step('Click on "Time Off" Link on the top menu bar');
-        await ElementHelper.click(HomePage.toolBarMenuItems.timeOff);
-
-        stepLogger.verification('"Time Off - New Item" window is displayed');
-        await WaitHelper.getInstance().waitForElementToBeDisplayed(CommonPage.dialogTitle);
-        await expect(await CommonPage.dialogTitle.getText())
-            .toBe(MyTimeOffPageConstants.pageName,
-                ValidationsHelper.getPageDisplayedValidation(MyTimeOffPageConstants.pageName));
-
-        await PageHelper.switchToFrame(CommonPage.contentFrame);
-
-        stepLogger.step(`Enter/Select below details in 'My Time Off' page`);
-        const uniqueId = PageHelper.getUniqueId();
-        const labels = MyTimeOffPageConstants.inputLabels;
-        const input = MyTimeOffPageConstants.inputValues;
-        const title = `${labels.title} ${uniqueId}`;
-        const timeOffType = MyTimeOffPageConstants.timeOffTypes.holiday;
-        const requestor = input.requestorValue;
-        const startDate = input.startDate;
-        const finishDate = input.finishDate;
-        await MyTimeOffPageHelper.fillFormAndVerify(title, timeOffType, requestor, startDate, finishDate, stepLogger);
-
-        await PageHelper.switchToDefaultContent();
-
-        stepLogger.verification('Newly created Time off displayed in Home page');
-        await WaitHelper.getInstance().waitForElementToBeDisplayed(ElementHelper.getElementByText(title));
-        await expect(await PageHelper.isElementPresent(ElementHelper.getElementByText(title)))
-            .toBe(true, ValidationsHelper.getLabelDisplayedValidation(title));
     });
 
     it('Add an item from Social Stream - [1124294]', async () => {
