eb6f64da5ed980777e2568564a4058aeaf453fe9
Rezgar Cadro <rezgar.cadro@devfactory.com>
2018-07-26 06:59:57 +0000

    Preparing structure for refactoring CAGrids: CADataGridCacheBase/CATopGridTemp

---
+++ PortfolioEngineCore/WorkEnginePPM/WebServices/CADataCache/CADataGridCacheBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CostDataValues;
+
+namespace CADataCache
+{
+    internal abstract class CADataGridCacheBase : GridBase<clsPeriodData, clsDetailRowData>
+    {
+        public readonly bool ShowFTEs;
+        public readonly bool UseQuantity;
+        public readonly bool UseCost;
+        public readonly bool ShowCostDetailed;
+        private readonly IList<CATGRow> _displayList;
+        private readonly IList<clsColDisp> _columns;
+
+        public CADataGridCacheBase(
+            bool showFTEs, 
+            bool useQuantity,
+            bool useCost, 
+            bool showCostDetailed, 
+            IList<CATGRow> displayList,
+            IList<clsColDisp> columns)
+        {
+            ShowFTEs = showFTEs;
+            UseQuantity = useQuantity;
+            UseCost = useCost;
+            ShowCostDetailed = showCostDetailed;
+            _displayList = displayList;
+            _columns = columns;
+        }
+
+        protected string CleanupString(string input)
+        {
+            return RemoveCharacters(input, "!@#$%^&*()_+-={}[]|:;'?/~` '\r\n\"\\");
+        }
+    }
+}
---
+++ PortfolioEngineCore/WorkEnginePPM/WebServices/CADataCache/CATopGrid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CostDataValues;
+
+namespace CADataCache
+{
+    internal class CATopGridTemp : CADataGridCacheBase
+    {
+        public CATopGridTemp(
+            bool showFTEs, 
+            bool useQuantity,
+            bool useCost, 
+            bool showCostDetailed,
+            IList<CATGRow> displayList,
+            IList<clsColDisp> columns) 
+        : base(showFTEs, useQuantity, useCost, showCostDetailed, displayList, columns)
+        {
+        }
+
+        protected override void AddDetailRow(clsDetailRowData detailRowData, int rowId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddPeriodColumns(IEnumerable<clsPeriodData> periods)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CheckIfDetailRowShouldBeAdded(clsDetailRowData detailRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InitializeGridData(GridRenderingTypes renderingType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InitializeGridLayout(GridRenderingTypes renderingType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string ResolvePeriodId(clsPeriodData periodData, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
