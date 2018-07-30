69db42cb2937a9153010adf4d38bdf33fc94301e
Rezgar Cadro <rezgar.cadro@devfactory.com>
2018-07-30 13:02:58 +0000

    TargetRowData/clsTargetRowData refactored to use a common interface

--- PortfolioEngineCore/PortfolioEngineCore/Analyzers/clsCostData.cs
+++ PortfolioEngineCore/PortfolioEngineCore/Analyzers/clsCostData.cs
@@ -120,26 +120,32 @@ namespace CostDataValues
         }
     }
     [Serializable()]
-    public class clsTargetRowData
+    public class clsTargetRowData : ITargetRowData
     {
-        public int CT_ID;
-        public int BC_UID;
-        public int BC_ROLE_UID;
-        public int BC_SEQ;
-        public string MC_Val;
-        public int CAT_UID;
-        public string CT_Name, Cat_Name, Role_Name, MC_Name, FullCatName, CC_Name, FullCCName, Grouping;
-        public bool bGroupRow;
-
-        public int m_rt;
-        public string m_rt_name;
-
-
-        public string sUoM;
-        public double[] zCost, zValue, zFTE;
-        public int[] OCVal;
-        public string[] Text_OCVal;
-        public string[] TXVal;
+        public int CT_ID { get; set; }
+        public int BC_UID { get; set; }
+        public int BC_ROLE_UID { get; set; }
+        public int BC_SEQ { get; set; }
+        public string MC_Val { get; set; }
+        public int CAT_UID { get; set; }
+        public string CT_Name { get; set; }
+        public string Cat_Name { get; set; }
+        public string Role_Name { get; set; }
+        public string MC_Name { get; set; }
+        public string FullCatName { get; set; }
+        public string CC_Name { get; set; }
+        public string FullCCName { get; set; }
+        public string Grouping { get; set; }
+        public bool bGroupRow { get; set; }
+        public int m_rt { get; set; }
+        public string m_rt_name { get; set; }
+        public string sUoM { get; set; }
+        public double[] zCost { get; set; }
+        public double[] zValue { get; set; }
+        public double[] zFTE { get; set; }
+        public int[] OCVal { get; set; }
+        public string[] Text_OCVal { get; set; }
+        public string[] TXVal { get; set; }
     }
     [Serializable()]
     public class clsDetailRowData : BaseDetailRowData
