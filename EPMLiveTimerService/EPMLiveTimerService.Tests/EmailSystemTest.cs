7e648f0fdcf14b75d0629c45ee778c145fcdb9f0
juliano.bueno <juliano.bueno@devfactory.com>
2018-07-30 17:50:09 -0400

    CC-76367: Refactor

---
+++ EPMLiveTimerService/EPMLiveTimerService.Tests/EmailSystemTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimerService;
+
+namespace EPMLiveTimerService.Tests
+{
+    [TestClass]
+    public class EmailSystemTest
+    {
+        private string _body;
+        private string _subject;
+        private bool _hideFrom;
+        private SPUser _fromUser;
+        private SPUser _toUser;
+        private IDisposable _context;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _context = ShimsContext.Create();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _context?.Dispose();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void SendFullEmail_WhenHideFromIsTrue_ThrowsException()
+        {
+            // Arrange
+            ShimSPAdministrationWebApplication.LocalGet = () => new ShimSPAdministrationWebApplication();
+
+            // Act
+            EmailSystem.SendFullEmail(_body, _subject, _hideFrom, _fromUser, _toUser);
+
+            // Assert
+            // Expected ArgumentNullException
+        }
+    }
+}
--- EPMLiveTimerService/EPMLiveTimerService/EmailSystem.cs
+++ EPMLiveTimerService/EPMLiveTimerService/EmailSystem.cs
@@ -1,61 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Mail;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.Administration;
-using System.Net;
-using System.Net.Mail;
+using EpmCoreFunctions = EPMLiveCore.CoreFunctions;
 
 namespace TimerService
 {
-    class EmailSystem
+    internal class EmailSystem
     {
+        private const string NameKey = "{ToUser_Name}";
+        private const string EmailKey = "{ToUser_Email}";
+        private const string UsernameKey = "{ToUser_Username}";
+
         public static void SendFullEmail(string body, string subject, bool hideFrom, SPUser fromUser, SPUser toUser)
         {
             try
             {
-                SPAdministrationWebApplication spWebAdmin = Microsoft.SharePoint.Administration.SPAdministrationWebApplication.Local;
-                string sMailSvr = spWebAdmin.OutboundMailServiceInstance.Server.Address;
+                var spWebAdmin = SPAdministrationWebApplication.Local;
+                var sMailSvr = spWebAdmin.OutboundMailServiceInstance.Server.Address;
 
-                System.Net.Mail.MailMessage mailMsg = new MailMessage();
-                if(hideFrom)
+                using (var mailMsg = new MailMessage())
                 {
-                    mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress);
-                }
-                else
-                {
-                    if(fromUser.Email == "")
+                    if (hideFrom)
                     {
-                        mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress, fromUser.Name);
+                        mailMsg.From = new MailAddress(spWebAdmin.OutboundMailSenderAddress);
                     }
                     else
                     {
-                        mailMsg.From = new MailAddress(fromUser.Email, fromUser.Name);
+                        mailMsg.From = fromUser.Email == string.Empty
+                            ? new MailAddress(spWebAdmin.OutboundMailSenderAddress, fromUser.Name)
+                            : new MailAddress(fromUser.Email, fromUser.Name);
+                    }
+
+                    body = body.Replace(NameKey, toUser.Name);
+                    body = body.Replace(EmailKey, toUser.Email);
+                    body = body.Replace(UsernameKey, EpmCoreFunctions.GetJustUsername(toUser.LoginName));
+
+                    subject = subject.Replace(NameKey, toUser.Name);
+                    subject = subject.Replace(EmailKey, toUser.Email);
+                    subject = subject.Replace(UsernameKey, EpmCoreFunctions.GetJustUsername(toUser.LoginName));
+
+                    mailMsg.To.Add(new MailAddress(toUser.Email));
+                    mailMsg.Subject = subject;
+                    mailMsg.Body = body;
+                    mailMsg.IsBodyHtml = true;
+                    
+                    using (var smtpClient = new SmtpClient())
+                    {
+                        smtpClient.Host = sMailSvr;
+                        smtpClient.Send(mailMsg);
                     }
                 }
-
-                body = body.Replace("{ToUser_Name}", toUser.Name);
-                body = body.Replace("{ToUser_Email}", toUser.Email);
-                body = body.Replace("{ToUser_Username}", EPMLiveCore.CoreFunctions.GetJustUsername(toUser.LoginName));
-
-                subject = subject.Replace("{ToUser_Name}", toUser.Name);
-                subject = subject.Replace("{ToUser_Email}", toUser.Email);
-                subject = subject.Replace("{ToUser_Username}", EPMLiveCore.CoreFunctions.GetJustUsername(toUser.LoginName));
-
-                mailMsg.To.Add(new MailAddress(toUser.Email));
-                mailMsg.Subject = subject;
-                mailMsg.Body = body;
-                mailMsg.IsBodyHtml = true;
-
-                SmtpClient smtpClient = new SmtpClient();
-
-                smtpClient.Host = sMailSvr;
-
-                smtpClient.Send(mailMsg);
             }
-            catch(Exception ex) { throw new Exception(ex.Message); }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
-
     }
 }
