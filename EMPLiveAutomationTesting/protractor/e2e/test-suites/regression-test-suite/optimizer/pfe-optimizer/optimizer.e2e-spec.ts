49bfd965d7ac72c94313d68812720b41dfd02332
snarayanaswamy <Sharath.narayanaswamy@aurea.com>
2018-08-09 09:05:48 +0530

    SKYVERA-13

--- EMPLiveAutomationTesting/protractor/e2e/test-suites/regression-test-suite/optimizer/pfe-optimizer/optimizer.e2e-spec.ts
+++ EMPLiveAutomationTesting/protractor/e2e/test-suites/regression-test-suite/optimizer/pfe-optimizer/optimizer.e2e-spec.ts
@@ -16,80 +16,80 @@ describe(SuiteNames.regressionTestSuite, () => {
         await loginPage.goToAndLogin();
     });
 
-    // it('Verify the Close button of the Optimizer page  - [744351]', async () => {
-    //     const stepLogger = new StepLogger(744351);
-    //     // Step 1 is inside the below function
-    //     await CommonPageHelper.navigateToItemPageUnderNavigation(
-    //         HomePage.navigation.projects.projects,
-    //         CommonPage.pageHeaders.projects.projectsCenter,
-    //         CommonPageConstants.pageHeaders.projects.projectCenter,
-    //         stepLogger);
-    //     await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
-    //     // Step 2 is inside the below function
-    //     await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
-    //     stepLogger.stepId(3);
-    //     await CommonPageHelper.gotoOptimizer(stepLogger);
-    //     await OptimizerPageHelper.verifyOptimizerPageOpened(stepLogger);
-    //     stepLogger.stepId(4);
-    //     await OptimizerPageHelper.closeOptimizerWindowFromOptimizerTab(stepLogger);
-    //     await OptimizerPageHelper.verifyOptimizerWindowClosed(stepLogger);
-    // });
-    //
-    // it('Verify that when user select only single project  - [744353]', async () => {
-    //     const stepLogger = new StepLogger(744353);
-    //     // Step 1 is inside the below function
-    //     await CommonPageHelper.navigateToItemPageUnderNavigation(
-    //         HomePage.navigation.projects.projects,
-    //         CommonPage.pageHeaders.projects.projectsCenter,
-    //         CommonPageConstants.pageHeaders.projects.projectCenter,
-    //         stepLogger);
-    //     await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
-    //     // Step 2 is inside the below function
-    //     await CommonPageHelper.selectRecordFromGrid(stepLogger);
-    //     stepLogger.stepId(3);
-    //     await OptimizerPageHelper.gotoConfigureSection(stepLogger);
-    //     await OptimizerPageHelper.verifyAlertMessageForSingleProjectSelection(stepLogger);
-    // });
-    //
-    // it('Verify that Saved Strategy name displayed in the Current Strategy drop down box  - [744366]', async () => {
-    //     const stepLogger = new StepLogger(744366);
-    //     // Step 1 is inside the below function
-    //     await CommonPageHelper.navigateToItemPageUnderNavigation(
-    //         HomePage.navigation.projects.projects,
-    //         CommonPage.pageHeaders.projects.projectsCenter,
-    //         CommonPageConstants.pageHeaders.projects.projectCenter,
-    //         stepLogger);
-    //     await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
-    //     // Step 2 is inside the below function
-    //     await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
-    //     stepLogger.stepId(3);
-    //     await CommonPageHelper.gotoOptimizer(stepLogger);
-    //     await OptimizerPageHelper.openSaveStrategyPopup(stepLogger);
-    //     await OptimizerPageHelper.verifySaveStrtegyPopupDisplayed(stepLogger);
-    //     stepLogger.stepId(4);
-    //     const strategyName = await OptimizerPageHelper.enterNewStrategyNameAndSubmit(stepLogger);
-    //     stepLogger.stepId(5);
-    //     await OptimizerPageHelper.verifyCurrentStrategyName(strategyName, stepLogger);
-    // });
-    //
-    // it('Verify the Delete Strategy button  - [744369]', async () => {
-    //     const stepLogger = new StepLogger(744369);
-    //     // Step 1 is inside the below function
-    //     await CommonPageHelper.navigateToItemPageUnderNavigation(
-    //         HomePage.navigation.projects.projects,
-    //         CommonPage.pageHeaders.projects.projectsCenter,
-    //         CommonPageConstants.pageHeaders.projects.projectCenter,
-    //         stepLogger);
-    //     await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
-    //     // Step 2 is inside the below function
-    //     await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
-    //     stepLogger.stepId(3);
-    //     await CommonPageHelper.gotoOptimizer(stepLogger);
-    //     await OptimizerPageHelper.verifyOptimizerPageOpened(stepLogger);
-    //     stepLogger.stepId(4);
-    //     await OptimizerPageHelper.openDeleteStrategyPopup(stepLogger);
-    //     await OptimizerPageHelper.verifyDeleteStrategyPopup(stepLogger);
-    // });
+    it('Verify the Close button of the Optimizer page  - [744351]', async () => {
+        const stepLogger = new StepLogger(744351);
+        // Step 1 is inside the below function
+        await CommonPageHelper.navigateToItemPageUnderNavigation(
+            HomePage.navigation.projects.projects,
+            CommonPage.pageHeaders.projects.projectsCenter,
+            CommonPageConstants.pageHeaders.projects.projectCenter,
+            stepLogger);
+        await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
+        // Step 2 is inside the below function
+        await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
+        stepLogger.stepId(3);
+        await CommonPageHelper.gotoOptimizer(stepLogger);
+        await OptimizerPageHelper.verifyOptimizerPageOpened(stepLogger);
+        stepLogger.stepId(4);
+        await OptimizerPageHelper.closeOptimizerWindowFromOptimizerTab(stepLogger);
+        await OptimizerPageHelper.verifyOptimizerWindowClosed(stepLogger);
+    });
+
+    it('Verify that when user select only single project  - [744353]', async () => {
+        const stepLogger = new StepLogger(744353);
+        // Step 1 is inside the below function
+        await CommonPageHelper.navigateToItemPageUnderNavigation(
+            HomePage.navigation.projects.projects,
+            CommonPage.pageHeaders.projects.projectsCenter,
+            CommonPageConstants.pageHeaders.projects.projectCenter,
+            stepLogger);
+        await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
+        // Step 2 is inside the below function
+        await CommonPageHelper.selectRecordFromGrid(stepLogger);
+        stepLogger.stepId(3);
+        await OptimizerPageHelper.gotoConfigureSection(stepLogger);
+        await OptimizerPageHelper.verifyAlertMessageForSingleProjectSelection(stepLogger);
+    });
+
+    it('Verify that Saved Strategy name displayed in the Current Strategy drop down box  - [744366]', async () => {
+        const stepLogger = new StepLogger(744366);
+        // Step 1 is inside the below function
+        await CommonPageHelper.navigateToItemPageUnderNavigation(
+            HomePage.navigation.projects.projects,
+            CommonPage.pageHeaders.projects.projectsCenter,
+            CommonPageConstants.pageHeaders.projects.projectCenter,
+            stepLogger);
+        await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
+        // Step 2 is inside the below function
+        await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
+        stepLogger.stepId(3);
+        await CommonPageHelper.gotoOptimizer(stepLogger);
+        await OptimizerPageHelper.openSaveStrategyPopup(stepLogger);
+        await OptimizerPageHelper.verifySaveStrtegyPopupDisplayed(stepLogger);
+        stepLogger.stepId(4);
+        const strategyName = await OptimizerPageHelper.enterNewStrategyNameAndSubmit(stepLogger);
+        stepLogger.stepId(5);
+        await OptimizerPageHelper.verifyCurrentStrategyName(strategyName, stepLogger);
+    });
+
+    it('Verify the Delete Strategy button  - [744369]', async () => {
+        const stepLogger = new StepLogger(744369);
+        // Step 1 is inside the below function
+        await CommonPageHelper.navigateToItemPageUnderNavigation(
+            HomePage.navigation.projects.projects,
+            CommonPage.pageHeaders.projects.projectsCenter,
+            CommonPageConstants.pageHeaders.projects.projectCenter,
+            stepLogger);
+        await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
+        // Step 2 is inside the below function
+        await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
+        stepLogger.stepId(3);
+        await CommonPageHelper.gotoOptimizer(stepLogger);
+        await OptimizerPageHelper.verifyOptimizerPageOpened(stepLogger);
+        stepLogger.stepId(4);
+        await OptimizerPageHelper.openDeleteStrategyPopup(stepLogger);
+        await OptimizerPageHelper.verifyDeleteStrategyPopup(stepLogger);
+    });
 
     it('Verify the content of label name "Which fields will be used as filters? of Optimizer configuration screen. - [744356]',
         async () => {
