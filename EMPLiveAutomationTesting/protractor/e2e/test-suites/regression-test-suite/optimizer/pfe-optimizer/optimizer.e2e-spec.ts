92ea044847af6a8ae75c2717dca846abb6a5f4d0
snarayanaswamy <Sharath.narayanaswamy@aurea.com>
2018-08-10 00:00:56 +0530

    SKYVERA-46

--- EMPLiveAutomationTesting/protractor/e2e/test-suites/regression-test-suite/optimizer/pfe-optimizer/optimizer.e2e-spec.ts
+++ EMPLiveAutomationTesting/protractor/e2e/test-suites/regression-test-suite/optimizer/pfe-optimizer/optimizer.e2e-spec.ts
@@ -227,4 +227,24 @@ describe(SuiteNames.regressionTestSuite, () => {
         stepLogger.stepId(4);
         await OptimizerPageHelper.verfyCurrentStrategyDropdown(stepLogger);
     });
+
+    it('Verify the Select column button. - [744384]', async () => {
+        const stepLogger = new StepLogger(744384);
+        // Step 1 is inside the below function
+        await CommonPageHelper.navigateToItemPageUnderNavigation(
+            HomePage.navigation.projects.projects,
+            CommonPage.pageHeaders.projects.projectsCenter,
+            CommonPageConstants.pageHeaders.projects.projectCenter,
+            stepLogger);
+        await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
+        // Step 2 is inside the below function
+        await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
+        stepLogger.stepId(3);
+        await CommonPageHelper.gotoOptimizer(stepLogger);
+        await OptimizerPageHelper.clickViewTab(stepLogger);
+        await OptimizerPageHelper.verifyViewPageOpened(stepLogger);
+        stepLogger.stepId(4);
+        await OptimizerPageHelper.clickSelectColumns(stepLogger);
+        await OptimizerPageHelper.verifySelectColumnsPopup(stepLogger);
+    });
 });
