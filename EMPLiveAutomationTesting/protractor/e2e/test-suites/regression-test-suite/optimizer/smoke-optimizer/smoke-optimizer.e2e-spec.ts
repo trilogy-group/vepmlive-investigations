84441bc39f5d8f40b2101b4250ca52f593bf343a
snarayanaswamy <Sharath.narayanaswamy@aurea.com>
2018-08-17 08:41:35 +0530

    SKYVERA-87

--- EMPLiveAutomationTesting/protractor/e2e/test-suites/regression-test-suite/optimizer/smoke-optimizer/smoke-optimizer.e2e-spec.ts
+++ EMPLiveAutomationTesting/protractor/e2e/test-suites/regression-test-suite/optimizer/smoke-optimizer/smoke-optimizer.e2e-spec.ts
@@ -170,4 +170,24 @@ describe(SuiteNames.regressionTestSuite, () => {
         await OptimizerPageHelper.clickViewTab(stepLogger);
         await OptimizerPageHelper.verifyViewTabContent(stepLogger);
     });
+
+    fit('Verify the on Delete View button. - [744414]', async () => {
+        const stepLogger = new StepLogger(744414);
+        // Step 1 is inside the below function
+        await CommonPageHelper.navigateToItemPageUnderNavigation(
+            HomePage.navigation.projects.projects,
+            CommonPage.pageHeaders.projects.projectsCenter,
+            CommonPageConstants.pageHeaders.projects.projectCenter,
+            stepLogger);
+        await CommonPageHelper.verifyProjectCenterDisplayed(stepLogger);
+        // Step 2 is inside the below function
+        await CommonPageHelper.selectTwoRecordsFromGrid(stepLogger);
+        stepLogger.stepId(3);
+        await CommonPageHelper.gotoOptimizer(stepLogger);
+        await OptimizerPageHelper.verifyOptimizerPageOpened(stepLogger);
+        stepLogger.stepId(4);
+        await OptimizerPageHelper.clickViewTab(stepLogger);
+        await OptimizerPageHelper.clickDeleteView(stepLogger);
+        await OptimizerPageHelper.verifyDeleteViewPopup(stepLogger);
+    });
 });
