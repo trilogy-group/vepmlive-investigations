a12d1a14875c9b2c2425cdce378232d207c76a1e
Vladimir Kulgun <vladimir.kulgun@devfactory.com>
2018-08-22 10:39:28 +0300

    CC-75626

--- EPMLiveCore/EPMLiveCore/ListDisplaySettingIterator.cs
+++ EPMLiveCore/EPMLiveCore/ListDisplaySettingIterator.cs
@@ -1204,6 +1204,7 @@ namespace EPMLiveCore
                     Controls[i]?.Dispose();
                 }
             }
+            userPanel?.Dispose();
             permissionPanel?.Dispose();
             base.Dispose();
         }
