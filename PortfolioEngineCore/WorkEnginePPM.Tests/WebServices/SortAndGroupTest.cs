d44dd19c13ed615d6c224815330e6d7c9704b449
Adriano de Oliveira Cipriano <adriano.cipriano@aurea.com>
2018-08-27 20:19:31 +0000

    exclude test class from Code Coverage

--- PortfolioEngineCore/WorkEnginePPM.Tests/WebServices/SortAndGroupTest.cs
+++ PortfolioEngineCore/WorkEnginePPM.Tests/WebServices/SortAndGroupTest.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shouldly;
 
 namespace WorkEnginePPM.Tests.WebServices
 {
     [TestClass]
+    [ExcludeFromCodeCoverage]
     public class SortAndGroupTest
     {
         private SortAndGroup _testObj;
