e2bf7a03d91d0c4cf84628622c505185b1de82a1
Vladimir Kulgun <vladimir.kulgun@devfactory.com>
2018-09-14 00:29:28 +0300

    CC-75660

--- EPMLiveCore/EPMLiveCore/API/PlatformIntegration.cs
+++ EPMLiveCore/EPMLiveCore/API/PlatformIntegration.cs
@@ -68,34 +68,36 @@ namespace EPMLiveCore.API
                             SqlConnection cn = new SqlConnection(EPMLiveCore.CoreFunctions.getConnectionString(web.Site.WebApplication.Id));
                             cn.Open();
 
-                            SqlCommand cmd = new SqlCommand("DELETE FROM PLATFORMINTEGRATIONS where PlatformIntegrationId=@id", cn);
-                            cmd.Parameters.AddWithValue("@id", doc.FirstChild.Attributes["IntID"].Value);
-                            cmd.ExecuteNonQuery();
+                            SqlCommand command = new SqlCommand("DELETE FROM PLATFORMINTEGRATIONS where PlatformIntegrationId=@id", cn);
+                            command.Parameters.AddWithValue("@id", doc.FirstChild.Attributes["IntID"].Value);
+                            command.ExecuteNonQuery();
 
-                            cmd = new SqlCommand("INSERT INTO PLATFORMINTEGRATIONS (PlatformIntegrationId,ListId,IntegrationKey,IntegrationUrl) VALUES (@id,@listid,@key,@url)", cn);
-                            cmd.Parameters.AddWithValue("@id", doc.FirstChild.Attributes["IntID"].Value);
-                            cmd.Parameters.AddWithValue("@listid", doc.FirstChild.Attributes["List"].Value);
-                            cmd.Parameters.AddWithValue("@url", doc.FirstChild.Attributes["APIUrl"].Value);
-                            cmd.Parameters.AddWithValue("@key", doc.FirstChild.Attributes["IntKey"].Value);
-                            cmd.ExecuteNonQuery();
+                            command = new SqlCommand("INSERT INTO PLATFORMINTEGRATIONS (PlatformIntegrationId,ListId,IntegrationKey,IntegrationUrl) VALUES (@id,@listid,@key,@url)", cn);
+                            command.Parameters.AddWithValue("@id", doc.FirstChild.Attributes["IntID"].Value);
+                            command.Parameters.AddWithValue("@listid", doc.FirstChild.Attributes["List"].Value);
+                            command.Parameters.AddWithValue("@url", doc.FirstChild.Attributes["APIUrl"].Value);
+                            command.Parameters.AddWithValue("@key", doc.FirstChild.Attributes["IntKey"].Value);
+                            command.ExecuteNonQuery();
 
-                            cmd = new SqlCommand("INSERT INTO PLATFORMINTEGRATIONLOG (PlatformIntegrationId, DTLOGGED, MESSAGE, LOGLEVEL) VALUES (@intid, GETDATE(), 'Successfully installed integration', 10)", cn);
-                            cmd.Parameters.AddWithValue("@intid", doc.FirstChild.Attributes["IntID"].Value);
-                            cmd.ExecuteNonQuery();
+                            command = new SqlCommand("INSERT INTO PLATFORMINTEGRATIONLOG (PlatformIntegrationId, DTLOGGED, MESSAGE, LOGLEVEL) VALUES (@intid, GETDATE(), 'Successfully installed integration', 10)", cn);
+                            command.Parameters.AddWithValue("@intid", doc.FirstChild.Attributes["IntID"].Value);
+                            command.ExecuteNonQuery();
 
      
                             foreach (XmlNode ndControl in doc.FirstChild.SelectSingleNode("Controls").SelectNodes("Control"))
                             {
-                                cmd = new SqlCommand("INSERT INTO PLATFORMINTEGRATIONCONTROLS (PlatformIntegrationId, ControlId, DisplayName, Image, Global, ButtonStyle, WindowStyle) VALUES (@PlatformIntegrationId, @ControlId, @DisplayName, @Image, @Global, @ButtonStyle, @WindowStyle)", cn);
-                                cmd.Parameters.AddWithValue("@PlatformIntegrationId", doc.FirstChild.Attributes["IntID"].Value);
-                                cmd.Parameters.AddWithValue("@ControlId", ndControl.Attributes["Id"].Value);
-                                cmd.Parameters.AddWithValue("@DisplayName", ndControl.Attributes["DisplayName"].Value);
-                                cmd.Parameters.AddWithValue("@Image", ndControl.Attributes["Image"].Value);
-                                cmd.Parameters.AddWithValue("@Global", ndControl.Attributes["Global"].Value);
-                                cmd.Parameters.AddWithValue("@ButtonStyle", ndControl.Attributes["ButtonStyle"].Value);
-                                cmd.Parameters.AddWithValue("@WindowStyle", ndControl.Attributes["WindowStyle"].Value);
+                                using (command = new SqlCommand("INSERT INTO PLATFORMINTEGRATIONCONTROLS (PlatformIntegrationId, ControlId, DisplayName, Image, Global, ButtonStyle, WindowStyle) VALUES (@PlatformIntegrationId, @ControlId, @DisplayName, @Image, @Global, @ButtonStyle, @WindowStyle)", cn))
+                                {
+                                    command.Parameters.AddWithValue("@PlatformIntegrationId", doc.FirstChild.Attributes["IntID"].Value);
+                                    command.Parameters.AddWithValue("@ControlId", ndControl.Attributes["Id"].Value);
+                                    command.Parameters.AddWithValue("@DisplayName", ndControl.Attributes["DisplayName"].Value);
+                                    command.Parameters.AddWithValue("@Image", ndControl.Attributes["Image"].Value);
+                                    command.Parameters.AddWithValue("@Global", ndControl.Attributes["Global"].Value);
+                                    command.Parameters.AddWithValue("@ButtonStyle", ndControl.Attributes["ButtonStyle"].Value);
+                                    command.Parameters.AddWithValue("@WindowStyle", ndControl.Attributes["WindowStyle"].Value);
 
-                                cmd.ExecuteNonQuery();
+                                    command.ExecuteNonQuery();
+                                }
                             }
 
                             cn.Close();
