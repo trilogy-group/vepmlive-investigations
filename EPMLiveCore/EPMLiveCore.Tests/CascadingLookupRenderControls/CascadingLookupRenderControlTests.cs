532dc8054a0592e8e516f95f41ed196b281c6c7d
Bruno Serrano <bruno.serrano@aurea.com>
2018-09-14 19:25:11 +0000

    Removing unnecessary whitespace

--- EPMLiveCore/EPMLiveCore.Tests/CascadingLookupRenderControls/CascadingLookupRenderControlTests.cs
+++ EPMLiveCore/EPMLiveCore.Tests/CascadingLookupRenderControls/CascadingLookupRenderControlTests.cs
@@ -564,7 +564,6 @@ namespace EPMLiveCore.Tests.CascadingLookupRenderControls
                 },
                 FieldsGet = () => _shimFields.Instance,
                 ParentWebGet = () => _shimWeb,
-
                 IsThrottledGet = () => true
             };
         }
