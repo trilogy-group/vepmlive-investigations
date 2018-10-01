d1954dfa34e7ef3268a5a06c887c9868ec295bae
Vladimir Kulgun <vladimir.kulgun@devfactory.com>
2018-10-01 21:15:08 +0300

    add: InstallProperties UTs

---
+++ EPMLiveCore/EPMLiveCore.Tests/API/Applications/ApplicationInstallerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections.Specialized.Fakes;
+using System.Data.SqlClient.Fakes;
+using EPMLive.TestFakes.Utility;
+using EPMLiveCore.Fakes;
+using EPMLiveCore.Jobs.Applications.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.SharePoint.Fakes;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Utilities.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveCore.API
+{
+    [TestClass]
+    public partial class ApplicationInstallerTests
+    {
+        private IDisposable _shimsObject;
+        private AdoShims _shimAdo;
+        private PrivateObject _privateObject;
+        private ApplicationInstaller _applicationInstaller;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _shimsObject = ShimsContext.Create();
+            SharepointShims.ShimSharepointCalls();
+            _shimAdo = AdoShims.ShimAdoNetCalls();
+
+            ArrangeShims();
+
+            _applicationInstaller = new ApplicationInstaller(
+                string.Empty, 
+                new ShimSqlConnection().Instance, 
+                new ShimInstallAndConfigure().Instance);
+
+            _privateObject = new PrivateObject(_applicationInstaller);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _shimsObject?.Dispose();
+        }
+
+        private void ArrangeShims()
+        {
+            ShimSPWeb.AllInstances.PropertiesGet = _ => new ShimSPPropertyBag();
+
+            ShimCoreFunctions.setConfigSettingSPWebStringString = (a, b, c) => { };
+
+            ShimSPListItem.AllInstances.ParentListGet = _ => new ShimSPList();
+            ShimSPListItem.AllInstances.ItemGetString = (a, b) => new ShimSPListItem();
+
+            ShimSPList.AllInstances.ParentWebGet = _ => new ShimSPWeb();
+        }
+    }
+}
---
+++ EPMLiveCore/EPMLiveCore.Tests/API/Applications/ApplicationInstallerTests.iInstallProperties.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Specialized.Fakes;
+using System.Data;
+using System.Text;
+using EPMLiveCore.Fakes;
+using Microsoft.SharePoint.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveCore.API
+{
+    public partial class ApplicationInstallerTests
+    {
+        private const string InstallPropertiesMethod = "iInstallProperties";
+        private const string AppDefProp = "appDef";
+        private const string OWebProp = "oWeb";
+        private const string VerifyOnlyProp = "bVerifyOnly";
+        private const string ListItemProp = "oListItem";
+        private const string DTMessagesProp = "_dtMessages";
+
+        [TestMethod]
+        public void InstallProperties_When_Append()
+        {
+            InstallPropertiesCommon(false, true, true, "value\0", true, "Installing Properties", "name", "Property found and will append");
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Append_And_Verify()
+        {
+            InstallPropertiesCommon(true, true, true, "value\0", true, "Checking Properties", "name", "Property found and will append");
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Found()
+        {
+            InstallPropertiesCommon(false, true, true, string.Empty, true, "Installing Properties", "name", string.Empty);
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Append_And_Not_Overwrite()
+        {
+            InstallPropertiesCommon(true, true, false, "value\0", true, "Checking Properties", "name", "Cannot append value, value already exists");
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Contains()
+        {
+            InstallPropertiesCommon(true, true, false, "value\0", false, "Checking Properties", "name", string.Empty);
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Append()
+        {
+            InstallPropertiesCommon(false, false, true, "value\0", true, "Installing Properties", "name", string.Empty);
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Append_And_Verify()
+        {
+            InstallPropertiesCommon(true, false, true, "value\0", true, "Checking Properties", "name", "Property already exists and will overwrite");
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Append_And_Verify_And_Not_Contains()
+        {
+            InstallPropertiesCommon(true, false, true, "value\0", false, "Checking Properties", "name", string.Empty);
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Append_Not_OverWrite()
+        {
+            InstallPropertiesCommon(true, false, false, "value\0", true, "Checking Properties", "name", "Property already exists and cannot overwrite");
+        }
+
+        [TestMethod]
+        public void InstallProperties_When_Not_Append_Not_OverWrite_Not_Contains()
+        {
+            InstallPropertiesCommon(true, false, false, "value\0", false, "Checking Properties", "name", string.Empty);
+        }
+
+        private void InstallPropertiesCommon(
+            bool verifyOnly, 
+            bool append, 
+            bool overWrite, 
+            string values, 
+            bool containsKey,
+            string expectedRow03,
+            string expectdRow13,
+            string expectedRow14)
+        {
+            // Arrange
+            var appDef = new ApplicationDef();
+            var xml = new StringBuilder();
+            xml.Append("<root>")
+                .Append("<Web>")
+                .Append("<Properties>")
+                .AppendFormat("<Property Name='name' Value='value' Append='{0}' Overwrite='{1}' LockWebProperty='false' Seperator='\\0' DuplicateRegEx=''>", append, overWrite)
+                .Append("</Property>")
+                .Append("</Properties>")
+                .Append("</Web>")
+                .Append("</root>");
+            appDef.ApplicationXml.LoadXml(xml.ToString());
+            _privateObject.SetFieldOrProperty(AppDefProp, appDef);
+            _privateObject.SetFieldOrProperty(OWebProp, new ShimSPWeb().Instance);
+            _privateObject.SetFieldOrProperty(VerifyOnlyProp, verifyOnly);
+            _privateObject.SetFieldOrProperty(ListItemProp, new ShimSPListItem().Instance);
+
+            ShimCoreFunctions.getConfigSettingSPWebString = (a, b) => values;
+            ShimStringDictionary.AllInstances.ContainsKeyString = (a, b) => containsKey;
+
+            // Act
+            _privateObject.Invoke(InstallPropertiesMethod);
+            var dtMessages = _privateObject.GetFieldOrProperty(DTMessagesProp) as DataTable;
+
+            // Assert
+            Assert.IsNotNull(dtMessages);
+            Assert.AreEqual(2, dtMessages.Rows.Count);
+            Assert.AreEqual(expectedRow03, dtMessages.Rows[0][3]);
+            Assert.AreEqual(expectdRow13, dtMessages.Rows[1][3]);
+            Assert.AreEqual(expectedRow14, dtMessages.Rows[1][4]);
+        }
+    }
+}
