6b56094cdccc50e8130f144f47dc02dc45dd954f
Vladimir Kulgun <vladimir.kulgun@devfactory.com>
2018-10-08 17:37:05 +0300

    CC-78477

--- EPMLiveTimesheets/EPMLiveTimesheets/WebPageCode/gettsapprovals.cs
+++ EPMLiveTimesheets/EPMLiveTimesheets/WebPageCode/gettsapprovals.cs
@@ -1,18 +1,11 @@
 using System;
-using System.Data;
-using System.Configuration;
 using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
-using Microsoft.SharePoint;
-using System.Text;
-using System.Xml;
+using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Text;
+using System.Xml;
+using Microsoft.SharePoint;
 
 namespace TimeSheets
 {
@@ -71,10 +64,10 @@ namespace TimeSheets
             base.getParams(curWeb);
             isTimesheet = true;
 
-            string strPeriod = Request["period"];
+            var strPeriod = Request["period"];
             period = int.Parse(strPeriod);
             base.inEditMode = false;
-            string resUrl = string.Empty;
+            var resUrl = string.Empty;
 
             SPSecurity.RunWithElevatedPrivileges(delegate ()
             {
@@ -96,7 +89,7 @@ namespace TimeSheets
                 {
                     if (resUrl.ToLower() != curWeb.Url.ToLower())
                     {
-                        using (SPSite tempSite = new SPSite(resUrl))
+                        using (var tempSite = new SPSite(resUrl))
                         {
                             tempSite.CatchAccessDeniedException = false;
                             resWeb = tempSite.OpenWeb();
@@ -144,157 +137,6 @@ namespace TimeSheets
             dsTimesheetMeta = DataSetUtils.CreateDataSet(cn, sql, period, siteId);            
         }
 
-        public override void populateGroups(string query, SortedList arrGTemp, SPWeb curWeb)
-        {
-            if (arrGTemp == null)
-            {
-                throw new ArgumentNullException(nameof(arrGTemp));
-            }
-            if (curWeb == null)
-            {
-                throw new ArgumentNullException(nameof(curWeb));
-            }
-            if (resWeb == null)
-            {
-                return;
-            }
-
-            var spquery = new SPQuery
-            {
-                Query = "<Where><Eq><FieldRef Name='TimesheetManager'/><Value Type='User'><UserID/></Value></Eq></Where>"
-            };
-
-            var dtItems = new DataTable();
-            dtItems.Columns.Add("WebId");
-            dtItems.Columns.Add("ListId");
-            dtItems.Columns.Add("ItemId");
-            dtItems.Columns.Add("Username");
-            dtItems.Columns.Add("Name");
-
-            var resources = resWeb.Lists["Resources"];
-
-            foreach (SPListItem listItem in resources.GetItems(spquery))
-            {
-                if (listItem["SharePointAccount"] != null)
-                {
-                    FillItems(arrGTemp, curWeb, dtItems, listItem);
-                }
-            }
-
-            foreach (var dataRow in dtItems.Select(string.Empty, "WebId, ListId"))
-            {
-                try
-                {
-                    AddListItem(arrGTemp, dataRow);
-                }
-                catch (Exception ex)
-                {
-                    // here we catch more than few exceptions
-                    // listing all of them might kill simplicty
-                    Debug.WriteLine(ex);
-                }
-            }
-        }
-
-        private void FillItems(IDictionary arrGTemp, SPWeb curWeb, DataTable dtItems, SPListItem listItem)
-        {
-            if (arrGTemp == null)
-            {
-                throw new ArgumentNullException(nameof(arrGTemp));
-            }
-            if (dtItems == null)
-            {
-                throw new ArgumentNullException(nameof(dtItems));
-            }
-            if (listItem == null)
-            {
-                throw new ArgumentNullException(nameof(listItem));
-            }
-            var userValue = new SPFieldUserValue(curWeb, listItem["SharePointAccount"].ToString());
-            if (userValue.User == null)
-            {
-                return;
-            }
-
-            var dataRowsTimeSheetTask = dsTimesheetTasks.Tables[0].Select($"username = '{userValue.User.LoginName}'");
-
-            var dataRowsTimeSheet = dsTimesheets.Tables[0].Select($"username = '{userValue.User.LoginName} '");
-
-            if (!arrGTemp.Contains(listItem.Title))
-            {
-                arrGTemp.Add(listItem.Title, string.Empty);
-            }
-
-            var newNode = docXml.CreateNode(XmlNodeType.Element, "row", docXml.NamespaceURI);
-            var attrId = docXml.CreateAttribute("id");
-            attrId.InnerText = listItem.Title;
-            newNode.Attributes.Append(attrId);
-
-            var status = string.Empty;
-            var notes = string.Empty;
-
-            if (dataRowsTimeSheet.Length > 0)
-            {
-                AddColumn(newNode, dataRowsTimeSheet[0][TsUid].ToString(), "tsuid");
-                string locked;
-                GetValues(dataRowsTimeSheet, out status, out locked);
-                AddColumn(newNode, locked, "nosub");
-                notes = dataRowsTimeSheet[0][ApprovalNotes].ToString();
-            }
-            else
-            {
-                AddColumn(newNode, string.Empty, "tsuid");
-                AddColumn(newNode, "1", "nosub");
-            }
-
-            AddCells(listItem, newNode, status, notes);
-
-            ndMainParent.AppendChild(newNode);
-
-            hshItemNodes.Add(listItem.Title, newNode);
-
-            foreach (var dataRow in dataRowsTimeSheetTask)
-            {
-                var array = new string[] {
-                    dataRow[WebUid].ToString(),
-                    dataRow[ListUid].ToString(),
-                    dataRow[ItemId].ToString(),
-                    dataRow[UserName].ToString(),
-                    listItem.Title };
-
-                dtItems.Rows.Add(array);
-            }
-        }
-
-        private void AddCells(SPListItem listItem, XmlNode newNode, string status, string notes)
-        {
-            if (listItem == null)
-            {
-                throw new ArgumentNullException(nameof(listItem));
-            }
-            if (newNode == null)
-            {
-                throw new ArgumentNullException(nameof(newNode));
-            }
-            AddCell(newNode, string.Empty);
-            AddCell(newNode, notes);
-
-            var newCell = docXml.CreateNode(XmlNodeType.Element, "cell", docXml.NamespaceURI);
-            newCell.InnerXml = status;
-            var attrStyle = docXml.CreateAttribute("style");
-            attrStyle.Value = "background: #DFE7F7; font-weight:bold";
-            newCell.Attributes.Append(attrStyle);
-            newNode.AppendChild(newCell);
-
-            AddCell(newNode, string.Empty);
-
-            newCell = docXml.CreateNode(XmlNodeType.Element, "cell", docXml.NamespaceURI);
-            newCell.InnerText = listItem.Title;
-            attrStyle = docXml.CreateAttribute("style");
-            attrStyle.Value = "background: #DFE7F7; font-weight:bold";
-            newNode.Attributes.Append(attrStyle);
-            newNode.AppendChild(newCell);
-        }
 
         private static void GetValues(DataRow[] dataRowsTimeSheet, out string status, out string locked)
         {
@@ -414,189 +256,6 @@ namespace TimeSheets
             addTSItem(listItem, arrGTemp, dataRow[3].ToString(), dataRow[4].ToString());
         }
 
-        protected override void outputXml()
-        {
-            var arrayList = new ArrayList();
-            var workTypes = string.Empty;
-            var timeEditor = false;
-            var timeNotes = false;
-            var filterHead = string.Empty;
-            var ndCols = docXml.SelectNodes("//head/column");
-
-            var columnCount = ndCols.Count;
-            var strColumns = string.Empty;
-
-            var web = site.RootWeb;
-            {
-                usecurrent = bool.Parse(EPMLiveCore.CoreFunctions.getConfigSetting(web, "EPMLiveTSUseCurrent"));
-            }
-
-            try
-            {
-                AddNodes(
-                    ndCols,
-                    site,
-                    cn,
-                    "<![CDATA[Notes]]>",
-                    "tsnotes",
-                    "50",
-                    period,
-                    arrayList,
-                    ref filterHead,
-                    ref workTypes,
-                    ref timeEditor,
-                    ref timeNotes);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-
-            var ndHead = docXml.SelectSingleNode("//head");
-            if (!usecurrent)
-            {
-                strColumns = GetColumns(ndHead);
-            }
-
-            AddColumnWork(ndHead);
-            ndHead.RemoveChild(ndHead.SelectSingleNode("settings"));
-
-            var strWorktypes = workTypes.Split('|');
-            var rowCounter = 1;
-
-            foreach (XmlNode rowNode in docXml.SelectNodes("//row"))
-            {
-                var ndListId = rowNode.SelectSingleNode(string.Format(UserData, "listid"));
-                var ndItemId = rowNode.SelectSingleNode(string.Format(UserData, "itemid"));
-                var ndTsuid = rowNode.SelectSingleNode(string.Format(UserData, "tsuid"));
-
-                if (ndListId != null && ndItemId != null)
-                {
-                    var sql = "select ts_item_uid,submitted,approval_status,project from vwtstasks where list_uid=@listuid and item_id=@itemid and username=@username and period_id=@period_id";
-                    AddCells(
-                        rowNode,
-                        site,
-                        ndListId,
-                        ndItemId,
-                        period,
-                        dsTSHours,
-                        arrayList,
-                        bgcolor,
-                        usecurrent,
-                        strColumns,
-                        dsTimesheetTasks,
-                        dsTimesheetMeta,
-                        timeEditor,
-                        strWorktypes,
-                        timeNotes,
-                        cn,
-                        sql);
-                }
-                else
-                {
-                    AddCells(rowNode, bgcolor, arrayList);
-                }
-                rowNode.Attributes[Id].Value = rowCounter.ToString();
-                rowCounter++;
-            }
-
-            var ndFilter = docXml.SelectSingleNode("//head/beforeInit/call[@command='attachHeader']");
-            if (ndFilter != null)
-            {
-                var xml = ndFilter.FirstChild.InnerXml;
-                xml = xml.Replace("<![CDATA[", "<![CDATA[&nbsp;,&nbsp;,&nbsp;,&nbsp;,")
-                        .Replace($"gridfilter{gridname}('1", $"gridfilter{gridname}('2")
-                        .Replace("]]>", $"{filterHead},&nbsp;]]>");
-                ndFilter.FirstChild.InnerXml = xml;
-            }
-
-            UpdateHeadColumns(columnCount);
-
-            cn.Close();
-
-            data = docXml.OuterXml;
-        }
-
-        private void UpdateHeadColumns(int columnCount)
-        {
-            var fullWidth = double.Parse(Request[Width]);
-            double reservedWidth = 0;
-
-            var ndNewCols = docXml.SelectNodes("//head/column");
-            for (var i = columnCount + 4; i < ndNewCols.Count; i++)
-            {
-                try
-                {
-                    var witdth = ndNewCols[i].Attributes[Width].Value;
-                    reservedWidth += double.Parse(witdth);
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex);
-                }
-            }
-            reservedWidth += 125;
-
-            fullWidth -= reservedWidth;
-            fullWidth = fullWidth / (columnCount + 1);
-
-            if (fullWidth < 50)
-            {
-                fullWidth = 50;
-            }
-
-            for (var i = ColumnStartIndex; i <= columnCount + 3; i++)
-            {
-                var id = string.Empty;
-                try
-                {
-                    id = ndNewCols[i].Attributes[TypeAttribute].Value;
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex);
-                }
-                if (id == TreeId)
-                {
-                    ndNewCols[i].Attributes[Width].Value = (fullWidth * 2 - 10).ToString();
-                }
-                else
-                {
-                    ndNewCols[i].Attributes[Width].Value = fullWidth.ToString();
-                    ndNewCols[i].Attributes[TypeAttribute].Value = "ro";
-                }
-            }
-        }
-
-        private void AddColumnWork(XmlNode ndHead)
-        {
-            if (ndHead == null)
-            {
-                throw new ArgumentNullException(nameof(ndHead));
-            }
-
-            var newColWork = docXml.CreateNode(XmlNodeType.Element, Column, docXml.NamespaceURI);
-            newColWork.InnerXml = "<![CDATA[% Work Spent]]>";
-            var attrTypeWork = docXml.CreateAttribute(TypeAttribute);
-            attrTypeWork.Value = "percentwork";
-            var attrWidthWork = docXml.CreateAttribute(Width);
-            attrWidthWork.Value = "125";
-            var attrAlignWork = docXml.CreateAttribute(Align);
-            attrAlignWork.Value = "left";
-            var attrColorWork = docXml.CreateAttribute(Color);
-            attrColorWork.Value = "#F0F0F0";
-            var attrIdWork = docXml.CreateAttribute(Id);
-            attrIdWork.Value = "_PercentWork_";
-
-            newColWork.Attributes.Append(attrTypeWork);
-            newColWork.Attributes.Append(attrWidthWork);
-            newColWork.Attributes.Append(attrAlignWork);
-            newColWork.Attributes.Append(attrColorWork);
-            newColWork.Attributes.Append(attrIdWork);
-
-            ndHead.AppendChild(newColWork);
-        }
-
         private static string GetColumns(XmlNode ndHead)
         {
             if (ndHead == null)
@@ -624,15 +283,17 @@ namespace TimeSheets
 
         private void addTSItem(SPListItem li, SortedList arrGTemp, string username, string resource)
         {
-            string[] group = new string[1] { null };
+            var group = new string[1] { null };
 
             group[0] = resource;
 
             ProcessGroupFields(arrGroupFields, li, group, arrGTemp);
 
-            AddItemType it = new AddItemType();
-            it.indexer = li.ParentList.ParentWeb.ID + "." + li.ParentList.ID + "." + li.ID + "." + username;
-            it.o = li;
+            var it = new AddItemType
+            {
+                indexer = li.ParentList.ParentWeb.ID + "." + li.ParentList.ID + "." + li.ID + "." + username,
+                o = li
+            };
             arrItems.Add(li.ParentList.ParentWeb.ID + "." + li.ParentList.ID + "." + li.ID + "." + username, group);
             queueAllItems.Enqueue(it);
         }
@@ -644,7 +305,7 @@ namespace TimeSheets
 
             newCell = docXml.CreateNode(XmlNodeType.Element, "cell", docXml.NamespaceURI);
             newCell.InnerText = string.Empty;
-            XmlAttribute attrType = docXml.CreateAttribute("type");
+            var attrType = docXml.CreateAttribute("type");
             attrType.Value = "ro";
             newCell.Attributes.Append(attrType);
             attrStyle2 = docXml.CreateAttribute("style");
@@ -704,7 +365,7 @@ namespace TimeSheets
             var attrType = docXml.CreateAttribute("type");
             attrType.Value = "timeeditor";
             newCol.Attributes.Append(attrType);
-            foreach (string strWorkType in strworktypes)
+            foreach (var strWorkType in strworktypes)
             {
                 var drs = dsTSHours.Tables[0].Select("ts_item_uid = '" + ts_item_uid + "' and TS_ITEM_DATE=#" + dt.ToString("MM/dd/yyyy") + "# and tstype_id='" + strWorkType + "'");
                 if (drs.Length > 0)
@@ -740,7 +401,6 @@ namespace TimeSheets
             newCol.Attributes.Append(attrColor);
 
             docXml.SelectSingleNode("//head").InsertBefore(newCol, ndCols[0]);
-
 
             newCol = docXml.CreateNode(XmlNodeType.Element, "column", docXml.NamespaceURI);
             newCol.InnerXml = "<![CDATA[PM]]>";
