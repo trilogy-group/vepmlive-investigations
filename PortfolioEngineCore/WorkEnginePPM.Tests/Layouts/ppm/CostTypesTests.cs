a8b3a58ad636cc6abb8bc8c9271385878c8a67f9
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-10-11 14:34:17 +0000

    add unit tests file

---
+++ PortfolioEngineCore/WorkEnginePPM.Tests/Layouts/ppm/CostTypesTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace WorkEnginePPM.Tests.Layouts.ppm
+{
+    [TestClass]
+    public class CostTypesTests
+    {
+        private IDisposable shimsContext;
+        private CostTypes costTypes;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+            costTypes = new CostTypes();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+
+        
+
+    }
+}
