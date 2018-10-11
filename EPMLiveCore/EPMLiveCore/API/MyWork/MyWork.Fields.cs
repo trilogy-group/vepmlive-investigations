fdd798c6a05078f38dd62c251a125e510f8ec993
Muralidaran <k_murali_daran@yahoo.co.in>
2018-10-11 18:18:11 +0530

    CC-78131 Refactored Long Method GetMyWorkGridData in EPMLiveCore/EPMLiveCore/API/MyWork/MyWork.cs
    Addresses - https://jira.devfactory.com/browse/CC-78131
    GetMyWorkGridData - 2 UTs - 93%
    Verified refactored code against the UTs delivered by the HUT team

--- EPMLiveCore/EPMLiveCore/API/MyWork/MyWork.Fields.cs
+++ EPMLiveCore/EPMLiveCore/API/MyWork/MyWork.Fields.cs
@@ -2,21 +2,46 @@
 {
     public partial class MyWork
     {
+        private const string Zero = "0";
         private const string AssignedToId = "AssignedToID";
+        private const string BField = "B";
+        private const string Body = "Body";
+        private const string Choice = "Choice";
         private const string CompleteItemsQuery = "CompleteItemsQuery";
         private const string DateRange = "DateRange";
+        private const string Edit = "Edit";
+        private const string Field = "Field";
+        private const string Fields = "Fields";
         private const string From = "From";
+        private const string Grid = "Grid";
+        private const string IText = "I";
+        private const string IdText = "ID";
         private const string InternalName = "InternalName";
+        private const string Item = "Item";
+        private const string ItemIdText = "ItemID";
         private const string ItemId = "ItemId";
+        private const string Key = "Key";
+        private const string ListIdText = "ListID";
         private const string ListId = "ListId";
+        private const string MaxHeight = "MaxHeight";
         private const string MyWorkText = "My Work";
+        private const string MyPersonalizationText = "MyPersonalization";
         private const string MyWork1 = "MyWork";
+        private const string Name = "Name";
         private const string Params = "Params";
+        private const string PersonalizationsField = "Personalizations";
         private const string ProcessFlag = "ProcessFlag";
+        private const string SiteIdText = "SiteID";
         private const string SiteId = "SiteId";
+        private const string SiteUrlText = "SiteURL";
         private const string To = "To";
+        private const string Type = "Type";
+        private const string Value = "Value";
+        private const string WebIdText = "WebID";
         private const string WebId = "WebId";
         private const string WorkType = "WorkType";
+        private const string Workspace = "Workspace";
+        private const string Id = "id";
         private const string Comma = ",";
         private const string OpenCurlyBrace = "{";
         private const string ClosedCurlyBrace = "}";
@@ -26,6 +51,10 @@
         private const string UnableToRetrieveMyWorkDataList = "Unable to retrieve My Work data from My Work lists.";
         private const string AssignedToField = "AssignedTo";
         private const string CompletedField = "Complete";
+        private const string DueDateField = "DueDate";
+        private const string DueDayField = "DueDay";
+        private const string FlagField = "Flag";
+        private const string WorkTypeField = "Work0000Type";
         private const string WorkingOnField = "WorkingOn";
     }
 }
