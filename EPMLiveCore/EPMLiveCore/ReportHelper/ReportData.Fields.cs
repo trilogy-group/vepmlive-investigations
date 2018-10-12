e2fbb439b8aaa0a56d039bbba6c7f2513d382a62
Muralidaran <k_murali_daran@yahoo.co.in>
2018-10-12 22:23:41 +0530

    CC-78133 - LM EPMLiveCore/EPMLiveCore/ReportHelper/ReportData.cs::ListItemsDataTable
    Addresses https://jira.devfactory.com/browse/CC-78133
    Verified refactored code against UTs delivered by the HUT team
    Code Coverage : 88%

--- EPMLiveCore/EPMLiveCore/ReportHelper/ReportData.Fields.cs
+++ EPMLiveCore/EPMLiveCore/ReportHelper/ReportData.Fields.cs
@@ -15,5 +15,18 @@
         private const int Size256 = 256;
         private const int Size8001 = 8001;
         private const int DefaultSize = -1;
+        private const string ColumnName = "ColumnName";
+        private const string InternalName = "internalname";
+        private const string SharePointType = "SharepointType";
+        private const string Lookup = "lookup";
+        private const string ColumnNameLower = "columnname";
+        private const string MultiChoice = "multichoice";
+        private const string UniqueIdentifier = "uniqueidentifier";
+        private const string IntText = "int";
+        private const string DateTimeLower = "datetime";
+        private const string FloatLower = "float";
+        private const string ColumnType = "ColumnType";
+        private const string User = "user";
+        private const string SemiColonHash = ";#";
     }
 }
