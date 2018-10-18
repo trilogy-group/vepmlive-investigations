9701ebf5d04a0967ae082f65917263083cb28444
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-10-18 16:55:38 +0000

    add unit tests file

---
+++ PortfolioEngineCore/PortfolioEngineCore.Tests/Resources/ResourceRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioEngineCore.Tests.Resources
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ResourceRepositoryTests
+    {
+        private IDisposable shimsContext;
+
+        [TestMethod]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+        }
+
+        [TestMethod]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+
+
+    }
+}
