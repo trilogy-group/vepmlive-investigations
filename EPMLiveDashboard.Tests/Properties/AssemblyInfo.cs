34096f3f209d25c5d1bdfa44ae694a9630ceaf66
Muralidaran <k_murali_daran@yahoo.co.in>
2018-10-18 23:08:45 +0530

    CC-76097 - ML EPMLiveDashboards/EPMLiveDashboards/Dashboard/Dashboard.cs
    Addresses https://jira.devfactory.com/browse/CC-76097 and https://jira.devfactory.com/browse/CC-76098
    
    Added 1 UT verfiying memory leak

---
+++ EPMLiveDashboard.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EPMLiveDashboard.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EPMLiveDashboard.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("668b2326-1463-4375-92b4-c5b18f94380a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
