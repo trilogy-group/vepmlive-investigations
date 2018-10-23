74006840b4f1d069cb7ee2ed4a8f582dfb1703e4
Essam Elsayed <essam.elsayed@aurea.com>
2018-10-23 11:31:42 +0000

    Fix assemblies to cover

--- BuildJenkins/MsBuild/CiBuild/assembliesToCover.ps1
+++ BuildJenkins/MsBuild/CiBuild/assembliesToCover.ps1
@@ -1,37 +1,22 @@
 function GetAssembliesToCover()
 {
     return @(
-        "EPK_QueueMgr/EPK_QueueMgr/bin/debug/WE_QueueMgr.dll",
-        "EPMLiveCore/EPMLive.Data/bin/debug/EPMLive.Data.dll",
+        "EPK_QueueMgr/EPK_QueueMgr/bin/debug/WE_QueueMgr.exe",
         "EPMLiveCore/EPMLiveCore/bin/debug/EPM Live Core.dll",
         "EPMLiveDashboards/EPMLiveDashboards/bin/debug/Dashboard.dll",
-        "EPMLiveInstaller/EPMLive.Install.Launcher/bin/debug/Launcher.dll",
-        "EPMLiveInstaller/NewsGator.Install.Common/bin/debug/NewsGator.Install.Common.dll",
-        "EPMLiveInstaller/NewsGator.Install.Resources/bin/debug/NewsGator.Install.Resources.dll",
-        "EPMLiveIntegrationService/EPMLiveIntegrationService/bin/debug/EPMLiveIntegrationService.dll",
+        "EPMLiveIntegrationService/EPMLiveIntegrationService/bin/EPMLiveIntegrationService.dll",
         "EPMLiveIntegration/EPMLiveIntegration/bin/debug/EPMLiveIntegration.dll",
-        "EPMLiveNativeSSRSComponents/EPMLive.SSRSConfigInjector/bin/debug/EPMLive.SSRSConfigInjector.dll",
-        "EPMLiveNativeSSRSComponents/EPMLive.SSRSCustomAuthentication/bin/debug/EPMLive.SSRSCustomAuthentication.dll",
         "EPMLivePS/EPMLivePS/bin/debug/EPMLiveEnterprise.dll",
         "EPMLiveReporting/EPMLiveReporting/bin/debug/EPMLiveReportsAdmin.dll",
-        "EPMLiveSignals/EPMLiveSignals/bin/debug/EPMLiveSignals.dll",
         "EPMLiveSynch/EPMLiveSynch/bin/debug/EPMLiveSynch.dll",
         "EPMLiveTimerJobs/bin/debug/EPMLiveTimerJobs.dll",
-        "EPMLiveTimerService/EPMLiveTimerService/bin/debug/EPMLiveTimerService.dll",
+        "EPMLiveTimerService/EPMLiveTimerService/bin/debug/EPMLiveTimerService.exe",
         "EPMLiveTimesheets/EPMLiveTimesheets/bin/debug/TimeSheets.dll",
         "EPMLiveWebParts/EPMLiveWebParts/bin/debug/EPMLiveWebParts.dll",
         "EPMLiveWorkPlanner/EPMLiveWorkPlanner/bin/debug/EPMLiveWorkPlanner.dll",
-        "PortfolioEngineCore/PortfolioEngineCore/bin/x86/Debug/PortfolioEngineCore.dll",
+        "PortfolioEngineCore/PortfolioEngineCore/bin/Debug/PortfolioEngineCore.dll",
         "PortfolioEngineCore/WorkEnginePPM/bin/debug/WorkEnginePPM.dll",
         "ProjectPublisher2016/ProjectPublisher2016/bin/debug/ProjectPublisher2016.dll",
-        "PWChanger/bin/debug/PWChanger.dll",
-        "Saas/EPMLive.Saas/AdminSite/bin/debug/AdminSite.dll",
-        "Saas/EPMLive.Saas/BillingSite/bin/debug/BillingSite.dll",
-        "Saas/EPMLive.Saas/OnlineLicensing.Api/bin/debug/EPMLive.OnlineLicensing.Api.dll",
-        "Saas/EPMLive.Saas/Utils/LoadZuoraProducts/bin/debug/LoadZuoraProducts/LoadZuoraProducts.dll",
-        "Saas/EPMLiveAccountManagement/EPMLiveAccountManagement/bin/debug/EPMLiveAccountManagement.dll",
-        "UplandIntegrations/UplandIntegrations/bin/debug/UplandIntegrations.dll",
-        "Utils/SyncReportGenerator/bin/debug/SyncReportGenerator.dll",
-        "Utils/SyncSubscriptions/SyncSubscriptions/bin/debug/SyncSubscriptions.dll"
+        "UplandIntegrations/UplandIntegrations/bin/debug/UplandIntegrations.dll"
     )
 }
