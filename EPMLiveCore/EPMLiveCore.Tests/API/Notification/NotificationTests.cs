7eb08a0f08f9c3b09f3d8d5729f1983ef5989109
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-10-24 13:56:02 +0000

    add unit tests class

---
+++ EPMLiveCore/EPMLiveCore.Tests/API/Notification/NotificationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveCore.Tests.API.Notification
+{
+    [TestClass]
+    public class NotificationTests
+    {
+        private IDisposable shimsContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+    }
+}
