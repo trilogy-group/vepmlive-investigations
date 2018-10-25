c9ff8152651d4492dfd007b8e76a1f816d86fbac
Essam Elsayed <essam.elsayed@versata.com>
2018-10-25 14:53:09 +0200

    Revert "CC-78355 - Long class EPMLiveCore/EPMLiveCore/ReportHelper/ReportData.cs"

--- EPMLiveCore/EPMLiveCore/ReportHelper/ReportData.Fields.cs
+++ EPMLiveCore/EPMLiveCore/ReportHelper/ReportData.Fields.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Data.SqlClient;
-
-namespace EPMLiveCore.ReportHelper
+﻿namespace EPMLiveCore.ReportHelper
 {
     public partial class ReportData
     {
@@ -19,9 +16,9 @@ namespace EPMLiveCore.ReportHelper
         private const int Size8001 = 8001;
         private const int DefaultSize = -1;
         private const string ColumnName = "ColumnName";
-        private const string InternalNameLower = "internalname";
+        private const string InternalName = "internalname";
         private const string SharePointType = "SharepointType";
-        private const string LookupLower = "lookup";
+        private const string Lookup = "lookup";
         private const string ColumnNameLower = "columnname";
         private const string MultiChoice = "multichoice";
         private const string UniqueIdentifier = "uniqueidentifier";
@@ -31,73 +28,5 @@ namespace EPMLiveCore.ReportHelper
         private const string ColumnType = "ColumnType";
         private const string User = "user";
         private const string SemiColonHash = ";#";
-        private const string SingleQuote = "'";
-        private const string InsertAction = "insert";
-        private const string UpdateAction = "update";
-        private const string EpmLiveId = "EPM Live";
-        private const string EpmLiveReportingGetColumnValue = "EPMLive Reporting GetColumnValue";
-        private const string AssignedTo = "AssignedTo";
-        private const string Work = "Work";
-        private const string WorkTypeText = "WorkType";
-        private const string DataSourceText = "DataSource";
-        private const string AssignedToText = "assignedtotext";
-        private const string InternalName = "InternalName";
-        private const string Lookup = "Lookup";
-        private const string SAccount = "SAccount";
-        private const string DatabaseName = "DatabaseName";
-        private const string ReportingV2 = "reportingV2";
-
-        protected readonly string _epmLiveCs;
-        protected readonly Guid _siteId;
-        protected readonly Guid _webId;
-        protected string webTitle = string.Empty;
-        protected EPMData _DAO;
-        protected SqlCommand _cmdWithParams;
-        protected bool _isReportingV2Enabled;
-        protected bool _isRootWeb = true;
-        protected SqlParameterCollection _params;
-        protected string _siteName;
-        protected string _siteUrl;
-        private string _sqlError;
-        private bool _disposed;
-
-        public string SiteName
-        {
-            get { return _siteName; }
-        }
-
-        public bool EPMLiveConOpen
-        {
-            get { return _DAO.EPMLiveConOpen; }
-        }
-
-        public string UserName
-        {
-            get { return _DAO.UserName; }
-            set { _DAO.UserName = value; }
-        }
-
-        public string Password
-        {
-            get { return _DAO.Password; }
-            set { _DAO.Password = value; }
-        }
-
-        public bool UseSqlAccount
-        {
-            get { return _DAO.UseSqlAccount; }
-            set { _DAO.UseSqlAccount = value; }
-        }
-
-        public string SiteUrl
-        {
-            get { return _siteUrl; }
-        }
-
-        public string Command
-        {
-            get { return _DAO.Command; }
-            set { _DAO.Command = value; }
-        }
     }
 }
