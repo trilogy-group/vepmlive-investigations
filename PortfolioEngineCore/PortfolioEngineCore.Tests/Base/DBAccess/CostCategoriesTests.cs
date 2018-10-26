1cf0ba21ce9e4d2f25654c3925e4ed3680792715
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-10-26 10:26:39 +0000

    add unit tests file

---
+++ PortfolioEngineCore/PortfolioEngineCore.Tests/Base/DBAccess/CostCategoriesTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioEngineCore.Tests.Base
+{
+    [TestClass]
+    public class CostCategoriesTests
+    {
+        private IDisposable shimsContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+    }
+}
