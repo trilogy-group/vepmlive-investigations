fd69e83c953e9c6659a359918c8efe3ffd9b715b
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-10-29 17:47:53 +0000

    add unit tests file

---
+++ PortfolioEngineCore/WorkEnginePPM.Tests/WebServices/PortfolioEngineAPITests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkEnginePPM.Tests.WebServices
+{
+    [TestClass]
+    public class PortfolioEngineAPITests
+    {
+        private IDisposable shimsContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+
+
+
+    }
+}
