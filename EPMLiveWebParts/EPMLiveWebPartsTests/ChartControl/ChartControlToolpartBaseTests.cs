34f5a2f7b53dabb6317e6b303e78e36f484945b7
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-10-30 16:35:17 +0000

    add unit tests class

---
+++ EPMLiveWebParts/EPMLiveWebPartsTests/ChartControl/ChartControlToolpartBaseTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveWebParts.Tests.ChartControl
+{
+    [TestClass]
+    public class ChartControlToolpartBaseTests
+    {
+        private IDisposable shimsContext;
+        private ChartControlToolpartBase chartControlToolpartBase;
+        private PrivateObject privateObject;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+
+
+
+    }
+}
