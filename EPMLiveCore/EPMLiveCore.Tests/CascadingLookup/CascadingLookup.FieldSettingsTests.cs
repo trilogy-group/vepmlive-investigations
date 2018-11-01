a6497a9825574daaed80f41b08b37644e102c214
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-11-01 11:45:51 +0000

    add unit tests class

---
+++ EPMLiveCore/EPMLiveCore.Tests/CascadingLookup/CascadingLookup.FieldSettingsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveCore.Tests.CascadingLookup
+{
+    [TestClass]
+    public class CascadingLookup
+    {
+        private CascadingLookupFieldSettings cascadingLookupFieldSettings;
+        private IDisposable shimsContext;
+        private PrivateObject privateObject;
+
+        [TestMethod]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+            cascadingLookupFieldSettings = new CascadingLookupFieldSettings();
+            privateObject = new PrivateObject(cascadingLookupFieldSettings);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+
+
+
+    }
+}
