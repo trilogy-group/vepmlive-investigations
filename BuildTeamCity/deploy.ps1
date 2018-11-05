35bacdef43c458aca127621eb738568f77a70114
Essam Elsayed <essam.elsayed@aurea.com>
2018-11-05 20:45:45 +0000

    fix multi-join

--- BuildTeamCity/deploy.ps1
+++ BuildTeamCity/deploy.ps1
@@ -10,8 +10,8 @@ Remove-Item (Join-Path "C:\SilentInstaller" $buildNumber) -Force -Recurse -Error
 Write-Host 'Removed old folder'
 [io.compression.zipfile]::ExtractToDirectory("C:\SilentInstaller\SilentInstaller.zip", "C:\SilentInstaller\$buildNumber")
 Write-Host 'Extracted new folder'
-Copy-Item "C:\SilentInstaller\routines.ps1" (Join-Path "C:\SilentInstaller" $buildNumber "routines.ps1")
-Copy-Item "C:\SilentInstaller\epmliveSilentInstaller.ps1" (Join-Path "C:\SilentInstaller" $buildNumber "epmliveSilentInstaller.ps1")
+Copy-Item "C:\SilentInstaller\routines.ps1" (Join-Path "C:\SilentInstaller" $buildNumber | Join-Path -ChildPath "routines.ps1")
+Copy-Item "C:\SilentInstaller\epmliveSilentInstaller.ps1" (Join-Path "C:\SilentInstaller" $buildNumber | Join-Path -ChildPath "epmliveSilentInstaller.ps1")
 
 CD "C:\SilentInstaller\$buildNumber"
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
