75c3dffbe810826a8c4a9fed7073a283122309db
Essam Elsayed <essam.elsayed@aurea.com>
2018-11-06 11:16:39 +0000

    credssp

--- BuildTeamCity/deployRemotely.ps1
+++ BuildTeamCity/deployRemotely.ps1
@@ -18,7 +18,7 @@ $passwd = convertto-securestring -AsPlainText -Force -String $password
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $passwd
 
 Write-Host "Connecting to $serverIP, $webAppName, $siteCollectionToUpgrade, $buildNumber"
-$session = New-PSSession -ComputerName $serverIP -Credential $cred
+$session = New-PSSession -ComputerName $serverIP -Credential $cred -Authentication Credssp
 Invoke-Command -Session $session -ScriptBlock { Register-PSSessionConfiguration -Name 'EPMRemoteDeploy' -RunAsCredential $username -Force }
 
 Write-Host "Configuring WSMAN remotely: $serverIP"
