07406ec7c49fdb00f900e1230288125dce11adc8
Fabiano Bizarro <fabiano.augustosilvabizarro@aurea.com>
2018-11-08 16:54:23 +0000

    add unit test class

---
+++ EPMLiveWebParts/EPMLiveWebPartsTests/WebPageCode/GetGanttItemsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPMLiveWebParts.Tests.WebPageCode
+{
+    [TestClass]
+    public class GetGanttItemsTests
+    {
+        private IDisposable shimsContext;
+        private getganttitems getganttitems;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimsContext = ShimsContext.Create();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            shimsContext?.Dispose();
+        }
+
+
+
+    }
+}
