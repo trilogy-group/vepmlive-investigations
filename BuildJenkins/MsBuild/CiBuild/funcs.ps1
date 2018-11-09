e4548d817c9737b09d8d3730815ea6fd9cd66dab
Essam Elsayed <essam.elsayed@aurea.com>
2018-11-09 20:28:16 +0000

    replacing with only changed files
    fix jenkins file
    
    use trx not .coverage
    
    using saxon
    
    fix syntax and eliminate AUT
    
    fix
    
    fixing junit generation
    
    trial
    
    trial #2
    
    Trial #3
    
    revert to mstest publisher but separate files
    
    trial #4
    
    Revert back all removed features
    
    change coveragexml name
    
    revert unittests
    
    Back to saxon
    
    mstest publisherback
    
    mstest publisherback
    
    try new trx2html tool
    
    remove comments
    
    convert to xml wit out
    
    convert to xml without out
    
    new logger trial
    
    use start-process
    
    to string
    
    to string
    
    to string

--- BuildJenkins/MsBuild/CiBuild/funcs.ps1
+++ BuildJenkins/MsBuild/CiBuild/funcs.ps1
@@ -1,12 +1,11 @@
 function CollectTestAssemblies()
 {
   $dllsInBin = Get-ChildItem -Filter "*.dll" -Recurse | where{$_.FullName -match "\\bin\\"}
-  $filters = @("*test*.dll", "*tests*.dll")
+  $filters = @("*test*.dll")
   $assmsToRun2 = New-Object System.Collections.Generic.HashSet[object]
-
   foreach ($fi in $filters) {
     foreach ($dll in ($dllsInBin | where{$_.Name -like $fi})) {
-      $assmsToRun2.Add($dll.FullName) > $null
+		$assmsToRun2.Add($dll.FullName) > $null
     }
   }
   return $assmsToRun2
--- BuildJenkins/MsBuild/CiBuild/unittests.ps1
+++ BuildJenkins/MsBuild/CiBuild/unittests.ps1
@@ -15,46 +15,78 @@ $vstestConsolePath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAg
 $codeCoveragePath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAgent\Team Tools\Dynamic Code Coverage Tools\amd64\CodeCoverage.exe"
 $reportGeneratorPath = "C:\ReportGenerator\ReportGenerator.exe"
 
-$asmsToSendToVsTest = New-Object System.Collections.Generic.HashSet[object]
+Write-Host 'Clearing test results directory'
+Remove-Item -Path $testResultDir -Recurse -ErrorAction:SilentlyContinue
+New-Item -ItemType Directory -Force -Path $testResultDir > $null
 
-Write-Host 'Collecting test assemblies...'
-$assmsToRun = (CollectTestAssemblies)
-Write-Host $assmsToRun.Count "assemblies found:"
-foreach ($testAsm in $assmsToRun) {
-    $asmsToSendToVsTest.Add($testAsm) > $null
-    Write-Host $testAsm
-}
 
 Write-Host 'Listing assemblies to cover'
 $assembliesToCover = (GetAssembliesToCover)
 Write-Host $assembliesToCover.Count "assemblies"
+
+Write-Host 'Collecting test assemblies...'
+$assmsToRun = (CollectTestAssemblies)
+
+$asmsToSendToVsTest = ""
 foreach ($asmToCover in $assembliesToCover) {
     $asmToCover = Join-Path $sourcesRoot $asmToCover
     if(![System.IO.File]::Exists($asmToCover)){
         throw "Cannot find assembly $asmToCover"
     }
-    $asmsToSendToVsTest.Add($asmToCover) > $null
-    Write-Host $asmToCover
+    $asmsToSendToVsTest = $asmsToSendToVsTest + '"' + $asmToCover + '" '
 }
 
-Write-Host 'Clearing test results directory'
-Remove-Item -Path $testResultDir -Recurse -ErrorAction:SilentlyContinue
-New-Item -ItemType Directory -Force -Path $testResultDir > $null
+Write-Host $assmsToRun.Count "assemblies found:"
+for ($i=0; $i -lt $assmsToRun.Count; $i++) {
+	$asmsToSendToVsTest = $asmsToSendToVsTest + '"' + $assmsToRun[$i] + '" '
+}
 
+$testResultFile = Join-Path $testResultDir "JUnitLikeResultsOutputFile1.xml"
 Write-Host 'Executing unit tests and collecting coverage'
-.$vstestConsolePath /Logger:trx /Platform:x64 /InIsolation /EnableCodeCoverage /Parallel $asmsToSendToVsTest
+$setupProcess = Start-Process -PassThru -Wait -NoNewWindow -FilePath $vstestConsolePath -ArgumentList "/logger:JUnitLogger;TestResultsFile=$testResultFile /Platform:x64 /InIsolation /EnableCodeCoverage /Parallel $asmsToSendToVsTest"
+
 
 Write-Host 'Converting .coverage to .coveragexml'
-$coverageFile = (Get-ChildItem -File (Join-Path $sourcesRoot *.coverage) -Recurse | Select-Object -first 1).FullName
-$coverageXmlFile = Join-Path $testResultDir "coverage.xml"
+$coverageFiles = (Get-ChildItem -File (Join-Path $sourcesRoot *.coverage) -Recurse)
+$coverageFile = ''
+foreach ($coverageItem in $coverageFiles)
+{
+  $coverageFile = $coverageFile + '"' + $coverageItem.FullName + '" '
+}
+$coverageFile.trimend(',')
+
+
+<#
+$trxFile = (Get-ChildItem -File (Join-Path $sourcesRoot *.trx) -Recurse | Select-Object -first 1).FullName
+Write-Host "Trx File: $trxFile"
+$convertedFile = Join-Path $testResultDir "JUnitLikeResultsOutputFile1.xml"
+Write-Host "Converting trx to xml: $convertedFile"
+$setupProcess = Start-Process -PassThru -Wait -NoNewWindow -FilePath 'C:/Program Files/Saxonica/SaxonPE9.8N/bin/transform.exe' -ArgumentList "-s:$trxFile -xsl:C:\Msxml\mstest-to-junit_withOutput.xsl -o:$convertedFile"
+If($setupProcess.Exitcode -ne 0) {      Throw "Errorlevel $($setupProcess.ExitCode)" } 
+
+
+$trxFile = (Get-ChildItem -File (Join-Path $sourcesRoot *.trx) -Recurse | Select-Object -first 1).FullName
+Write-Host "Trx File: $trxFile"
+Write-Host "Converting trx to html"
+$setupProcess = Start-Process -PassThru -Wait -NoNewWindow -FilePath 'C:/trx2html/trx2html.exe' -ArgumentList $trxFile
+If($setupProcess.Exitcode -ne 0) {      Throw "Errorlevel $($setupProcess.ExitCode)" } 
+$generatedFile = $trxFile + '.htm'
+Rename-Item  $generatedFile -newname 'TestResultsReport.htm'
+#>
+
+$coverageXmlFile = Join-Path $testResultDir "vstest.coveragexml" 
 Write-Host "Coverage file (input): $coverageFile"
 Write-Host "CoverageXml file (output): $coverageXmlFile"
-.$codeCoveragePath analyze /output:$coverageXmlFile $coverageFile
+$setupProcess = Start-Process -PassThru -Wait -NoNewWindow -FilePath $codeCoveragePath  -ArgumentList "analyze /output:$coverageXmlFile $coverageFile"
+If($setupProcess.Exitcode -ne 0) {      Throw "Errorlevel $($setupProcess.ExitCode)" } 
+
+
 
 Write-Host 'Generating coverage report'
 .$reportGeneratorPath `
     -reports:"$coverageXmlFile"  `
     -targetdir:(Join-Path $testResultDir CoverageReport) `
     -reportTypes:"HtmlSummary;XMLSummary" `
-    -assemblyfilters:"-*tests*;-moq;-*autofixture*;-*newtonsoft*;-*.test*;-*tests;-*.Test.Commons;-*.TestCommons;-*.aut.*;-*.resources" `
-    -classfilters:"-Moq.*"
+    -assemblyfilters:"-*tests*;-*moq*;-*autofixture*;-*newtonsoft*;-*.test*;-*tests;-*.Test.Commons;-*.TestCommons;-*.aut.*;-*.resources;-*NUnit*" `
+    -classfilters:"-Moq.*;*nunit*"
+
