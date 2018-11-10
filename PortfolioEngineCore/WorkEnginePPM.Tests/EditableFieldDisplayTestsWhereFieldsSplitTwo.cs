299006395393e917d81477f4333821dd070148e6
alexandreyamamoto <alexandreyamamoto87@gmail.com>
2018-11-10 22:36:31 +0000

    hut/CC-78733 HUT for PortfolioEngineCore/WorkEnginePPM/EditableFieldDisplay.cs

---
+++ PortfolioEngineCore/WorkEnginePPM.Tests/EditableFieldDisplayTestsWhereFieldsSplitTwo.cs
@@ -0,0 +1,337 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace WorkEnginePPM.Tests
+{
+    public partial class EditableFieldDisplayTestsWhereFields
+    {
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToDateTimeSame_ReturnsFalse()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyDateTime.ToString();
+            _conditionField.TypeGet = () => SPFieldType.Text;
+            _conditionField.SchemaXmlGet = () => FormatDateTime;
+            _listItem.ItemGetGuid = guid => DummyDateTime;
+
+            var valueParam = "[Today]";
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToDateTimeDifferent_ReturnsTrue()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => BiggerDummyDateTime.ToString();
+            _conditionField.TypeGet = () => SPFieldType.Text;
+            _conditionField.SchemaXmlGet = () => FormatDateTime;
+            _listItem.ItemGetGuid = guid => DummyDateTime;
+
+            var valueParam = "[Today]";
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToDateTimeSameFormatShort_ReturnsFalse()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyDateTime.ToShortDateString();
+            _conditionField.TypeGet = () => SPFieldType.Text;
+            _conditionField.SchemaXmlGet = () => FormatShortDate;
+            _listItem.ItemGetGuid = guid => DummyDateTime;
+
+            var valueParam = "[Today]";
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToDateTimeDifferentFormatShort_ReturnsTrue()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => BiggerDummyDateTime.ToShortDateString();
+            _conditionField.TypeGet = () => SPFieldType.Text;
+            _conditionField.SchemaXmlGet = () => FormatShortDate;
+            _listItem.ItemGetGuid = guid => DummyDateTime;
+
+            var valueParam = "[Today]";
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToNumberSame_ReturnsFalse()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyInt.ToString();
+            _conditionField.TypeGet = () => SPFieldType.Number;
+            _listItem.ItemGetGuid = guid => DummyInt;
+
+            var valueParam = DummyInt.ToString();
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToNumberDifferent_ReturnsTrue()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyInt.ToString();
+            _conditionField.TypeGet = () => SPFieldType.Number;
+            _listItem.ItemGetGuid = guid => BiggerDummyInt;
+
+            var valueParam = DummyInt.ToString();
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToCurrencySame_ReturnsFalse()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyDouble.ToString();
+            _conditionField.TypeGet = () => SPFieldType.Currency;
+            _listItem.ItemGetGuid = guid => DummyDouble;
+
+            var valueParam = DummyDouble.ToString();
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToCurrencyDifferent_ReturnsTrue()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyDouble.ToString();
+            _conditionField.TypeGet = () => SPFieldType.Currency;
+            _listItem.ItemGetGuid = guid => BiggerDummyDouble;
+
+            var valueParam = DummyDouble.ToString();
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToBooleanSame_ReturnsFalse()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyTrue;
+            _conditionField.TypeGet = () => SPFieldType.Boolean;
+            _listItem.ItemGetGuid = guid => true;
+
+            var valueParam = DummyTrue;
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToBooleanDifferent_ReturnsTrue()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyTrue;
+            _conditionField.TypeGet = () => SPFieldType.Boolean;
+            _listItem.ItemGetGuid = guid => false;
+
+            var valueParam = DummyTrue;
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToStringSame_ReturnsFalse()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyString;
+            _conditionField.TypeGet = () => SPFieldType.Text;
+            _listItem.ItemGetGuid = guid => DummyString;
+
+            var valueParam = DummyString;
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void WhereField_ConditionIsNotEqualToStringDifferent_ReturnsTrue()
+        {
+            // Arrange
+            _conditionField.IdGet = () => ConditionFieldId;
+            _conditionField.GetFieldValueAsTextObject = item => DummyStringDifferent;
+            _conditionField.TypeGet = () => SPFieldType.Text;
+            _listItem.ItemGetGuid = guid => DummyStringDifferent;
+
+            var valueParam = DummyString;
+
+            // Act
+            var actualResult = (bool)_privateType.InvokeStatic(
+                WhereFieldMethod,
+                new object[]
+                {
+                    _conditionField.Instance,
+                    string.Empty,
+                    ConditionIsNotEqualTo,
+                    valueParam,
+                    _listItem.Instance
+                });
+
+            // Assert
+            actualResult.ShouldBeTrue();
+        }
+    }
+}
