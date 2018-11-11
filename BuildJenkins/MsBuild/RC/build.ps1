a320ee4b786d2b5713e80f777ab3a1cde97f8df6
Essam Elsayed <essam.elsayed@aurea.com>
2018-11-11 11:17:35 +0000

    deploy first trial

--- BuildJenkins/MsBuild/RC/build.ps1
+++ BuildJenkins/MsBuild/RC/build.ps1
@@ -26,13 +26,19 @@ Set-Location -Path $rootDir
 Write-Host "Write build number"
 "[assembly:System.Reflection.AssemblyFileVersion(""$version"")]" > CommonAssemblyInfo.cs
 
+Write-Host "Marking fake dlls as recent"
+Get-ChildItem -Include *.dll -Recurse | Where-Object {$_.FullName -like '*\FakesAssemblies\*.dll'} | ForEach-Object { $_.LastWriteTime = [System.DateTime]::Now }
+
 Write-Host "Execute builds"
-Write-Host "Executing tl_buildInstaller.ps1"
-& .\BuildTeamCity\tl_buildInstaller.ps1 -SkipInstallShield
 Write-Host "Executing tl_buildCode.ps1"
-& .\BuildTeamCity\tl_buildCode.ps1 -TestsOnly -CleanBuild $false
+& .\BuildTeamCity\tl_buildCode.ps1 -ConfigurationToBuild Release
+
+Write-Host "Executing tl_buildInstaller.ps1"
+& .\BuildTeamCity\tl_buildInstaller.ps1 
+
 $buildRoot = Join-Path $rootDir "BuildTeamCity"
 Set-Location -Path $buildRoot
+
 # Execute ps1 for EPM Live (generates config.json and other required wsp or dll files)
 Write-Host "Executing tl_buildSilentInstaller.ps1"
 & .\tl_buildSilentInstaller.ps1
@@ -40,20 +46,5 @@ Write-Host "Executing tl_buildSilentInstaller.ps1"
 Write-Host "Copy Silent installer to output dir"
 $installerDir = Join-Path $buildRoot "SilentInstaller"
 Copy-Item -Path (Join-Path $installerDir "*") -Destination $outputDir -Recurse
-
-Write-Host "Make GUI installer"
-$epmLiveInstallerRoot = Join-Path $rootDir "EPMLiveInstaller"
-SetBuildVersion (Join-Path $epmLiveInstallerRoot "NewsGator.Install.Common/BuildVersion.cs") $version
-SetAssemblyFileVersion (Join-Path $epmLiveInstallerRoot "NewsGator.Install.Common/Version.cs") $version
-SetAssemblyFileVersion (Join-Path $epmLiveInstallerRoot "NewsGator.Install.Resources/Version.cs") $version
-
-$epmLiveInstallLauncherRoot = Join-Path $epmLiveInstallerRoot "EPMLive.Install.Launcher"
-SetAssemblyFileVersion (Join-Path $epmLiveInstallLauncherRoot "Properties/AssemblyInfo.cs") $version
-$epmLiveInstallerSln = Join-Path $epmLiveInstallerRoot "EPMLive.Installer.sln"
-RunMsBuild $epmLiveInstallerSln
-
-Write-Host "Copy GUI installer to output dir"
-$epmLiveInstallerBin = Join-Path $epmLiveInstallLauncherRoot "bin/Release"
-Copy-Item -Path (Join-Path $epmLiveInstallerBin "*") -Destination $outputDir -Recurse -Exclude "*.pdb"
 
 Write-Host "Done. Installer is located at $outputDir"
