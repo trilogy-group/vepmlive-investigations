f5bf484fd0b8ab52eb388f66f8252e2ef52b1060
Essam Elsayed <essam.elsayed@aurea.com>
2018-11-11 09:19:13 +0000

    Fixing params

--- BuildJenkins/Protractor/update-hosts-file.sh
+++ BuildJenkins/Protractor/update-hosts-file.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 ETC_HOSTS=/etc/hosts
-IP="10.197.1.62" #"%targetAppHost%"
-HOSTNAME="sp2013.social.com"
-HOSTS_LINE="$IP\t$HOSTNAME"
+HOSTS_LINE="$1\t$2"
 
 # Removing existing entry
-sudo sed "/$HOSTNAME/d" $ETC_HOSTS
+sudo sed "/$2/d" $ETC_HOSTS
 
 # Adding updated entry
 sudo sh -c -e "echo '$HOSTS_LINE' >> $ETC_HOSTS"
