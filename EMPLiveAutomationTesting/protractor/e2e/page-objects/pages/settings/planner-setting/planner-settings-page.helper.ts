08cd051a7d0a4437d4b6e1660f2a91cf799629ff
sumaiyakidwai <sumaiya.kidwai@gmail.com>
2018-11-14 13:59:45 +0500

    Fix for 1032747

--- EMPLiveAutomationTesting/protractor/e2e/page-objects/pages/settings/planner-setting/planner-settings-page.helper.ts
+++ EMPLiveAutomationTesting/protractor/e2e/page-objects/pages/settings/planner-setting/planner-settings-page.helper.ts
@@ -77,7 +77,7 @@ export class PlannerSettingsPageHelper {
         await ElementHelper.scrollToBottomOfPage();
 
         StepLogger.step('Click on Save Settings button');
-        await PageHelper.click(PlannerSettingPage.saveButton);
+        await PageHelper.clickAndWaitForElementToHide(PlannerSettingPage.saveButton);
 
         StepLogger.verification('"Planner Settings" page is closed');
         await expect(await browser.getTitle()).not.toBe(PlannerSettingsPageConstants.plannerPage,
@@ -114,7 +114,7 @@ export class PlannerSettingsPageHelper {
 
         StepLogger.step('Click on the ITEMS tab above the grid\n' +
             'From the ITEMS ribbon menu, click on Edit Plan');
-        await PageHelper.click(CommonPage.editPlan);
+        await PageHelper.clickAndWaitForElementToHide(CommonPage.editPlan);
 
         StepLogger.verification('Select Planner pop-up displays with different planner options to select');
         await expect(await PageHelper.isElementDisplayed(CommonPage.dialogTitle)).toBe(true,
@@ -135,11 +135,9 @@ export class PlannerSettingsPageHelper {
 
         StepLogger.stepId(8);
         StepLogger.step('Select the newly added planner [Ex: Smoke Test Planner 1] in Select Planner pop up');
-        await ProjectItemPageHelper.selectPlannerIfPopUpAppears(ElementHelper.getElementByText(name));
+        await ProjectItemPageHelper.selectPlannerIfPopUpAppears(CommonPageHelper.getSpanByText(name));
 
         StepLogger.verification('Selected Project is opened using the Newly created Planner [Ex: Smoke Test Planner 1]');
-        // After select project Planner wait required, not element found which can use with waitHelper.
-        await browser.sleep(PageHelper.timeout.m);
         await WaitHelper.waitForElementToBeHidden(CommonPage.plannerbox);
         await expect(await browser.getTitle()).toContain(projectName,
             ValidationsHelper.getDisplayedValidation(PlannerSettingsPageConstants.validation.project));
@@ -148,12 +146,11 @@ export class PlannerSettingsPageHelper {
 
         StepLogger.stepId(9);
         StepLogger.step('Click Close button in Project Planner window [Ex: Smoke Test Planner 1]');
-        await WaitHelper.waitForElementToBePresent(ProjectItemPage.close);
-        await ElementHelper.actionHoverOver(ProjectItemPage.close);
         await PageHelper.click(ProjectItemPage.close);
 
         StepLogger.stepId(10);
         StepLogger.step('Click on Leave button in the confirmation dialog');
+        await PageHelper.acceptAlert();
 
         StepLogger.verification('Project Planner window [Ex: Smoke Test Planner 1] is closed');
         await WaitHelper.waitForElementToBeClickable(CommonPage.editPlan);
