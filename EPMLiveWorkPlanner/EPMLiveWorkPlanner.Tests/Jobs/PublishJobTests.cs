335fe1794053c9e0e7b06f6a64b520f64fc1ba7e
vigneshkanna-aurea <vignesh.kanna@aurea.com>
2018-11-17 01:24:42 +0000

    Unnecessary fakes removed

--- EPMLiveWorkPlanner/EPMLiveWorkPlanner.Tests/Jobs/PublishJobTests.cs
+++ EPMLiveWorkPlanner/EPMLiveWorkPlanner.Tests/Jobs/PublishJobTests.cs
@@ -10,7 +10,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO.Fakes;
 using System.Linq;
 using System.Reflection;
-using System.Web.Fakes;
 using System.Xml;
 using System.Xml.Linq;
 using EPMLiveCore.API.Fakes;
@@ -21,7 +20,6 @@ using Microsoft.SharePoint;
 using Microsoft.SharePoint.Administration.Fakes;
 using Microsoft.SharePoint.Fakes;
 using Microsoft.SharePoint.Utilities.Fakes;
-using Microsoft.SharePoint.WebControls.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shouldly;
 using static EPMLiveWorkPlanner.WorkPlannerAPI;
@@ -109,37 +107,15 @@ namespace EPMLiveWorkPlanner.Tests.Jobs
             ShimSqlConnection.AllInstances.BeginTransaction = _ => transaction;
             ShimDbTransaction.AllInstances.Dispose = _ => { };
             ShimSqlConnection.AllInstances.CreateCommand = _ => new SqlCommand();
-            ShimSqlCommand.AllInstances.ExecuteNonQuery = _ => DummyInt;
             ShimComponent.AllInstances.Dispose = _ => { };
             ShimSqlCommand.AllInstances.TransactionSetSqlTransaction = (_, __) => { };
             ShimSPDatabase.AllInstances.DatabaseConnectionStringGet = _ => DummyString;
-            ShimGridGanttSettings.ConstructorSPList = (_, __) => new ShimGridGanttSettings();
-            ShimHttpUtility.HtmlEncodeString = input => input;
-            ShimSPSite.ConstructorString = (_, __) => new ShimSPSite();
-            ShimSPSite.ConstructorGuid = (_, __) => new ShimSPSite();
             ShimSPSite.ConstructorGuidSPUserToken = (_, _1, _2) => new ShimSPSite();
-            ShimSPSite.AllInstances.OpenWeb = _ => spWeb;
-            ShimSPSite.AllInstances.OpenWebString = (_, __) => spWeb;
             ShimSPSite.AllInstances.OpenWebGuid = (_, __) => spWeb;
             ShimSPSite.AllInstances.Dispose = _ => { };
             ShimSPWeb.AllInstances.Dispose = _ => { };
-            ShimCoreFunctions.getLockedWebSPWeb = _ => guid;
-            ShimCoreFunctions.getConfigSettingSPWebString = (_, __) => DummyString;
-            ShimCoreFunctions.getListSettingStringSPList = (_, __) => DummyString;
             ShimCoreFunctions.getConnectionStringGuid = _ => DummyString;
-            ShimCoreFunctions.getLockConfigSettingSPWebStringBoolean = (_1, _2, _3) => DummyString;
-            ShimSPList.AllInstances.GetItemsSPQuery = (_, __) => spListItemCollection;
             ShimSPPersistedObject.AllInstances.IdGet = _ => guid;
-            ShimSPSecurity.RunWithElevatedPrivilegesSPSecurityCodeToRunElevated = codeToRun => codeToRun();
-            ShimUnsecuredLayoutsPageBase.AllInstances.SiteGet = _ => spSite;
-            ShimUnsecuredLayoutsPageBase.AllInstances.WebGet = _ => spWeb;
-            ShimAct.ConstructorSPWeb = (_, __) => new ShimAct();
-            ShimSPContext.CurrentGet = () => new ShimSPContext();
-            ShimSPContext.AllInstances.WebGet = _ => spWeb;
-            ShimSPContext.AllInstances.SiteGet = _ => spSite;
-            ShimSPFieldLookupValueCollection.ConstructorString = (_, __) => new ShimSPFieldLookupValueCollection();
-            ShimSPFieldLookupValue.ConstructorString = (_, __) => new ShimSPFieldLookupValue();
-            ShimSPFieldLookupValue.AllInstances.LookupIdGet = _ => DummyInt;
             ShimDisabledItemEventScope.Constructor = _ => new ShimDisabledItemEventScope();
             ShimDisabledItemEventScope.AllInstances.Dispose = _ => { };
             ShimSPUserCollection.AllInstances.GetByIDInt32 = (_, __) => spUser;
@@ -159,9 +135,6 @@ namespace EPMLiveWorkPlanner.Tests.Jobs
                 SiteGet = () => spSite,
                 ListsGet = () => spListCollection,
                 GetFolderString = _ => spFolder,
-                GetFileString = _ => spFile,
-                FoldersGet = () => spFolderCollection,
-                CurrentUserGet = () => spUser,
                 ServerRelativeUrlGet = () => SampleUrl,
                 AllUsersGet = () => new ShimSPUserCollection(),
                 SiteUsersGet = () => new ShimSPUserCollection(),
@@ -170,7 +143,6 @@ namespace EPMLiveWorkPlanner.Tests.Jobs
             {
                 IDGet = () => guid,
                 WebApplicationGet = () => new ShimSPWebApplication(),
-                RootWebGet = () => spWeb,
                 FeaturesGet = () => new ShimSPFeatureCollection()
                 {
                     ItemGetGuid = _ => new ShimSPFeature()
@@ -179,7 +151,6 @@ namespace EPMLiveWorkPlanner.Tests.Jobs
             };
             spListCollection = new ShimSPListCollection()
             {
-                TryGetListString = _ => spList,
                 ItemGetString = _ => spList,
                 ItemGetGuid = _ => spList
             };
@@ -188,94 +159,46 @@ namespace EPMLiveWorkPlanner.Tests.Jobs
                 IDGet = () => guid,
                 FieldsGet = () => spFieldCollection,
                 GetItemByIdInt32 = _ => spListItem,
-                ItemsGet = () => spListItemCollection,
-                GetItemsSPQuery = _ => spListItemCollection,
-                RootFolderGet = () => spFolder,
                 ParentWebGet = () => spWeb,
-                DefaultViewGet = () => spView,
-                ViewsGet = () => spViewCollection,
                 ContentTypesGet = () => spContentTypeCollection,
-                TitleGet = () => DummyString,
                 EventReceiversGet = () => new ShimSPEventReceiverDefinitionCollection(),
                 DefaultViewUrlGet = () => SampleUrl
             };
-            spListItemCollection = new ShimSPListItemCollection()
-            {
-                CountGet = () => DummyInt,
-                ItemGetInt32 = _ => spListItem
-            };
+            spListItemCollection = new ShimSPListItemCollection();
             spListItem = new ShimSPListItem()
             {
                 IDGet = () => DummyInt,
-                TitleGet = () => DummyString,
-                ItemGetString = _ => DummyString,
                 ItemGetGuid = _ => DummyString,
-                ItemSetGuidObject = (_, __) => { },
-                Update = () => { },
-                FileGet = () => spFile,
                 ParentListGet = () => spList,
                 NameGet = () => DummyString
             };
             spFieldCollection = new ShimSPFieldCollection()
             {
                 GetFieldByInternalNameString = _ => spField,
-                ContainsFieldString = _ => false,
-                GetFieldString = _ => spField,
                 ItemGetString = _ => spField
             };
             spField = new ShimSPField()
             {
                 IdGet = () => guid,
-                TitleGet = () => DummyString,
-                InternalNameGet = () => DummyString,
                 ReadOnlyFieldGet = () => false,
-                HiddenGet = () => false,
-                ReorderableGet = () => true,
-                TypeAsStringGet = () => DummyString
             };
             spUser = new ShimSPUser()
             {
                 IDGet = () => DummyInt,
-                IsSiteAdminGet = () => true,
                 UserTokenGet = () => new ShimSPUserToken()
             };
-            spFolderCollection = new ShimSPFolderCollection()
-            {
-                ItemGetString = _ => spFolder,
-                AddString = _ => spFolder
-            };
+            spFolderCollection = new ShimSPFolderCollection();
             spFolder = new ShimSPFolder()
             {
                 ExistsGet = () => false,
-                SubFoldersGet = () => spFolderCollection,
-                FilesGet = () => spFileCollection,
                 UrlGet = () => SampleUrl,
                 UniqueIdGet = () => guid,
                 ParentWebGet = () => spWeb
             };
-            spFileCollection = new ShimSPFileCollection()
-            {
-                CountGet = () => DummyInt,
-                AddStringByteArrayBoolean = (_1, _2, _3) => spFile,
-                AddStringStream = (_1, _2) => spFile,
-                ItemGetString = _ => spFile
-            };
-            spFile = new ShimSPFile()
-            {
-                Delete = () => { },
-                OpenBinaryStream = () => null,
-                NameGet = () => DummyString,
-                GetListItemStringArray = _ => spListItem
-            };
-            spViewCollection = new ShimSPViewCollection()
-            {
-                ItemGetString = _ => spView
-            };
-            spView = new ShimSPView()
-            {
-                ViewFieldsGet = () => spViewFieldCollection,
-                ServerRelativeUrlGet = () => SampleUrl
-            };
+            spFileCollection = new ShimSPFileCollection();
+            spFile = new ShimSPFile();
+            spViewCollection = new ShimSPViewCollection();
+            spView = new ShimSPView();
             spViewFieldCollection = new ShimSPViewFieldCollection();
             spContentTypeCollection = new ShimSPContentTypeCollection()
             {
@@ -283,13 +206,9 @@ namespace EPMLiveWorkPlanner.Tests.Jobs
             };
             spContentType = new ShimSPContentType()
             {
-                IdGet = () => default(SPContentTypeId),
-                FieldLinksGet = () => spFieldLinkCollection
+                IdGet = () => default(SPContentTypeId)
             };
-            spFieldLinkCollection = new ShimSPFieldLinkCollection()
-            {
-                ItemGetGuid = _ => new ShimSPFieldLink()
-            };
+            spFieldLinkCollection = new ShimSPFieldLinkCollection();
             transaction = new ShimSqlTransaction()
             {
                 Commit = () => { },
