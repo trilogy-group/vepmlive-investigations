d32434553fb4883d42cfaf1389686a189d15d27c
Essam Elsayed <essam.elsayed@versata.com>
2018-11-19 20:54:23 +0200

    Update update-hosts-file.sh

--- BuildJenkins/Protractor/update-hosts-file.sh
+++ BuildJenkins/Protractor/update-hosts-file.sh
@@ -4,7 +4,7 @@ ETC_HOSTS=/etc/hosts
 HOSTS_LINE="$1\t$2"
 
 # Removing existing entry
-sudo sed "/$2/d" $ETC_HOSTS
+sudo sed -i "/$2/d" $ETC_HOSTS
 
 # Adding updated entry
 sudo sh -c -e "echo '$HOSTS_LINE' >> $ETC_HOSTS"
