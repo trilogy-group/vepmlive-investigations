2fa6573afe6c450e801951be7699d72ed59f6323
Essam Elsayed <essam.elsayed@aurea.com>
2018-11-19 10:45:29 +0000

    fix paths

--- BuildTeamCity/tl_buildInstaller.ps1
+++ BuildTeamCity/tl_buildInstaller.ps1
@@ -434,7 +434,7 @@ Copy-Item $SourcesDirectory\EPMLiveCore\EPMLiveCore\Resources\*.sql $BuildDepend
 if (!$SkipInstallShield)
 {
 	#Run Installshield project to generate product .exe
-	& 'C:\Program Files (x86)\InstallShield\2015\System\IsCmdBld.exe' -p '$SourcesDirectory\InstallShield\WorkEngine5\WorkEngine5.ism' -y $NewReleaseNumber -a 'Product Configuration 1' -r 'PrimaryRelease' -l PATH_TO_BUILDDDEPENDENC_FILES='$BuildDependenciesFolder' -l PATH_TO_PRODUCTOUTPUT_FILES='$ProductOutput'
+	& "C:\Program Files (x86)\InstallShield\2015\System\IsCmdBld.exe" -p "$SourcesDirectory\InstallShield\WorkEngine5\WorkEngine5.ism" -y $NewReleaseNumber -a "Product Configuration 1" -r "PrimaryRelease" -l PATH_TO_BUILDDDEPENDENC_FILES="$BuildDependenciesFolder" -l PATH_TO_PRODUCTOUTPUT_FILES="$ProductOutput"
 	Rename-Item -Path '$SourcesDirectory\InstallShield\WorkEngine5\Product Configuration 1\PrimaryRelease\DiskImages\DISK1\Setup.exe' -NewName 'WorkEngine$NewReleaseNumber.exe'
 }
 Stop-Process -Name MSBuild -Force -ErrorAction Ignore  
