4c83e795202cda5d5c1dd5aa24092eff725bdbe2
Bruno Serrano <bruno.serrano@aurea.com>
2018-11-26 13:06:01 +0000

    Initial

---
+++ EPMLiveWebParts/EPMLiveWebPartsTests/WebPageCode/getgriditemsTests.addItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Fakes;
+using System.Collections.Generic;
+using System.Collections.Generic.Fakes;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.DirectoryServices.ActiveDirectory.Fakes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Fakes;
+using System.Web.UI.Fakes;
+using System.Xml;
+using System.Xml.Fakes;
+using EPMLiveWebParts;
+using EPMLiveWebParts.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace EPMLiveWebParts.Tests
+{
+    public partial class getgriditemsTests
+    {
+        private const string AddItemMethod = "addItem";
+
+        [TestMethod]
+        public void addItem_()
+        {
+            // Arrange
+            var listItem = new ShimSPListItem();
+
+            // Act
+            //_privateObj.Invoke(AddItemMethod, new object[] { listItem.Instance, DummyVal });
+
+            // Assert
+        }
+    }
+}
