44e954b55a6a7268bb848b81310721708837e1f0
Essam Elsayed <essam.elsayed@versata.com>
2018-12-11 20:04:28 +0200

    Update build.ps1

--- BuildJenkins/RC/build.ps1
+++ BuildJenkins/RC/build.ps1
@@ -26,6 +26,9 @@ Set-Location -Path $rootDir
 Write-Host "Write build number"
 "[assembly:System.Reflection.AssemblyFileVersion(""$version"")]" > CommonAssemblyInfo.cs
 $env:build_number = $version
+Write-Host "Noted Version: $version"
+Write-Host "Noted env.build_number: $env:build_number"
+
 
 Write-Host "Execute builds"
 Write-Host "Executing tl_buildInstaller.ps1"
