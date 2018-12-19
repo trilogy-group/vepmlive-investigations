36ffef862421a7f63de8a50f4a7163a82aff4334
Lucas Taves <lucas.taves@devfactory.com>
2018-12-19 09:09:37 -0200

    [CC-783730 - Reduing Long Class Length

---
+++ EPMLiveDashboards/EPMLiveDashboards/ProjectData.Getters.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Diagnostics;
+using Microsoft.SharePoint;
+
+namespace Dashboard
+{
+    internal partial class ProjectData
+    {
+        private string getIssueStatus(string project)
+        {
+            try
+            {
+                var status = "green";
+
+                var query = new SPQuery
+                {
+                    Query = string.Format("<Where><Eq><FieldRef Name='Project'/><Value Type='Text'>{0}</Value></Eq></Where>", project)
+                };
+
+                foreach (SPListItem liTask in issueList.GetItems(query))
+                {
+                    var taskStatus = string.Empty;
+                    var dueDate = DateTime.Now;
+                    try
+                    {
+                        taskStatus = liTask["Status"].ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.TraceError("Exception Suppressed {0}", ex);
+                    }
+                    try
+                    {
+                        dueDate = DateTime.Parse(liTask["DueDate"].ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.TraceError("Exception Suppressed {0}", ex);
+                    }
+                    if (taskStatus == "Active")
+                    {
+                        issue_active++;
+                        if (status == "green")
+                        {
+                            status = "yellow";
+                        }
+                        if (dueDate < DateTime.Now)
+                        {
+                            status = "red";
+                        }
+                    }
+                    else if (taskStatus == "Postponed")
+                    {
+                        issue_postponed++;
+                    }
+                    else if (taskStatus == "Closed")
+                    {
+                        issue_closed++;
+                    }
+                }
+
+                return status;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private string getRiskStatus(string project)
+        {
+            try
+            {
+                var status = "green";
+
+                var query = new SPQuery
+                {
+                    Query = string.Format("<Where><Eq><FieldRef Name='Project'/><Value Type='Text'>{0}</Value></Eq></Where>", project)
+                };
+
+                foreach (SPListItem liTask in riskList.GetItems(query))
+                {
+                    var taskStatus = string.Empty;
+                    var dueDate = DateTime.Now;
+
+                    try
+                    {
+                        taskStatus = liTask["Status"].ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.TraceError("Exception Suppressed {0}", ex);
+                    }
+                    try
+                    {
+                        dueDate = DateTime.Parse(liTask["DueDate"].ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.TraceError("Exception Suppressed {0}", ex);
+                    }
+                    if (taskStatus == "Active")
+                    {
+                        risk_active++;
+                        if (status == "green")
+                        {
+                            status = "yellow";
+                        }
+                        if (dueDate < DateTime.Now)
+                        {
+                            status = "red";
+                        }
+                    }
+                    else if (taskStatus == "Postponed")
+                    {
+                        risk_postponed++;
+                    }
+                    else if (taskStatus == "Closed")
+                    {
+                        risk_closed++;
+                    }
+                }
+
+                return status;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private int getTaskCount(string project)
+        {
+            var taskCount = 0;
+            try
+            {
+                TaskHelper.GetTaskCount(
+                    taskList,
+                    project,
+                    ref taskCount,
+                    ref ms_complete,
+                    ref task_complete,
+                    ref ms_future,
+                    ref task_future,
+                    ref ms_late,
+                    ref task_late,
+                    ref ms_current,
+                    ref task_current);
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return -1;
+            }
+            return taskCount;
+        }
+    }
+}
