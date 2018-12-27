8be0112bef529536eea381a2fd5026b91da9157c
Chander Kishore <chander.k@versata.com>
2018-12-26 23:36:56 -0800

    Online Help Guide Linki s removed from help menu

--- ProjectPublisher2016/ProjectPublisher2016/PublisherRibbon.cs
+++ ProjectPublisher2016/ProjectPublisher2016/PublisherRibbon.cs
@@ -84,10 +84,7 @@ namespace ProjectPublisher2016
             Globals.ThisAddIn.c.Activate();
         }
 
-        private void btnOnline_Click(object sender, RibbonControlEventArgs e)
-        {
-            Globals.ThisAddIn.c.Help();
-        }        
+    
 
         private void btnAbout_Click(object sender, RibbonControlEventArgs e)
         {
