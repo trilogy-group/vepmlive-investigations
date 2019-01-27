d240162acc833e1548a0c0416b5ac3c2e8ab98ea
Essam Elsayed <essam.elsayed@versata.com>
2019-01-27 18:40:50 +0200

    Update 0Tables02.sql

--- EPMLiveCore/EPMLiveCore/Resources/0Tables02.sql
+++ EPMLiveCore/EPMLiveCore/Resources/0Tables02.sql
@@ -497,3 +497,16 @@ begin
 	[TS_UID] ASC
 	)WITH (SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
 end
+-------------------------------------------
+		   
+if not exists(select * from sys.indexes where name = 'IX_ITEMSEC_QUEUE')
+begin
+	CREATE INDEX [IX_ITEMSEC_QUEUE] ON [dbo].[ITEMSEC] ([QUEUE], [STATUS]) INCLUDE ([USER_ID], [dtadded], [PRIORITY]) WITH (ONLINE=ON)
+end 
+
+if not exists(select * from sys.indexes where name = 'IX_TSRESMETA_TS_UID')
+begin
+	CREATE INDEX [IX_TSRESMETA_TS_UID] ON [dbo].[TSRESMETA] ([TS_UID]) WITH (ONLINE=ON)
+end
+													  
+													  
