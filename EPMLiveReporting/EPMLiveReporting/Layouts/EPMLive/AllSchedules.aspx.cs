6ad899f95086b6b54164ac201f21acc8ed940b40
Essam Elsayed <essam.elsayed@versata.com>
2019-02-21 23:35:29 +0200

    Revert "https://jira.devfactory.com/browse/SKYVERA-3737 (#1869)" (#1874)
    This reverts commit 01946fe15f1c0372149bba3164440979b7735986.

--- EPMLiveReporting/EPMLiveReporting/Layouts/EPMLive/AllSchedules.aspx.cs
+++ EPMLiveReporting/EPMLiveReporting/Layouts/EPMLive/AllSchedules.aspx.cs
@@ -180,31 +180,26 @@ namespace EPMLiveReportsAdmin.Layouts.EPMLive
                     cn.Open();
 
                     Guid timerjobuid = Guid.Empty;
-                    using (var cmd =
+                    var cmd =
                         new SqlCommand("select timerjobuid from timerjobs where siteguid=@siteguid and jobtype=5",
-                            cn))
+                            cn);
+                    cmd.Parameters.AddWithValue("@siteguid", SPContext.Current.Site.ID.ToString());
+                    object obj = cmd.ExecuteScalar();
+                    if (obj != null)
                     {
-                        cmd.Parameters.AddWithValue("@siteguid", SPContext.Current.Site.ID.ToString());
-                        object obj = cmd.ExecuteScalar();
-                        if (obj != null)
-                        {
-                            timerjobuid = new Guid(obj.ToString());
-                        }
+                        timerjobuid = new Guid(obj.ToString());
                     }
-
-                    if (timerjobuid == Guid.Empty)
+                    else
                     {
                         timerjobuid = Guid.NewGuid();
-                        using (var cmd =
+                        cmd =
                             new SqlCommand(
-                                "INSERT INTO TIMERJOBS (timerjobuid, siteguid, jobtype, jobname, scheduletype, webguid) VALUES (@timerjobuid, @siteguid, 5, 'Reporting Refresh', 2, @webguid)",
-                                cn))
-                        {
-                            cmd.Parameters.AddWithValue("@timerjobuid", timerjobuid);
-                            cmd.Parameters.AddWithValue("@siteguid", SPContext.Current.Site.ID.ToString());
-                            cmd.Parameters.AddWithValue("@webguid", SPContext.Current.Web.ID.ToString());
-                            cmd.ExecuteNonQuery();
-                        }
+                                "INSERT INTO TIMERJOBS (timerjobuid, siteguid, jobtype, jobname, scheduletype, webguid) VALUES (@timerjobuid, @siteguid, 5, 'Reporting Refresh', 0, @webguid)",
+                                cn);
+                        cmd.Parameters.AddWithValue("@timerjobuid", timerjobuid);
+                        cmd.Parameters.AddWithValue("@siteguid", SPContext.Current.Site.ID.ToString());
+                        cmd.Parameters.AddWithValue("@webguid", SPContext.Current.Web.ID.ToString());
+                        cmd.ExecuteNonQuery();
                     }
 
                     if (timerjobuid != Guid.Empty)
