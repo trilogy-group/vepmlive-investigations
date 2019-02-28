61726a8a757b078f8515c15e206d752d81763446
Essam Elsayed <essam.elsayed@versata.com>
2019-02-28 17:24:42 +0200

    SKYVERA-3489 Releases/7.1.0 (#1882)
    * fix jenkins version
    
    * SKYVERA-3489: Generalizing the semicolon validation to handle all cases. (#1881)

--- EPMLiveCore/EPMLiveCore/ResourcePoolEvent/ResourcePoolEvent.cs
+++ EPMLiveCore/EPMLiveCore/ResourcePoolEvent/ResourcePoolEvent.cs
@@ -742,19 +742,32 @@ namespace EPMLiveCore
             {
 
                 SPFieldUserValue uv = null;
+                SPFieldUserValue uv_new = null;
                 if (isAdd)
+                {
                     uv = new SPFieldUserValue(properties.Web, properties.AfterProperties["SharePointAccount"].ToString());
+                }
                 else
+                {
                     uv = new SPFieldUserValue(properties.Web, properties.ListItem["SharePointAccount"].ToString());
+
+                    if (properties.AfterProperties["SharePointAccount"] != null)
+                        uv_new = new SPFieldUserValue(properties.Web, properties.AfterProperties["SharePointAccount"].ToString());
+                }
 
                 SPUser u = uv.User;
                 if (u == null)
                 {
                     u = properties.Web.EnsureUser(uv.LookupValue);
                 }
+                SPUser u_new = null;
+                if (uv_new != null)
+                {
+                    u_new = properties.Web.EnsureUser(uv_new.LookupValue);
+                }
 
                 // semicolon causes issues in list cleanup and potentially in other modules.
-                if (u.Name.Contains(";"))
+                if ((isAdd && u.Name.Contains(";")) || (!isAdd && u_new != null && u_new.Name.Contains(";")))
                 {
                     throw new InvalidOperationException("Semicolon is not allowed for AD username.");
                 }
@@ -817,7 +830,7 @@ namespace EPMLiveCore
                 }
                 catch { }
             }
-            catch(InvalidOperationException ex)
+            catch (InvalidOperationException ex)
             {
                 throw ex;
             }
