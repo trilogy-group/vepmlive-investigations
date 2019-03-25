c2c7dedaac5e0bb3441da0cf7f654810dc2c7c3c
Lucas Taves <lucas.taves@devfactory.com>
2019-03-25 18:01:50 -0300

    CC-77316: Reduces GetAssociatedItems method for AssociatedListItems.cs

---
+++ EPMLiveCore/EPMLiveCore/API/AssociatedListItems/ProjectData.cs
@@ -0,0 +1,20 @@
+﻿namespace EPMLiveCore.API
+{
+    using System.Text;
+
+    internal class ProjectData
+    {
+        internal ProjectData(StringBuilder sqlGetListHeaders, StringBuilder ListAssociatedItemsDivStringBuilder, string projectLinkedField, string sourceUrl)
+        {
+            SqlGetListHeaders = sqlGetListHeaders;
+            this.ListAssociatedItemsDivStringBuilder = ListAssociatedItemsDivStringBuilder;
+            ProjectLinkedField = projectLinkedField;
+            SourceUrl = sourceUrl;
+        }
+
+        internal StringBuilder SqlGetListHeaders { get; private set; }
+        internal StringBuilder ListAssociatedItemsDivStringBuilder { get; private set; }
+        internal string ProjectLinkedField { get; set; }
+        internal string SourceUrl { get; private set; }
+    }
+}
---
+++ EPMLiveCore/EPMLiveCore/API/AssociatedListItems/SiteData.cs
@@ -0,0 +1,28 @@
+﻿namespace EPMLiveCore.API
+{
+    using System;
+    using System.Data;
+
+    internal class SiteData
+    {
+        internal SiteData(string siteUrl, string webId, Guid projectListId, DataTable listNameTable, string projectId, string siteId,
+                          string projectTitle)
+        {
+            SiteUrl = siteUrl;
+            WebId = webId;
+            ProjectListId = projectListId;
+            ListNameTable = listNameTable;
+            ProjectId = projectId;
+            SiteId = siteId;
+            ProjectTitle = projectTitle;
+        }
+
+        internal string SiteUrl { get; private set; }
+        internal string WebId { get; private set; }
+        internal Guid ProjectListId { get; private set; }
+        internal DataTable ListNameTable { get; set; }
+        internal string ProjectId { get; private set; }
+        internal string SiteId { get; private set; }
+        internal string ProjectTitle { get; private set; }
+    }
+}
