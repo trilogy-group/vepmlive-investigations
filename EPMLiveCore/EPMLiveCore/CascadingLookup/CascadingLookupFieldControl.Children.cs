b87e8e553f0f3439abb390edf07f1013bb35551a
Lucas Taves <lucas.taves@devfactory.com>
2019-03-27 12:14:45 -0300

    CC-78270: Refactors CascadingLookup.FieldControl.cs

---
+++ EPMLiveCore/EPMLiveCore/CascadingLookup/CascadingLookupFieldControl.Children.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace EPMLiveCore
+{
+    public partial class CascadingLookupFieldControl
+    {
+        private void UpdateChildren(string ChildrenField)
+        {
+            var children = ChildrenField.Split(
+                new[]
+                {
+                    "] "
+                },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var child in children)
+            {
+                var control = FindControlRecursive(Page, child);
+                if (control != null)
+                {
+                    var childField = (CascadingLookupFieldControl)control.Parent.Parent;
+                    if (childField != null)
+                    {
+                        control = FindControlRecursive(Page, childField.strFilterValueField);
+                        if (control != null)
+                        {
+                            var ddlFilterValueField = (DropDownList)control;
+                            PopulateChild(childField, ddlFilterValueField.SelectedValue);
+
+                            if (!string.IsNullOrWhiteSpace(childField.strChildrenField))
+                            {
+                                UpdateChildren(childField.strChildrenField);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PopulateChild(CascadingLookupFieldControl field, string filterValue)
+        {
+            var query = BuildQuery(field.strField, field.strParentField, field.strFilterValueField, filterValue);
+            var queryResults = ExecuteQuery(field.strUrl, field.strList, field.strField, query);
+            if (queryResults != null)
+            {
+                BindDDL(field, queryResults.GetDataTable(), field.strField);
+            }
+        }
+    }
+}
---
+++ EPMLiveCore/EPMLiveCore/CascadingLookup/CascadingLookupFieldControl.EventHandling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EPMLiveCore
+{
+    public partial class CascadingLookupFieldControl
+    {
+        private void ddlCLField_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EnsureChildControls();
+            try
+            {
+                ClearError();
+                if (!string.IsNullOrWhiteSpace(strChildrenField))
+                {
+                    UpdateChildren(strChildrenField);
+                }
+            }
+            catch (Exception ex)
+            {
+                ReportError(ex);
+            }
+        }
+    }
+}
