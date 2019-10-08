1e8cd0b3b325d9d6683cc5541cf2e2b6bfc80882
Essam Elsayed <essam.elsayed@aurea.com>
2019-10-08 12:22:27 +0000

    Fix BasV2

--- BuildJenkins/MsBuild/CiBuild/build.ps1
+++ BuildJenkins/MsBuild/CiBuild/build.ps1
@@ -4,31 +4,6 @@ param(
 )
 $ErrorActionPreference = "Stop"
 
-$TempFileLocation = "$env:windir\Temp","$env:TEMP"
-$SoftwareDistributionLocation = "$env:windir\SoftwareDistribution\Download"
-
-$TempFile = Get-ChildItem $TempFileLocation -Recurse
-$TempFileCount = ($TempFile).count
-
-if($TempFileCount -eq "0") { 
-Write-Host "There are no files in the folder $TempFileLocation" -ForegroundColor Green
-}
-Else {
-$TempFile | Remove-Item -Confirm:$false -Recurse -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-Write-Host "Cleared $TempFileCount files in the folder $TempFileLocation" -ForegroundColor Green
-}
-
-$SoftwareDistribution = Get-ChildItem $SoftwareDistributionLocation -Recurse
-$SoftwareDistributionCount = ($SoftwareDistribution).Count
-if($SoftwareDistributionCount -eq "0"){
-Write-Host " There are no files in the folder $SoftwareDistributionLocation" -ForegroundColor Green
-}
-Else
-{
-$SoftwareDistribution | Remove-Item -Confirm:$false -Recurse -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-Write-Host "Cleared $SoftwareDistributionCount files in the folder $SoftwareDistributionLocation" -ForegroundColor Green
-}
-
 # Define directories
 $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 if ($rootDir -eq '') {
