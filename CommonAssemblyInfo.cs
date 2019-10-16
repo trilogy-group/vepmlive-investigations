6f6b11eb5e3f5009a2c0d087db0bb978a179359e
mena.armanyous <mena.armanyous@versata.com>
2019-10-16 15:51:52 +0000

    update version numbers

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyFileVersion("7.1.2.1")]
+[assembly: AssemblyFileVersion("7.1.3.1")]
