b025fb644e98930d679d629fc6323a7201755651
Essam Elsayed <essam.elsayed@aurea.com>
2020-02-12 10:40:24 +0000

    Pump fix versions

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyFileVersion("7.1.8.1")]
+[assembly: AssemblyFileVersion("7.1.9.1")]
