957f5d7d3c3deb02ebcb5c252372c7db83f37dab
essam.elsayed <essam.elsayed@aurea.com>
2020-09-16 06:22:31 -0500

    pump solution version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyFileVersion("7.2.5.1")]
+[assembly: AssemblyFileVersion("7.2.6.2")]
